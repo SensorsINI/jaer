74ad8be6f2d301a5a622fa00ecade3b00ccfb620
Tobi Delbruck <tobi@ini.uzh.ch>
2007-05-01 16:02:46 +0000

    set the executable permission property on jAERViewer.sh and removed the old script to run CaviarViewer.sh under linux.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@29 b7f4320f-462c-0410-a916-d9f35bb82d52

--- CaviarViewer.sh
+++
@@ -1,2 +0,0 @@
-# this script starts CaviarViewer under linux if it is run from this folder
-java -DJOGL_SINGLE_THREADED_WORKAROUND=false -Djava.library.path=JNI -Dsun.java2d.opengl=false -Dsun.java2d.noddraw=true -cp jars/spread.jar:dist/jAER.jar:jars/UsbIoJava.jar:jars/swing-layout-0.9.jar:jars/jogl.jar ch.unizh.ini.caviar.JAERViewer 
