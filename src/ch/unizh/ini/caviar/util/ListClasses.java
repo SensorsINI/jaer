8bc518c15b3ab4167622cc39edc506dca995d557
Tobi Delbruck <tobi@ini.uzh.ch>
2007-05-14 06:00:56 +0000

    added classes for finding subclasses of a class, for choosing classes from this list using a gui and for editing the list of chosen classses. not used yet in main code.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@47 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/ch/unizh/ini/caviar/util/ListClasses.java
@@ -0,0 +1,129 @@
+/** From http://forums.java.net/jive/thread.jspa?messageID=212405&tstart=0 */
+package ch.unizh.ini.caviar.util;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Vector;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ Provides a static method that returns a List<String> of all classes on java.class.path starting with root of package name, e.g. "org/netbeans/.." and ending with
+ ".class"
+ <p>
+  From http://forums.java.net/jive/thread.jspa?messageID=212405&tstart=0 
+ */
+public class ListClasses {
+    
+    private static boolean debug = false;
+    
+    /**
+     * Main method used in command-line mode for searching the system
+     * classpath for all the .class file available in the classpath
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length ==1) {
+            if (args[0].equals("-debug")) {
+                debug = true;
+                System.out.println(listClasses().size());
+            } else {
+                System.err.println(
+                        "error: unrecognized \"" + args[0] + "\" option ");
+                System.exit(-1);
+            }
+        } else if (args.length == 1 && args[0].equals("-help")) {
+            usage();
+        } else if (args.length == 0) {
+            List<String> classNames=listClasses();
+            for(String s:classNames){
+                System.out.println(s);
+            }
+        } else {
+            usage();
+        }
+        System.exit(0);
+    }
+    private static void usage() {
+        System.err.println(
+                "usage: java ListClasses [-debug]"
+                + "\n\tThe commandline version of ListClasses will search the system"
+                + "\n\tclasspath defined in your environment for all .class files available" );
+        System.exit(-1);
+    }
+    
+    /**
+     * Iterate over the system classpath defined by "java.class.path" searching
+     * for all .class files available
+     @return list of all fully qualified class names
+     *
+     */
+    public static List<String> listClasses() {
+        List<String> classes = new Vector<String>(10,10);
+        try {
+            // get the system classpath
+            String classpath = System.getProperty("java.class.path", "");
+            
+            if (classpath.equals("")) {
+                System.err.println("error: classpath is not set");
+            }
+            
+            if (debug) {
+                System.out.println("system classpath = " + classpath);
+            }
+            
+            StringTokenizer st =
+                    new StringTokenizer(classpath, File.pathSeparator);
+            
+            while (st.hasMoreTokens()) {
+                String token = st.nextToken();
+                File classpathElement = new File(token);
+                classes .addAll(classpathElement.isDirectory()
+                ? loadClassesFromDir(classpathElement.list(new CLASSFilter()))
+                : loadClassesFromJar(classpathElement));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return classes;
+    }
+    
+    private static List<String> loadClassesFromJar(File jarFile) {
+        List<String> files = new Vector<String>(10,10);
+        try {
+            if(jarFile.getName().endsWith(".jar")){
+                System.out.println(jarFile+" is being scanned");
+                Enumeration<JarEntry> fileNames;
+                fileNames = new JarFile(jarFile).entries();
+                JarEntry entry = null;
+                while(fileNames.hasMoreElements()){
+                    entry = fileNames.nextElement();
+                    if(entry.getName().endsWith(".class")){
+                        files.add(entry.getName());
+                    }
+                }
+            }
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+        
+        return files;
+    }
+    
+    private static List<String> loadClassesFromDir(String fileNames[]) {
+        return Arrays.asList(fileNames);
+    }
+}
+class CLASSFilter implements FilenameFilter{
+    public boolean accept(File dir, String name) {
+        return (name.endsWith(".class"));
+    }
+}
+
---
+++ src/ch/unizh/ini/caviar/util/SubclassFinder.java
@@ -0,0 +1,65 @@
+/*
+ * SubclassFinder.java
+ *
+ * Created on May 13, 2007, 8:13 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ *
+ *
+ *Copyright May 13, 2007 Tobi Delbruck, Inst. of Neuroinformatics, UNI-ETH Zurich
+ */
+
+package ch.unizh.ini.caviar.util;
+
+import ch.unizh.ini.caviar.chip.retina.Tmpdiff128;
+import java.util.*;
+
+/**
+ Finds subclasses of a given class name in classes on the loaded classpath.
+ <p>
+ * See http://www.javaworld.com/javaworld/javatips/jw-javatip113.html?page=2
+ * @author tobi
+ */
+public class SubclassFinder {
+    
+    /** Creates a new instance of SubclassFinder */
+    private SubclassFinder() {
+    }
+    
+    /** Finds and returns list of all subclases of a class
+     @param superClassName the fully qualified name, e.g. ch.unizh.ini.caviar.chip.AEChip
+     @return list of fully qualified class names that are subclasses (and not the same as) the argument
+     */
+    public static ArrayList<String> findSubclassesOf(String superClassName) {
+        ArrayList<String> classes=new ArrayList<String>();
+        try{
+            Class superClass = Class.forName(superClassName);
+            List<String> allClasses=ListClasses.listClasses();
+            int n=".class".length();
+            for(String s:allClasses){
+                s=s.substring(0,s.length()-n);
+                s=s.replace('/','.');
+                if(s.indexOf("$")!=-1) continue; // inner class
+                Class c=Class.forName(s);
+                if(c==superClass) continue; // don't add the superclass
+                if(superClass.isAssignableFrom(c)){
+                    classes.add(s);
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            return classes;
+        }
+    }
+    
+    public static void main(String[] args){
+        ArrayList<String> classNames=   findSubclassesOf("ch.unizh.ini.caviar.eventprocessing.EventFilter2D");
+        for(String s:classNames){
+            System.out.println(s);
+        }
+    }
+    
+    
+}
