ed5e63640b7be47339574a6a2f11e25bd8a176cf
Tobi Delbruck <tobi@ini.uzh.ch>
2007-07-15 18:39:41 +0000

    better documetation of LineDetector.java interface.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@167 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/ch/unizh/ini/caviar/eventprocessing/tracking/LineDetector.java
+++ src/ch/unizh/ini/caviar/eventprocessing/tracking/LineDetector.java
@@ -10,16 +10,20 @@
 package ch.unizh.ini.caviar.eventprocessing.tracking;
 
 /**
- * EventFilters implement this interface when they detect a line in scene.
+ * EventFilters implement this interface when they detect a line in scene. The line is characterized by the normal form (rho,theta) where
+ theta is the angle of the line and rho is the closest distance of the line from the origin. These coordinates are illustrated below.
+ Note that rho can be positive or negative but theta is bound to (0,180). Theta is cut at 0 and 180 degrees and 0 and 180 degrees can both represent
+ the same line with negated rho. Note also that theta is the angle of the <strong>normal</strong> to the line, not the angle of the line itself.
+ <p>
+ <img src="doc-files/LineDetector.png" />
  
  * @author tobi
  */
 public interface LineDetector {
     /**
      * returns the filtered Hough line radius estimate - the closest distance from the middle of the chip image.
-     * 
-     * @return the distance in pixels. If the chip size is sx by sy, can range over +-Math.sqrt( (sx/2)^2 + (sy/2)^2).
-     *     This number is positive if the line is above the origin (center of chip)
+     @return the distance in pixels. If the chip size is sx by sy, can range over +-Math.sqrt( (sx/2)^2 + (sy/2)^2).
+     This number is positive if the line is above the origin (center of chip)
      */
     float getRhoPixelsFiltered();
 
