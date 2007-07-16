68cc48eb7c07ed79556cb4da47b791ecb866c54c
Tobi Delbruck <tobi@ini.uzh.ch>
2007-07-16 17:35:34 +0000

    new utilty that all classes that use UsbIo can use to see if UsbIo is available
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@171 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/ch/unizh/ini/caviar/hardwareinterface/usb/UsbIoUtilities.java
@@ -0,0 +1,42 @@
+/*
+ * USBIOUtilities.java
+ *
+ * Created on July 16, 2007, 10:10 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ *
+ *
+ *Copyright July 16, 2007 Tobi Delbruck, Inst. of Neuroinformatics, UNI-ETH Zurich
+ */
+
+package ch.unizh.ini.caviar.hardwareinterface.usb;
+
+import java.util.logging.*;
+
+/**
+ * Static methods and flags for USBIO to help build code that will run on non-windows platforms even though Thesycon USBIO
+ package will not run there.
+ * @author tobi
+ */
+public class UsbIoUtilities {
+    
+    static Logger log=Logger.getLogger("USBIO");
+    
+    /** Creates a new instance of USBIOUtilities */
+    private UsbIoUtilities() {
+    }
+    
+    /** classes can check this before trying to do things with UsbIo */
+    public static boolean usbIoIsAvailable=true;
+    
+    static{
+        try{
+            log.info("checking USBIO");
+            System.loadLibrary("USBIOJAVA");
+        }catch(UnsatisfiedLinkError e){
+            log.warning(e.getMessage()+ ": USBIOJAVA libaray not found; either you are not running Windows, the UsbIoJava.jar is not on the classpath, or the native DLL is not on java.library.path");
+            usbIoIsAvailable=false;
+        }
+    }
+}
