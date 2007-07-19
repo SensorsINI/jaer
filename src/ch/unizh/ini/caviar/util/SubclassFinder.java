f71324469bac5074299b513e2a48c4a5562f462e
Albert Cardona <cardonaa@janelia.hhmi.org>
2007-07-19 19:50:07 +0000

    - Catching Throwable errors in the SubclassFinder.findSubclassesOf   which was shortening the proper listing of all classes in the classpath.   The bug appeared in linux, where there are no proper USB libs for jAER.
    - Added shell tools for convenient compilation under *nix OSes.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@228 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/ch/unizh/ini/caviar/util/SubclassFinder.java
+++ src/ch/unizh/ini/caviar/util/SubclassFinder.java
@@ -38,16 +38,20 @@ public class SubclassFinder {
             Class superClass = Class.forName(superClassName);
             List<String> allClasses=ListClasses.listClasses();  // expensive, must search all classpath and make big string array list
             int n=".class".length();
-            for(String s:allClasses){
-                s=s.substring(0,s.length()-n);
-                s=s.replace('/','.');
-                if(s.indexOf("$")!=-1) continue; // inner class
-                Class c=Class.forName(s);
-                if(c==superClass) continue; // don't add the superclass
-                if(superClass.isAssignableFrom(c)){
-                    if(!Modifier.isAbstract(c.getModifiers()))//if class is abstract, dont add to list.
-                        classes.add(s);
-                }
+	    for (String s:allClasses) {
+		try {
+			s=s.substring(0,s.length()-n);
+			s=s.replace('/','.');
+			if(s.indexOf("$")!=-1) continue; // inner class
+			Class c=Class.forName(s);
+			if(c==superClass) continue; // don't add the superclass
+			if(superClass.isAssignableFrom(c)){
+			    if(!Modifier.isAbstract(c.getModifiers()))//if class is abstract, dont add to list.
+				classes.add(s);
+			}
+		} catch (Throwable t) {
+			System.out.println("ERROR: " + t);
+		}
             }
         }catch(Exception e){
             e.printStackTrace();
@@ -61,6 +65,7 @@ public class SubclassFinder {
         for(String s:classNames){
             System.out.println(s);
         }
+	System.exit(0);
     }
     
     
