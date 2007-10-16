98c4f6398228b26efbdcd27971038b04eff7258d
Tobi Delbruck <tobi@ini.uzh.ch>
2007-10-16 05:58:01 +0000

    added new utility for doing a rolling linear fit to data.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@320 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/ch/unizh/ini/caviar/util/RollingLinearRegression.java
@@ -0,0 +1,114 @@
+/*
+ * RollingLinearRegression.java
+ *
+ * Created on October 16, 2007, 7:30 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ *
+ *
+ *Copyright October 16, 2007 Tobi Delbruck, Inst. of Neuroinformatics, UNI-ETH Zurich
+ */
+
+package ch.unizh.ini.caviar.util;
+
+import java.awt.geom.*;
+import java.util.*;
+
+/**
+ * Does a moving or rolling linear regression (a linear fit) on updated data.
+ The new data point replaces the oldest data point. Summary statistics holds the rollling values
+ and are updated by subtracting the oldest point and adding the newest one.
+ 
+ * @author tobi
+ */
+public class RollingLinearRegression {
+    
+    private int length=2;
+    private float sx, sy, sxx; // summary stats
+    private float sxy;
+    private LinkedList<Point2D.Float> points=new LinkedList<Point2D.Float>();
+    private float intercept, slope;
+    private float ux,uy;
+    
+    /** Creates a new instance of RollingLinearRegression */
+    public RollingLinearRegression() {
+    }
+    
+    /** Adds a new point.
+     @param p the point
+     */
+    synchronized public void addPoint(Point2D.Float p){
+        removeOldestPoint();
+        points.add(p);
+        int n=points.size();
+        sx+=p.x;
+        sy+=p.y;
+        ux=sx/n;
+        uy=sy/n;
+        float xx=p.x-ux;
+        sxx+=xx*xx;
+        sxy+=xx*(p.y-uy);
+        slope=sxy/sxx;
+        intercept=uy-slope*ux;
+    }
+    
+    private void removeOldestPoint() {
+        if(points.size()>length){
+            Point2D.Float p=points.removeFirst();
+            int n=points.size();
+            sx-=p.x;
+            sy-=p.y;
+            ux=sx/n;
+            uy=sy/n;
+            float xx=p.x-ux;
+            sxx-=xx*xx;
+            sxy-=xx*(p.y-uy);
+        }
+    }
+    
+    public int getLength() {
+        return length;
+    }
+    
+    /** Sets the window length. Default is 2 points.
+     @param length the number of points to fit 
+     */
+    synchronized public void setLength(int length) {
+        this.length = length;
+        points=new LinkedList<Point2D.Float>();
+        sx=0;
+        sy=0;
+        sxx=0;
+        sxy=0;
+    }
+    
+    public LinkedList<Point2D.Float> getPoints() {
+        return points;
+    }
+    
+    public float getIntercept() {
+        return intercept;
+    }
+    
+    public float getSlope() {
+        return slope;
+    }
+    
+    private static void add(RollingLinearRegression r, float x, float y){
+        r.addPoint(new Point2D.Float(x,y));
+        System.out.println(String.format("x,y=%.1f,%.1f, slope=%.1f, intercept=%.1f",x,y,r.getSlope(),r.getIntercept()));
+    }
+    
+    public static void main(String[] args){
+        RollingLinearRegression r=new RollingLinearRegression();
+        r.setLength(10);
+        add(r,0,0);
+        add(r,1,1);
+        add(r,2,2);
+        add(r,3,4);
+        add(r,3,2);
+    }
+    
+    
+}
