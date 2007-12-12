5b93f7afe20319c1082ed6925651011728762d01
Raphael Berner <raphael@ini.uzh.ch>
2007-12-12 17:18:33 +0000

    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@407 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/ch/unizh/ini/dollbrain/ColorEvent.java
@@ -0,0 +1,44 @@
+package ch.unizh.ini.dollbrain;
+
+import ch.unizh.ini.caviar.event.*;
+/*
+ * TypedEvent.java
+ *
+ * Created on May 28, 2006, 9:20 AM
+ *
+ *
+ *
+ *Copyright May 28, 2006 Tobi Delbruck, Inst. of Neuroinformatics, UNI-ETH Zurich
+ */
+
+/**
+ * Represents an event with a byte type. This is a legacy class to support previous implementations.
+ * @author tobi
+ */
+public class ColorEvent extends TypedEvent {
+    
+    public byte color=0;
+    
+    /** Creates a new instance of TypedEvent */
+    public ColorEvent() {
+    }
+    
+    public byte getColor(){
+        return color;
+    }
+    
+    @Override public String toString(){
+        return super.toString()+" color="+color;
+    }
+    
+    /** copies fields from source event src to this event
+     @param src the event to copy from
+     */
+    @Override public void copyFrom(BasicEvent src){
+        ColorEvent e=(ColorEvent)src;
+        super.copyFrom(e);
+        this.color=e.color;
+    }
+    
+    
+}
