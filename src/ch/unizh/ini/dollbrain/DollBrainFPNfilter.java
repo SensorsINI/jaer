c448b6fe2136318ea0abbb8535e2ba87d65c3e38
Raphael Berner <raphael@ini.uzh.ch>
2008-03-20 16:50:33 +0000

    added filter to reduce fixed patter noise of dollbrain
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@480 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/ch/unizh/ini/dollbrain/DollBrainFPNfilter.java
@@ -0,0 +1,186 @@
+/*
+ * RetinaBackgrondActivityFilter.java
+ *
+ * Created on October 21, 2005, 12:33 PM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package ch.unizh.ini.dollbrain;
+
+import ch.unizh.ini.caviar.chip.*;
+import ch.unizh.ini.caviar.event.*;
+import ch.unizh.ini.caviar.event.EventPacket;
+import ch.unizh.ini.caviar.eventprocessing.EventFilter2D;
+import java.util.*;
+
+/**
+ * An AE background that filters slow background activity by only passing inPacket that are
+ * supported by another event in the past {@link #setDt dt} in the immediate spatial neighborhood, defined
+ * by a subsampling bit shift.
+ * @author tobi
+ */
+public class DollBrainFPNfilter extends EventFilter2D implements Observer  {
+    
+   
+    float[][][] FPNmap;
+    
+    public DollBrainFPNfilter(AEChip chip){
+        super(chip);
+        chip.addObserver(this);
+        initFilter();
+        resetFilter();
+    }
+    
+    void allocateMaps(AEChip chip){
+        FPNmap=new float[4][5][2];
+    }
+    
+    
+    float averageColor=0;
+    double averageTimestamp=0;
+    int numberOfEvents=0;
+    int[][] EventsPerPixel;
+    
+    int frameStart=0;
+    
+    private void initRecordingFPN() {
+        averageColor=0;
+        averageTimestamp=0;
+        numberOfEvents=0;
+        EventsPerPixel= new int[4][5];
+        
+        frameStart=0;
+        
+        for(int i=0;i<EventsPerPixel.length;i++)
+            Arrays.fill(EventsPerPixel[i],0);
+        
+        resetFPNmap();
+    }
+   
+    private boolean recordingFPNinternal=false;
+    
+    /**
+     * filters in to out. if filtering is enabled, the number of out may be less
+     * than the number put in
+     *@param in input events can be null or empty.
+     *@return the processed events, may be fewer in number. filtering may occur in place in the in packet.
+     */
+    synchronized public EventPacket filterPacket(EventPacket in) {
+        if(!filterEnabled) return in;
+        if(enclosedFilter!=null) in=enclosedFilter.filterPacket(in);
+        checkOutputPacketEventType(in);
+        if(FPNmap==null) allocateMaps(chip);
+          
+        if (recordingFPN)
+        {
+            if (!this.recordingFPNinternal)
+            {
+                this.recordingFPNinternal=true;
+                this.initRecordingFPN();
+            }
+            
+            
+            for(Object e:in){
+                ColorEvent i=(ColorEvent)e;
+                
+                if (i.x==7 && i.y==3)
+                {
+                    frameStart=i.timestamp;
+                    continue;
+                }
+                
+                if (frameStart==0)
+                    continue;
+                
+                try {
+                    FPNmap[i.y][i.x][1] += i.color;
+                    FPNmap[i.y][i.x][0] += (i.timestamp-frameStart);
+                    averageColor+= i.color;
+                    averageTimestamp+= (i.timestamp-frameStart);
+                    numberOfEvents+=1;
+                    EventsPerPixel[i.y][i.x]+=1;
+                } catch (java.lang.IndexOutOfBoundsException er) // ignore framestart events
+                {}
+            }
+            
+            
+        } else {
+            if(this.recordingFPNinternal) {
+                this.recordingFPNinternal=false;
+                
+                averageColor=averageColor/numberOfEvents;
+                averageTimestamp=averageTimestamp/numberOfEvents;
+                
+                for (int i=0; i<4; i++)
+                    for (int k=0;k<5;k++) {
+                        FPNmap[i][k][1]=averageColor * EventsPerPixel[i][k] / FPNmap[i][k][1];
+                        FPNmap[i][k][0]=(float) averageTimestamp * EventsPerPixel[i][k] / FPNmap[i][k][0];
+                    }
+            }
+            
+            for(Object e:in){
+                ColorEvent i=(ColorEvent)e;
+                
+                if (i.x==7 && i.y==3)
+                {
+                    frameStart=i.timestamp;
+                }
+                
+                try {
+                    i.color= (short) (i.color*FPNmap[i.y][i.x][1]);
+                    i.timestamp= (int) ( frameStart + (i.timestamp-frameStart)*FPNmap[i.y][i.x][0]);
+                   // System.out.println("color " + i.color + " fpnmap " + FPNmap[i.y][i.x][1]);
+                } catch (java.lang.IndexOutOfBoundsException er) // ignore framestart events
+                {}
+            }
+        }
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+        return in;
+    }
+    
+    boolean recordingFPN;
+    
+    public boolean getRecordingFPN()
+    {
+        return recordingFPN;
+    }
+    
+    public void setRecordingFPN(boolean recordingFPN)
+    {
+        this.recordingFPN=recordingFPN;
+    }
+ 
+    
+    public Object getFilterState() {
+        return FPNmap;
+    }
+    
+    void resetFPNmap(){
+        for(int i=0;i<4;i++)
+            for(int k=0;k<5;k++)
+                for(int l=0;l<2;l++)
+                    FPNmap[i][k][l]=1f;
+    }
+    
+    synchronized public void resetFilter() {
+        // set all FPNmap to max value so that any event is soon enough, guarenteed to be less than it
+        resetFPNmap();
+    }
+    
+    
+    public void update(Observable o, Object arg) {
+//        if(!isFilterEnabled()) return;
+        initFilter();
+    }
+    
+    public void initFilter() {
+        allocateMaps(chip);
+    }
+    
+    
+}
