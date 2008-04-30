9969d8acdea7e9d6282929a6139310632419dd15
Christian Brändli <chbraen@ini.uzh.ch>
2008-04-30 13:52:03 +0000

    alles unnötige chli usebutzt ... guets grundgrüscht zum starte
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@536 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/ch/unizh/ini/tobi/rccar/FancyDriver.java
+++ src/ch/unizh/ini/tobi/rccar/FancyDriver.java
@@ -1,14 +1,3 @@
-/*
- * FancyDriver.java
- *
- * Created on February 27, 2007, 9:51 PM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- *
- *
- *
- */
 
 package ch.unizh.ini.tobi.rccar;
 
@@ -40,24 +29,7 @@ import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.glu.*;
 
 /**
- * Drives the RC car using the output from an enclosed line detector filter.
- The enclosed filter chain is a MultiLineClusterTracker which itself encloses
- a chain of XYTypeFilter - BackgroundActivityFilter - OnOffProximityFilter - SimpleOrientationFilter.
- <p>
- Preference values for these enclosed filters are stored by keys based on the enclosing FancyDriver filter.
- <p>
- FancyDriver uses the detected line to control steering and speed to drive along the line.
- The line is parameritized by its normal form (rho, theta), where rho is the angle of the normal to the
- line from the center of the chip image and theta is the length of the normal vector (closest passge to
- the origin at the chip image center). The angle rho is 0 or 180 for a vertical line and is 90 or 270
- for a horizontal line.
- <p>
- (rho,theta) control the steering
- <p>
- Fires PropertyChangeEvent for the following
- <ul>
- <li> "sendControlToBlenderEnabled" - when the control signaling to blender is enabled
- </ul>
+ * The FancyDriver controls the RcCar 
 
  
  * @author 
@@ -72,11 +44,7 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
             return "Drives an RC car using retina spikes";
         }
 
-        private SimpleOrientationFilter oriFilter;
-        private OnOffProximityLineFilter lineFilter;
-        private BackgroundActivityFilter backgroundFilter;
-        private XYTypeFilter xyTypeFilter;
-        private RotateFilter rotateFilter;
+        private HingeLineTracker hingeLineTracker;
         FilterChain filterChain;
         
         public DriverPreFilter(AEChip chip){
@@ -87,35 +55,17 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
             // filters in the filter chain
             
             filterChain=new FilterChain(chip);
-            xyTypeFilter=new XYTypeFilter(chip);
-            oriFilter=new SimpleOrientationFilter(chip);
-            backgroundFilter=new BackgroundActivityFilter(chip);
-            lineFilter=new OnOffProximityLineFilter(chip);
-            rotateFilter=new RotateFilter(chip);
             
-            xyTypeFilter.setEnclosed(true,this);
-            oriFilter.setEnclosed(true,this);
-            backgroundFilter.setEnclosed(true,this);
-            lineFilter.setEnclosed(true,this);
-            rotateFilter.setEnclosed(true,this);
+            hingeLineTracker=new HingeLineTracker(chip);
             
-            xyTypeFilter.getPropertyChangeSupport().addPropertyChangeListener("filterEnabled",this);
-            oriFilter.getPropertyChangeSupport().addPropertyChangeListener("filterEnabled",this);
-            backgroundFilter.getPropertyChangeSupport().addPropertyChangeListener("filterEnabled",this);
-            lineFilter.getPropertyChangeSupport().addPropertyChangeListener("filterEnabled",this);
-            rotateFilter.getPropertyChangeSupport().addPropertyChangeListener("filterEnabled",this);
+            hingeLineTracker.setEnclosed(true,this);
             
-            filterChain.add(rotateFilter);
-            filterChain.add(xyTypeFilter);
-            filterChain.add(backgroundFilter);
-            filterChain.add(lineFilter);
-            filterChain.add(oriFilter);
+            hingeLineTracker.getPropertyChangeSupport().addPropertyChangeListener("filterEnabled",this);
             
-            setEnclosedFilterEnabledAccordingToPref(rotateFilter,null);
-            setEnclosedFilterEnabledAccordingToPref(xyTypeFilter,null);
-            setEnclosedFilterEnabledAccordingToPref(oriFilter,null);
-            setEnclosedFilterEnabledAccordingToPref(backgroundFilter,null);
-            setEnclosedFilterEnabledAccordingToPref(lineFilter,null);
+            filterChain.add(hingeLineTracker);
+            
+            setEnclosedFilterEnabledAccordingToPref(hingeLineTracker,null);
+            
             setEnclosedFilterChain(filterChain);
         }
         
@@ -175,30 +125,6 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
         }
     }
     
-    static Logger log=Logger.getLogger("Driver");
-    private SiLabsC8051F320_USBIO_CarServoController servo;
-    
-    private LowpassFilter steeringFilter=new LowpassFilter(); // steering command filter
-    private LowpassFilter steerAngleFilter=new LowpassFilter(); // steer angle filter, same as above but yulia's
-    private float offsetGain=getPrefs().getFloat("Driver.offsetGain",0.005f);
-    
-    /** Maximum constant speed of car under automatic driving via FancyDriver property. Limited for safety. */
-    public static final float MAX_SPEED=0.05f;
-    
-    {setPropertyTooltip("offsetGain","gain for moving back to the line");}
-    private float angleGain=getPrefs().getFloat("Driver.angleGain",0.5f);
-    {setPropertyTooltip("angleGain","gain for aligning with the line");}
-//    private float lpCornerFreqHz=getPrefs().getFloat("FancyDriver.lpCornerFreqHz",1);
-//    {setPropertyTooltip("lpCornerFreqHz","corner freq in Hz for steering control");}
-    private EventFilter2D lineTracker;
-    private MultiLineClusterTracker multiLineTracker;
-    private float steerInstantaneous=0.5f; // instantaneous value, before filtering
-    private float steerCommand=0.5f; // actual command, as modified by filtering
-    private float speed;
-    private int sizex;
-    private float radioSteer=0.5f, radioSpeed=0.5f;
-//    private float speedGain=getPrefs().getFloat("FancyDriver.speedGain",1);
-//    {setPropertyTooltip("speedGain","gain for reducing steering with speed");}
     private boolean flipSteering=getPrefs().getBoolean("Driver.flipSteering",true);
     {setPropertyTooltip("flipSteering","flips the steering command for use with mirrored scene");}
     
@@ -214,9 +140,20 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
     private boolean sendControlToBlenderEnabled=true;
     {setPropertyTooltip("sendControlToBlenderEnabled","sends steering (controlled) and speed (from radio) to albert's blender client");}
     
-    int lastt=0;
-    DrivingController controller;
+    private float offsetGain=getPrefs().getFloat("Driver.offsetGain",0.005f);
+    {setPropertyTooltip("offsetGain","gain for moving back to the line");}
     
+    private float angleGain=getPrefs().getFloat("Driver.angleGain",0.5f);
+    {setPropertyTooltip("angleGain","gain for aligning with the line");}
+    
+    DrivingController controller;
+    static Logger log=Logger.getLogger("FancyDriver");
+    private SiLabsC8051F320_USBIO_CarServoController servo;
+    private EventFilter2D lineTracker;
+    private float radioSteer=0.5f;
+    private float radioSpeed=0.5f;
+
+
     /** Creates a new instance of FancyDriver */
     public FancyDriver(AEChip chip) {
         super(chip);
@@ -234,48 +171,14 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
                 radioSteer=servo.getRadioSteer();
                 radioSpeed=servo.getRadioSpeed();
             }
-            sizex=getChip().getSizeX();// must do this here in case chip has changed
-            // compute instantaneous position of line according to hough line tracker (which has its own lowpass filter)
-            double rhoPixels=(float)((LineDetector)lineTracker).getRhoPixelsFiltered();
-            // distance of line from center of image, could be negative for example for line of 30 deg running up to lower right of origin
             
-            double thetaRad=(float)(Math.toRadians(((LineDetector)lineTracker).getThetaDegFiltered()));
             
-            // we want to control to theta=0, rho=0
-            // we change the cut to -pi/2 to pi/2 instead of 0,pi for better control around atractor now at theta=0.
-            // after this theta>0 for line angled to left, theta=0 for vertical line (not normal to line), theta<0 for line to right
-            if (thetaRad > Math.PI/2)
-                thetaRad -= Math.PI;
-            // same thing with rho, we want to control rho=0 so now rho>0 for line to right of centerline and rho<0 to left, instead
-            // of rho>0 above centerline. now rho won't flip sign under normal driving.
-            if (thetaRad < 0)
-                rhoPixels = -rhoPixels;
             
-            float deltaTMs = (in.getLastTimestamp() - lastt)/1000f; // time ms since last packet
-            lastt=in.getLastTimestamp();
             
-            // attractor set on line
-            steerInstantaneous =  steerInstantaneous + (float)(deltaTMs/tauDynMs*(-steerInstantaneous
-                    + 0.5f - offsetGain*rhoPixels + angleGain*thetaRad ));
-            if(steerInstantaneous<0 ){
-//                log.info("steerInstantaneous was reset from "+steerInstantaneous+" to 0");
-                steerInstantaneous=0;
-            }
-            if(steerInstantaneous>1){
-//                log.info("steerInstantaneous was reset from "+steerInstantaneous+" to 1");
-                steerInstantaneous=1;
-            }
             
-//            float speedFactor=(radioSpeed-0.5f)*speedGain; // is zero for halted, positive for fwd, negative for reverse
-//            if(speedFactor<0)
-//                speedFactor= 0;
-//            else if(speedFactor<0.1f) {
-//                speedFactor=10; // going slowly, limit factor
-//            }else
-//                speedFactor=1/speedFactor; // faster, then reduce steering more
-            steerCommand=steerInstantaneous;
+            
             if(servo!=null && servo.isOpen()){
-                servo.setSteering(getSteerCommand()); // 1 steer right, 0 steer left
+                servo.setSteering(0.5f); 
                 servo.setSpeed(getDefaultSpeed()+0.5f); // set fwd speed
             }
             // send controls over socket to blender
@@ -284,20 +187,9 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
         
     }
     
-    /** Applies the enclosed LineDetector filter to extract the lowpassed dominant line feature
-     in the scene, then computes steering and speed based on the filter output.
-     The driver only controls the car speed via the throttle control, but the speed is reduced automatically
-     by the controller according to the controller steering command (more steer = lower speed).
-     The instantaneous steering command is based on the horizontal position of the line in the scene; if the line
-     is to the right, steer left, and vice versa. In addition, the instantaneous steering command is lowpass filtered
-     to produce the actual steering command.
-     @param in the input packet
-     @return the output packet, which is the output of the enclosed filter chain.
-     */
     public EventPacket<?> filterPacket(EventPacket<?> in) {
         if(!isFilterEnabled()) return in;
         if(!isSendControlToBlenderEnabled()) checkServo(); // don't bother with servo if in simulation
-//        in=getEnclosedFilterChain().filterPacket(in);
         in=getEnclosedFilter().filterPacket(in);
         
         int n=in.getSize();
@@ -332,7 +224,6 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
     
     public void resetFilter() {
         getEnclosedFilter().resetFilter();
-        steerInstantaneous=0.5f; // reset steering to middle
     }
     
     synchronized public void initFilter() {
@@ -388,21 +279,8 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
             gl.glLineWidth(6f);
             gl.glBegin(GL.GL_LINES);
             {
-                gl.glVertex2f(0,0);
-                double a=2*(getSteerCommand()-0.5f); // -1 to 1
-                a=Math.atan(a);
-                float x=radius*(float)Math.sin(a);
-                float y=radius*(float)Math.cos(a);
-                gl.glVertex2f(x,y);
-                if(servo!=null && servo.isOpen()){
-                    gl.glColor3f(1,0,0);
-                    gl.glVertex2f(0,0);
-                    a=2*(radioSteer-0.5f); // -1 to 1
-                    a=Math.atan(a);
-                    x=radius*(float)Math.sin(a);
-                    y=radius*(float)Math.cos(a);
-                    gl.glVertex2f(x,y);
-                }
+                
+                
             }
             gl.glEnd();
         }
@@ -484,10 +362,6 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
     
     /** Gets the actual steering command based on flipSteering
      */
-    public float getSteerCommand() {
-        if(flipSteering) return 1-steerCommand;
-        return steerCommand;
-    }
     
     public boolean isUseMultiLineTracker() {
         return useMultiLineTracker;
@@ -541,7 +415,6 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
     }
     
     public void setDefaultSpeed(float defaultSpeed) {
-        if(defaultSpeed<0) defaultSpeed=0; else if(defaultSpeed>MAX_SPEED) defaultSpeed=MAX_SPEED;
         this.defaultSpeed = defaultSpeed;
         getPrefs().putFloat("Driver.defaultSpeed",defaultSpeed);
     }
@@ -563,7 +436,7 @@ public class FancyDriver extends EventFilter2D implements FrameAnnotater{
                 dos=new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
             }
             dos.writeFloat(2f); // header for albert
-            dos.writeFloat(getSteerCommand());
+            //dos.writeFloat(getSteerCommand());
             dos.writeFloat(radioSpeed);
             dos.flush();
 //            System.out.println("sent controls steer="+getSteerCommand());
