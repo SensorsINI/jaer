7623b23c79c2b2c4f485a04b80a2df1fcb12c884
ozydingo <ozydingo@users.sf.net>
2008-07-17 01:55:20 +0000

    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@743 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/org/ine/telluride/cochlea/MSO.java
+++ src/org/ine/telluride/cochlea/MSO.java
@@ -7,7 +7,8 @@ import ch.unizh.ini.caviar.chip.AEChip;
 import ch.unizh.ini.caviar.event.EventPacket;
 import ch.unizh.ini.caviar.event.TypedEvent;
 import ch.unizh.ini.caviar.eventprocessing.EventFilter2D;
-import org.ine.telluride.cochlea.ANFSpikeBuffer;
+//import org.ine.telluride.cochlea.ANFSpikeBuffer;
+
 /**
  * Extracts pitch from AE cochlea spike output.
  * 
@@ -15,35 +16,23 @@ import org.ine.telluride.cochlea.ANFSpikeBuffer;
  */
 public class MSO extends EventFilter2D {
     private static final int NUM_CHANS=32;
-    private int bufferSize=getPrefs().getInt("MSO.bufferSize", 25);
-    
-
-    {
-        setPropertyTooltip("bufferSize", "Number of spikes held in ITD buffer");
-    }
     private int binWidth=getPrefs().getInt("MSO.binWidth", 100);
-    
-
-    {
-        setPropertyTooltip("binWidth", "Bin width for ITD hisotgram");
-    }
+    {setPropertyTooltip("binWidth", "Bin width for ITD hisotgram");}
     private int numBins=getPrefs().getInt("MSO.numBins", 15);
-    
+    {setPropertyTooltip("numBins", "Total number of bins, centered about 0");}
 
-    {
-        setPropertyTooltip("numBins", "Total number of bins, centered about 0");
-    }
-    private int[][][] spikeBuffer=null;
-    private int[][] bufferIndex=null;
-    private boolean[][] bufferFull=null;
     private float[] ITDBuffer=null;
     private int[] ITDBins=null;
     private int[] ITDBinEdges=null;
-    private boolean[] includeChannelInITD=null;
+    private boolean[] includeChannelInITD = new boolean[NUM_CHANS];
     private int[][] delays=null;
-    ANFSpikeBuffer anfSpikeBuffer=null;
-    int chan, id, ii, jj, bin, count;
-
+    private ANFSpikeBuffer anf=null;
+    private int chan, id, ii, jj, bin, count;
+    private int spikeCount = 0;
+    private int bufferSize = 0;
+    private int[][][] spikeBuffer = null;
+    private boolean[][] bufferFull;
+    
     @Override
     public String getDescription() {
         return "Computes ITD of incoming binaural signal";
@@ -62,24 +51,13 @@ public class MSO extends EventFilter2D {
         }
         if(!checkSpikeBuffer()){
             throw new RuntimeException("Can't find prior ANFSpikeBuffer in filter chain");
-        };
+        }
         if(in==null) {
             return in;
         }
         for(Object o : in) {
             TypedEvent e=(TypedEvent) o;
-            chan=e.x&31;
-            id=e.x&32;
-            spikeBuffer[chan][id][bufferIndex[chan][id]]=e.timestamp;
-            bufferIndex[chan][id]++;
-            if(bufferIndex[chan][id]>bufferSize) {
-                bufferIndex[chan][id]=0;
-                if(!bufferFull[chan][id]) {
-                    bufferFull[chan][id]=true;
-                //would it be quicker to just write without checking?
-                }
-            }
-
+            spikeCount++;
         }
         //e.x, e.timestamp
         return in;
@@ -87,7 +65,7 @@ public class MSO extends EventFilter2D {
 
     public void computeITD() {
         //update MSO.ITDBuffer with current ITD state
-
+        
         //compute delays in buffers
         for(chan=0; chan<NUM_CHANS; chan++) {
             if(includeChannelInITD[chan]) {
@@ -126,42 +104,15 @@ public class MSO extends EventFilter2D {
 
     @Override
     public void resetFilter() {
-        //allcoate spike buffers
-        spikeBuffer=new int[NUM_CHANS][2][bufferSize];
-        bufferIndex=new int[NUM_CHANS][2];
-        bufferFull=new boolean[NUM_CHANS][2];
-        includeChannelInITD=new boolean[NUM_CHANS];
-        delays=new int[bufferSize][bufferSize];
-
-        //define ITD bins from parameters
-        ITDBuffer=new float[numBins];
-        ITDBinEdges=new int[numBins+1];
-        ITDBins=new int[numBins];
-
-        //initialize per-channel buffer values
-        for(chan=0; chan<=NUM_CHANS; chan++) {
-            for(id=0; id<2; id++) {
-                for(ii=0; ii<bufferSize; ii++) {
-                    spikeBuffer[chan][id][ii]=0;
-                }
-                bufferIndex[chan][id]=0;
-                bufferFull[chan][id]=false;
-            }
-            includeChannelInITD[chan]=true;
-        }
-
-        //allocate ITD buffer values
-        for(ii=0; ii<numBins; ii++) {
-            ITDBinEdges[ii]=(-numBins*binWidth)/2+ii*binWidth;
-            ITDBins[ii]=(-(numBins-1)*binWidth)/2+ii*binWidth;
-            ITDBuffer[ii]=0;
-        }
-        ITDBinEdges[numBins+1]=-ITDBinEdges[0];
-
+        binWidth = getPrefs().getInt("MSO.binWidth",100);
+        numBins = getPrefs().getInt("MSO.numBins", 15);
+        
+        allocateITDBuffers();
     }
 
     @Override
     public void initFilter() {
+        allocateITDBuffers();
     }
 
     public int getBufferSize() {
@@ -192,11 +143,40 @@ public class MSO extends EventFilter2D {
     }
 
     private boolean checkSpikeBuffer() {
-        if(anfSpikeBuffer==null) {
-            anfSpikeBuffer=(ANFSpikeBuffer) chip.getFilterChain().findFilter(ANFSpikeBuffer.class);
-            return anfSpikeBuffer!=null;
+        if(anf==null) {
+            anf=(ANFSpikeBuffer) chip.getFilterChain().findFilter(ANFSpikeBuffer.class);
+            bufferSize = anf.getBufferSize();
+            allocateSpikeBuffer();
+            
+            return anf!=null;
         } else {
-            return false;
+            return true;
         }
+    }
+    
+    private void allocateSpikeBuffer() {
+        spikeBuffer= new int[NUM_CHANS][2][bufferSize];
+        bufferFull = new boolean[NUM_CHANS][2];
+    }
+    
+    private void allocateITDBuffers() {
+        delays=new int[bufferSize][bufferSize];
+        ITDBuffer=new float[numBins];
+        ITDBinEdges=new int[numBins+1];
+        ITDBins=new int[numBins];
+
+        //initialize per-channel buffer values
+        for(chan=0; chan<=NUM_CHANS; chan++) {
+            includeChannelInITD[chan]=true;
+        }
+
+        //allocate ITD buffer values
+        for(ii=0; ii<numBins; ii++) {
+            ITDBinEdges[ii]=(-numBins*binWidth)/2+ii*binWidth;
+            ITDBins[ii]=(-(numBins-1)*binWidth)/2+ii*binWidth;
+            ITDBuffer[ii]=0;
+        }
+        ITDBinEdges[numBins+1]=-ITDBinEdges[0];
+
     }
 }
