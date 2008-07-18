e071fdb9392502805733c3ecc744ff69a0a811d8
ozydingo <ozydingo@users.sf.net>
2008-07-18 07:06:24 +0000

    Built startup command sequence to shut the damn roboquad up!
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@765 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/org/ine/telluride/cochlea/auditoryReflex.java
+++ src/org/ine/telluride/cochlea/auditoryReflex.java
@@ -31,19 +31,23 @@ public class auditoryReflex extends EventFilter2D implements FrameAnnotater {
     {setPropertyTooltip("drawOutput", "Enable drawing");}
     
     private RoboQuadCommands rCommands;
-    private static int SPIKE_UPDATE_INTERVAL = 1000;
+    private static int SPIKE_UPDATE_INTERVAL = 3000;
+    private static long COMMAND_UPDATE_INTERVAL = 700;
     private static int NUM_CHANS = 32;
     private float[] ITDState=null;
     private int numITDBins=0;
     private int[] ITDBins=null;
+    private int ITDBinWidth=0;
     private int spikeCount=0;
     private MSO mso=null;
-    private int ii, jj, bin, count;
-    private int curCommand=0, nextCommand;
-    private float direction;
+    private int ii, jj, bin, count, cmdCount;
+    private int cmd;
+    private float direction=0;
     private float scale;
     private WowWeeRSHardwareInterface hw;
-    private boolean shutUp = false;
+    private boolean startupCommandSequenceDone = false;
+    private int startupCommandCount = 0;
+    private long time, lastTime=0;
     
     @Override
     public String getDescription() {
@@ -77,41 +81,49 @@ public class auditoryReflex extends EventFilter2D implements FrameAnnotater {
     }
 
     public void determineBehavior() {
-        count=mso.getNumBins();
-        if (count != numITDBins) {
-            System.out.println("Bins changed!  Allocating new memory");
-            allocateITDState();
-        }
-        direction = computeDirection();
-        
-//        try {
+        time = System.currentTimeMillis();
+        if (time>lastTime+COMMAND_UPDATE_INTERVAL) {
+            lastTime = time;
             checkHardware();
             if(hw!=null) {
-                if (!shutUp) {
-                    hw.sendWowWeeCmd((short) rCommands.Volume_Down);
-                    hw.sendWowWeeCmd((short) rCommands.Volume_Down);
-                    hw.sendWowWeeCmd((short) rCommands.Volume_Down);
-                    shutUp = true;
+                if (!startupCommandSequenceDone) { //shut the damn thing up
+                    switch (startupCommandCount) {
+                        case 0:
+                            cmd = rCommands.Toggle_Sensors;
+                            break;
+                        case 1:
+                        case 2:
+                        case 3:
+                            cmd = rCommands.Volume_Down;
+                            break;
+                        default:
+                            startupCommandSequenceDone = true;
+                            System.out.println("Done startup command sequence.");
+                            /*
+                            cmd = rCommands.Toggle_Activity_Level_3;
+                            cmd = rCommands.Toggle_Awareness_3;
+                            cmd = rCommands.Toggle_Aggression_3;
+                             */
+                            
+                    }
+                } else { // normal behavior
+                    count=mso.getNumBins();
+                    if (count != numITDBins) {
+                        System.out.println("Bins changed!  Allocating new memory");
+                        allocateITDState();
+                    }
+                    direction = computeDirection();
+                    if (direction < 30)
+                        cmd = rCommands.Rotate_Counter_Clockwise;
+                    else if (direction > 30)
+                         cmd = rCommands.Rotate_Clockwise;
+                    else
+                         cmd = rCommands.Stop;
                 }
-                if (direction < 25)
-                    nextCommand = rCommands.Rotate_Counter_Clockwise;
-                else if (direction > 25)
-                     nextCommand = rCommands.Rotate_Clockwise;
-                else
-                    nextCommand = rCommands.Stop;
-                
-                if (nextCommand != curCommand) {
-                    System.out.println("New command - "+ HexString.toString((short) curCommand));
-                    curCommand = nextCommand;
-                    hw.sendWowWeeCmd((short)curCommand);
-                }
-            System.out.print(".");
-            System.out.flush();
+            hw.sendWowWeeCmd((short)cmd);
+            System.out.println("Send command - "+ HexString.toString((short) cmd));
             }
-//        } catch(Exception e) {
-//            log.warning(e.toString());
-//        }
-
+        }
 
         return;
     }
@@ -163,6 +175,7 @@ public class auditoryReflex extends EventFilter2D implements FrameAnnotater {
         ITDState = mso.getITDState();
         ITDBins = mso.getITDBins();
         numITDBins = mso.getNumBins();
+        ITDBinWidth = mso.getBinWidth();
         scale = 0;
         direction = 0;
         for (bin=0;bin<numITDBins;bin++) {
@@ -195,21 +208,16 @@ public class auditoryReflex extends EventFilter2D implements FrameAnnotater {
         gl.glBegin(gl.GL_LINES);
         gl.glColor3d(0.8, 0, 0);
         
-        gl.glVertex3f(direction+(float)(numITDBins/2),0,0);
-        gl.glVertex3f(direction+(float)(numITDBins/2),1,0);
+        gl.glVertex3f(direction/(float)ITDBinWidth+(float)(numITDBins/2),0,0);
+        gl.glVertex3f(direction/(float)ITDBinWidth+(float)(numITDBins/2),1,0);
         
-        gl.glColor3d(0, 0, 1);
-        gl.glVertex3f((float)(numITDBins/2),0,0);
-        gl.glVertex3f((float)(numITDBins/2),1,0);
-        
-        gl.glColor3d(0, 1, 1);
+        gl.glColor3d(0, 0, .6);
+        gl.glVertex3i(0,0,0);
         gl.glVertex3i(0,1,0);
-        gl.glVertex3i(0,2,0);
         
-        gl.glColor3d(1, 1, 0);
+        gl.glColor3d(0, 0, .6);
+        gl.glVertex3i(numITDBins,0,0);
         gl.glVertex3i(numITDBins,1,0);
-        gl.glVertex3i(numITDBins,2,0);
-        
         gl.glEnd();
             
         gl.glPopMatrix();
