85c639269f3d9f8fad8c26103b8843066cdcb6eb
Tobi Delbruck <tobi@ini.uzh.ch>
2008-10-26 11:39:47 +0000

    fixed layout for PotPanel to better pack the panel. fixed bug in preference key for preferred binary firmware file in CypressFX2.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@941 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/ch/unizh/ini/caviar/hardwareinterface/usb/CypressFX2.java
+++ src/ch/unizh/ini/caviar/hardwareinterface/usb/CypressFX2.java
@@ -62,7 +62,7 @@ public class CypressFX2 implements UsbIoErrorCodes, PnPNotifyInterface, AEMonito
 
     public static void setDefaultFirmwareBixFileForBlankDevice(String aDefaultFirmwareBixFileForBlankDevice) {
         defaultFirmwareBixFileForBlankDevice = aDefaultFirmwareBixFileForBlankDevice;
-        prefs.put(CypressFX2.defaultFirmwareBixFileForBlankDevice, defaultFirmwareBixFileForBlankDevice);
+        prefs.put("CypressFX2.defaultFirmwareBixFileForBlankDevice", defaultFirmwareBixFileForBlankDevice);
     }
     
     protected Logger log = Logger.getLogger("CypressFX2");
