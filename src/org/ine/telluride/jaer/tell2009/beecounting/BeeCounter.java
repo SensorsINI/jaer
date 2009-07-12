c759d6d747c08d7d9133aed9c50d1120a6453846
Tobi Delbruck <tobi@ini.uzh.ch>
2009-07-12 05:14:55 +0000

    initial working version of BeeCounter, needs testing for memory leaks and benchmarking
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@1554 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/org/ine/telluride/jaer/tell2009/beecounting/BeeCounter.java
+++ src/org/ine/telluride/jaer/tell2009/beecounting/BeeCounter.java
@@ -4,6 +4,7 @@
  */
 package org.ine.telluride.jaer.tell2009.beecounting;
 import com.sun.opengl.util.GLUT;
+import java.util.ArrayList;
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import net.sf.jaer.chip.AEChip;
@@ -27,30 +28,111 @@ public class BeeCounter extends RectangularClusterTracker{
         super(chip);
         setPropertyTooltip("BeeCounting","topLine","exit line as fraction of screen");
         setPropertyTooltip("BeeCounting","botLine","entrance line as fraction of screen");
+    }
+    private ArrayList<RectangularClusterTracker.Cluster> crossedBotUpwards = new ArrayList();
+    private ArrayList<RectangularClusterTracker.Cluster> crossedTopDownwards = new ArrayList();
+private ArrayList<Cluster> purgeList=new ArrayList();
+    /** Overrides RectangularClusterTracker.filterPacket to add functionality of marking clusters
+     * that cross one line and then the other. Depending on the order of crossing, nOut or nIn are incremented.
+     *
+     * @param in the input packet.
+     * @return the input packet, unmodified.
+     */
+    @Override
+    synchronized public EventPacket<?> filterPacket (EventPacket<?> in){
+        super.filterPacket(in);
+        final float by = botLine * chip.getSizeY(),  ty = topLine * chip.getSizeY();
+        for ( Cluster c:clusters ){
+            if ( !c.isVisible() ){
+                continue;
+            }
+            // check for cluster that just crossed bot line up - add this to list
 
+            if ( c.getLastPacketLocation().y < by && c.getLocation().y >= by ){
+                // just crossed bot line upwards
+                if ( !crossedBotUpwards.contains(c) ){
+                    crossedBotUpwards.add(c);
+                }
+            }
+
+            // check for cluster that crossed bot line down and is in top down list, this bee entered
+            if ( c.getLastPacketLocation().y > by && c.getLocation().y <= by ){
+                // crossed bot line downwards
+                if ( crossedTopDownwards.contains(c) ){ // if crossed top line down, then bee entered
+                    nIn++;
+                    crossedTopDownwards.remove(c);
+                }
+            }
+
+            // same for bee that just crossed top line down, add to down list
+            if ( c.getLastPacketLocation().y > ty && c.getLocation().y <=ty ){
+                // just crossed top line downwards
+                if ( !crossedTopDownwards.contains(c) ){
+                    crossedTopDownwards.add(c);
+                }
+            }
+
+            // if bee crossed top line upwards and is in bot line up list, then it exited hive
+            if ( c.getLastPacketLocation().y < ty && c.getLocation().y >= ty ){
+                // crossed top line upwards
+                if ( crossedBotUpwards.contains(c) ){
+                    nOut++;
+                    crossedBotUpwards.remove(c);
+                }
+            }
+        }
+        
+        //purge lists
+        purgeList.clear();
+        for ( Cluster cl:crossedBotUpwards ){
+            if ( !clusters.contains(cl) ){
+                purgeList.add(cl);
+            }
+        }
+        crossedBotUpwards.removeAll(purgeList);
+
+        purgeList.clear();
+        for ( Cluster cl:crossedTopDownwards ){
+            if ( !clusters.contains(cl) ){
+                purgeList.add(cl);
+            }
+        }
+        crossedTopDownwards.removeAll(purgeList);
+
+        return in;
+    }
+    private class CrossingStats{
+        Cluster cluster;
+
+        CrossingStats (Cluster c){
+            this.cluster = c;
+        }
     }
 
     @Override
-    public synchronized void resetFilter (){
+    synchronized public void resetFilter (){
         super.resetFilter();
         nOut = 0;
         nIn = 0;
+        crossedBotUpwards.clear();
+        crossedTopDownwards.clear();
     }
     private final GLUT glut = new GLUT();
 
     @Override
     public synchronized void annotate (GLAutoDrawable drawable){
-        if(!isFilterEnabled()) return;
+        if ( !isFilterEnabled() ){
+            return;
+        }
         super.annotate(drawable);
         final int sx = chip.getSizeX(),  sy = chip.getSizeY();
         final GL gl = drawable.getGL();
-        gl.glRasterPos2f(nIn,nIn);
         gl.glLineWidth(2f);
         gl.glColor3f(0,0,1);
         gl.glRasterPos3f(0,sy * getTopLine(),0);
-        glut.glutBitmapString(GLUT.BITMAP_HELVETICA_18,String.format("%d exited",nOut));
+        glut.glutBitmapString(GLUT.BITMAP_TIMES_ROMAN_24,String.format("%d exited",nOut));
         gl.glRasterPos3f(0,sy * getBotLine(),0);
-        glut.glutBitmapString(GLUT.BITMAP_HELVETICA_18,String.format("%d entered",nIn));
+        glut.glutBitmapString(GLUT.BITMAP_TIMES_ROMAN_24,String.format("%d entered",nIn));
         gl.glBegin(GL.GL_LINES);
         gl.glVertex2f(0,sy * getTopLine());
         gl.glVertex2f(sx,sy * getTopLine());
@@ -98,9 +180,4 @@ public class BeeCounter extends RectangularClusterTracker{
         this.botLine = botLine;
         getPrefs().putFloat("BeeCounter.botLine",botLine);
     }
-
-//    @Override
-//    public EventPacket filterPacket (EventPacket in){
-//        in=super.filterPacket(in);
-//    }
 }
