3bb106fd63c92f8e9f6607d01094e48f03de8e5c
Tobi Delbruck <tobi@ini.uzh.ch>
2009-07-16 20:32:52 +0000

    isihistogrammer and gender classifier added. these work with CochleaAMS1b and weights from nima
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@1575 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/org/ine/telluride/jaer/tell2009/CochleaGenderClassifier.java
@@ -0,0 +1,125 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ine.telluride.jaer.tell2009;
+import com.sun.opengl.util.GLUT;
+import com.sun.opengl.util.j2d.TextRenderer;
+import java.awt.*;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.geom.*;
+import javax.media.opengl.*;
+import javax.media.opengl.GLAutoDrawable;
+import net.sf.jaer.chip.AEChip;
+import net.sf.jaer.event.EventPacket;
+import net.sf.jaer.graphics.FrameAnnotater;
+/**
+ * Extends ISIHistogrammer to use ISI histograms for gender classification based on pre-learned histograms.
+ * @author tobi, shih-chii, nima, telluride 2009
+ *
+ * This is part of jAER
+<a href="http://jaer.wiki.sourceforge.net">jaer.wiki.sourceforge.net</a>,
+licensed under the LGPL (<a href="http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License">http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License</a>.
+ */
+public class CochleaGenderClassifier extends ISIHistogrammer implements FrameAnnotater{
+    private float threshold = getPrefs().getFloat("CochleaGenderClassifier.threshold",1f);
+    private enum Gender{
+        Male, Female, Unknown
+    };
+    private Gender gender = Gender.Unknown;
+    private static float[] weights = { -0.080072f,-0.137934f,-0.121719f,-0.037092f,0.055964f,
+        0.211480f,0.189628f,-0.007691f,0.123760f,0.233077f,0.213994f,0.103129f,
+        0.157239f,0.273423f,0.163444f,0.225852f,0.130565f,-0.029779f,
+        -0.054216f,-0.132276f,-0.131664f,-0.183801f,0.023918f,0.007003f,
+        -0.138904f,-0.235957f,-0.203452f,-0.196405f,-0.231883f,-0.186246f,-0.180011f,-0.108253f,-0.256415f,-0.247059f,-0.157670f,
+        -0.073598f,-0.108831f,-0.115181f,-0.141374f,-0.097205f,-0.093439f,-0.067143f,-0.030989f,-0.034771f,-0.023676f,-0.024185f,
+        -0.065150f,-0.023104f,-0.025864f,-0.019137f
+    };
+    private TextRenderer titleRenderer;
+    /** The area to draw the title. */
+    private Rectangle titleArea;
+
+    public CochleaGenderClassifier (AEChip chip){
+        super(chip);
+        setNBins(weights.length);
+        setMaxIsiUs(9000);
+        setMinIsiUs(3000);
+        titleRenderer = new TextRenderer(new Font("Helvetica",Font.PLAIN,48));
+        Rectangle2D bounds = titleRenderer.getBounds(Gender.Unknown.toString());
+        titleArea = new Rectangle((int)bounds.getWidth(),(int)bounds.getHeight());
+    }
+
+    @Override
+    public synchronized EventPacket<?> filterPacket (EventPacket<?> in){
+        super.filterPacket(in);
+        float dot = computeDotProduct();
+        if ( dot > threshold ){
+            gender = Gender.Male;
+//            System.out.println("MALE");
+        } else if ( dot < -threshold ){
+            gender = Gender.Female;
+//            System.out.println("FEMALE");
+        } else{
+            gender = Gender.Unknown;
+        }
+        return in;
+    }
+
+    private float computeDotProduct (){
+        int[] bins = getBins();
+        if ( bins.length != weights.length ){
+            log.warning("bins.length!=weights.length");
+            return 0;
+        }
+        float[] normBins = new float[ bins.length ];
+        int max = 0;
+        for ( int i:bins ){
+            if ( i > max ){
+                max = i;
+            }
+        }
+        for ( int i = 0 ; i < bins.length ; i++ ){
+            normBins[i] = (float)bins[i] / max;
+        }
+        float dot = 0;
+        for ( int i = 0 ; i < bins.length ; i++ ){
+            dot += normBins[i] * weights[i];
+        }
+        return dot;
+    }
+
+    /**
+     * @return the threshold
+     */
+    public float getThreshold (){
+        return threshold;
+    }
+
+    /**
+     * @param threshold the threshold to set
+     */
+    public void setThreshold (float threshold){
+        this.threshold = threshold;
+        getPrefs().putFloat("CochleaGenderClassifier.threshold",threshold);
+    }
+
+    public void annotate (float[][][] frame){
+    }
+
+    public void annotate (Graphics2D g){
+    }
+    private GLUT glut = new GLUT();
+
+    public void annotate (GLAutoDrawable drawable){
+        GL gl = drawable.getGL();
+//        gl.glColor3f(1,1,1);
+//        gl.glRasterPos3f(10,10,0);
+//        glut.glutBitmapString(GLUT.BITMAP_HELVETICA_18,gender.toString());
+        // title
+        titleRenderer.beginRendering(drawable.getWidth(),drawable.getHeight());
+        titleRenderer.setColor(Color.WHITE);
+        titleRenderer.draw(gender.toString(),titleArea.x,titleArea.y);
+        titleRenderer.endRendering();
+    }
+}
---
+++ src/org/ine/telluride/jaer/tell2009/ISIHistogrammer.java
@@ -0,0 +1,395 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ine.telluride.jaer.tell2009;
+import java.awt.*;
+import java.awt.Dimension;
+import java.util.Arrays;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JFrame;
+import net.sf.jaer.chip.AEChip;
+import net.sf.jaer.event.BasicEvent;
+import net.sf.jaer.event.EventPacket;
+import net.sf.jaer.eventprocessing.EventFilter2D;
+import net.sf.jaer.util.chart.Axis;
+import net.sf.jaer.util.chart.Category;
+import net.sf.jaer.util.chart.Series;
+import net.sf.jaer.util.chart.XYChart;
+/**
+ * Histograms ISIs along selected direction of chip event space.
+ * @author tobi
+ *
+ * This is part of jAER
+<a href="http://jaer.wiki.sourceforge.net">jaer.wiki.sourceforge.net</a>,
+licensed under the LGPL (<a href="http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License">http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License</a>.
+ */
+public class ISIHistogrammer extends EventFilter2D implements Observer{
+    public static String getDescription (){
+        return "Computes ISI (inter spike interval) histogram";
+    }
+
+    /**
+     * @return the bins
+     */
+    public int[] getBins (){
+        return bins;
+    }
+
+    /**
+     * @return the maxBin
+     */
+    public int getMaxBin (){
+        return maxBin;
+    }
+
+
+    public enum Direction{
+        XDirection, YDirection, XtimesYDirection;
+    };
+    private Direction direction = Direction.valueOf(getPrefs().get("ISIHistogrammer.direction",Direction.XtimesYDirection.toString()));
+    private int nBins = getPrefs().getInt("ISIHistogrammer.nBins",50);
+    private int maxIsiUs = getPrefs().getInt("ISIHistogrammer.maxIsiUs",10000);
+    private int minIsiUs = getPrefs().getInt("ISIHistogrammer.minIsiUs",3000);
+    private int[] bins = new int[ nBins ];
+    private int maxBin = 0;
+    int nChans = 1;
+    int[] lastTs = null;
+    final int MAX_COUNT = 10000000;
+    JFrame isiFrame = null;
+    int lastDecayTs = 0;
+    private float tauDecayMs = getPrefs().getFloat("ISIHistogrammer.tauDecayMs",40);
+
+    public ISIHistogrammer (AEChip chip){
+        super(chip);
+        chip.addObserver(this);
+        setPropertyTooltip("maxIsiUs","maximim ISI in us, larger ISI's go to last bin");
+        setPropertyTooltip("NBins","number of histogram bins");
+        setPropertyTooltip("direction","X to use x AE addresses, y for y addresses, XtimesY for x*y addresses");
+        setPropertyTooltip("tauDecayMs","histogram bins are decayed to zero with this time constant in ms");
+        setPropertyTooltip("logPlotEnabled","enable to plot log histogram counts, disable for linear scale");
+    }
+
+    @Override
+    synchronized public EventPacket<?> filterPacket (EventPacket<?> in){
+        if ( lastTs == null ){
+            resetBins();
+        }
+        for ( BasicEvent e:in ){
+            int ts = e.timestamp;
+            int ch;
+            switch ( direction ){
+                case XDirection:
+                    ch = e.x;
+                    break;
+                case YDirection:
+                    ch = e.y;
+                    break;
+                case XtimesYDirection:
+                    ch = e.x * e.y;
+                    break;
+                default:
+                    ch = 0;
+            }
+            int dt = ts - lastTs[ch];
+            lastTs[ch] = ts;
+
+            addIsi(dt);
+            decayHistogram(ts);
+        }
+        isiFrame.repaint();
+        return in;
+    }
+
+    synchronized public void resetBins (){
+        if ( nChans == 0 ){
+            return; // not yet
+        }
+        lastTs = new int[ nChans ];
+        if ( bins.length != nBins ){
+            bins = new int[ nBins ];
+        }
+        Arrays.fill(bins,0);
+        maxBin = 0;
+        if ( activitySeries != null ){
+            activitySeries.setCapacity(nBins);
+        }
+    }
+
+    private void rescaleBins (){
+        for ( int i = 0 ; i < nBins ; i++ ){
+            bins[i] = bins[i] >> 1;
+        }
+    }
+
+    private void addIsi (int isi){
+        if ( isi < minIsiUs ){
+            return;
+        }
+        if ( isi >= maxIsiUs ){
+            return;
+        }
+
+        int bin= (isi * nBins / maxIsiUs);
+
+        bins[bin]++;
+        if ( bins[bin] > getMaxBin() ){
+            maxBin = bins[bin];
+        }
+        if ( getMaxBin() > MAX_COUNT ){
+            rescaleBins();
+            maxBin = getMaxBin() >> 1;
+        }
+    }
+
+    public void doPrintBins (){
+        for ( int i:bins ){
+            System.out.print(String.format("%8d ",i));
+        }
+        System.out.println("");
+    }
+
+    
+    
+
+
+    @Override
+    public Object getFilterState (){
+        return null;
+    }
+
+    @Override
+    public void resetFilter (){
+        resetBins();
+    }
+
+    @Override
+    public void initFilter (){
+        resetBins();
+    }
+
+    public void update (Observable o,Object arg){
+        if ( arg.equals("sizeX") || arg.equals("sizeY") ){
+            switch ( direction ){
+                case XDirection:
+                    nChans = chip.getSizeX();
+                    break;
+                case YDirection:
+                    nChans = chip.getSizeY();
+                    break;
+                case XtimesYDirection:
+                    nChans = chip.getSizeX() * chip.getSizeY();
+            }
+            resetBins();
+        }
+    }
+
+    /**
+     * @return the direction
+     */
+    public Direction getDirection (){
+        return direction;
+    }
+
+    /**
+     * @param direction the direction to set
+     */
+    public void setDirection (Direction direction){
+        this.direction = direction;
+        getPrefs().put("ISIHistogrammer.direction",direction.toString());
+    }
+
+    /**
+     * @return the nBins
+     */
+    public int getNBins (){
+        return nBins;
+    }
+
+    /**
+     * @param nBins the nBins to set
+     */
+    public void setNBins (int nBins){
+        if ( nBins < 1 ){
+            nBins = 1;
+        }
+        this.nBins = nBins;
+        getPrefs().putInt("ISIHistogrammer.nBins",nBins);
+        resetBins();
+    }
+
+    /**
+     * @return the maxIsiUs
+     */
+    public int getMaxIsiUs (){
+        return maxIsiUs;
+    }
+
+    /**
+     * @param maxIsiUs the maxIsiUs to set
+     */
+    synchronized public void setMaxIsiUs (int maxIsiUs){
+        if ( maxIsiUs < minIsiUs ){
+            maxIsiUs = minIsiUs;
+        }
+        this.maxIsiUs = maxIsiUs;
+        getPrefs().putInt("ISIHistogrammer.maxIsiUs",maxIsiUs);
+        resetBins();
+        if(binAxis!=null) {
+           binAxis.setUnit(String.format("%d,%d us",minIsiUs,maxIsiUs));
+        }
+        if(isiFrame!=null) isiFrame.repaint();
+    }
+
+    /**
+     * @return the minIsiUs
+     */
+    public int getMinIsiUs (){
+        return minIsiUs;
+    }
+
+    /**
+     * @param minIsiUs the minIsiUs to set
+     */
+    public void setMinIsiUs (int minIsiUs){
+        if ( minIsiUs > maxIsiUs ){
+            minIsiUs = maxIsiUs;
+        }
+        this.minIsiUs = minIsiUs;
+        getPrefs().putInt("ISIHistogrammer.minIsiUs",minIsiUs);
+        resetBins();
+        if(binAxis!=null) {
+            binAxis.setUnit(String.format("%d,%d us",minIsiUs,maxIsiUs));
+        }
+        if(isiFrame!=null) isiFrame.repaint();
+    }
+
+
+    /**
+     * @return the tauDecayMs
+     */
+    public float getTauDecayMs (){
+        return tauDecayMs;
+    }
+
+    /**
+     * @param tauDecayMs the tauDecayMs to set
+     */
+    public void setTauDecayMs (float tauDecayMs){
+        this.tauDecayMs = tauDecayMs;
+        getPrefs().putFloat("ISIHistogrammer.tauDecayMs",tauDecayMs);
+    }
+
+    @Override
+    public synchronized void setFilterEnabled (boolean yes){
+        super.setFilterEnabled(yes);
+        setIsiDisplay(yes);
+    }
+
+    @Override
+    public synchronized void cleanup (){
+        super.cleanup();
+        setIsiDisplay(false);
+    }
+
+    public void decayHistogram (int timestamp){
+        if ( tauDecayMs > 0 && timestamp > lastDecayTs + tauDecayMs * 1000 / 10 ){
+            float decayconstant = (float)java.lang.Math.exp(-( timestamp - lastDecayTs ) / ( tauDecayMs * 1000 ));
+            for ( int i = 0 ; i < bins.length ; i++ ){
+                bins[i] = (int)( bins[i] * decayconstant );
+            }
+        }
+        lastDecayTs = timestamp;
+    }
+    public Series activitySeries;
+    private Axis binAxis;
+    private Axis activityAxis;
+    private Category activityCategory;
+    private XYChart chart;
+    private boolean logPlotEnabled = getPrefs().getBoolean("ISIHistogrammer.logPlotEnabled",false);
+
+    private void setIsiDisplay (boolean yes){
+        if ( !yes ){
+            if ( isiFrame != null ){
+                isiFrame.dispose();
+                isiFrame = null;
+            }
+        } else{
+            isiFrame = new JFrame("ISIs"){
+                @Override
+                synchronized public void paint (Graphics g){
+                    super.paint(g);
+                    try{
+                        if ( bins != null ){
+                            activitySeries.clear();
+
+                            //log.info("numbins="+myBins.numOfBins);
+                            for ( int i = 0 ; i < nBins ; i++ ){
+                                if ( isLogPlotEnabled() ){
+                                    activitySeries.add(i,(float)Math.log(bins[i]));
+                                } else{
+                                    activitySeries.add(i,bins[i]);
+                                }
+                            }
+
+                            binAxis.setMaximum(nBins);
+                            binAxis.setMinimum(0);
+                            if ( isLogPlotEnabled() ){
+                                activityAxis.setMaximum((float)Math.log(getMaxBin()));
+                            } else{
+                                activityAxis.setMaximum(getMaxBin());
+                            }
+                        } else{
+                            log.warning("bins==null");
+                        }
+                    } catch ( Exception e ){
+                        log.warning("while displaying bins chart caught " + e);
+                    }
+                }
+            };
+            isiFrame.setPreferredSize(new Dimension(200,100));
+            Container pane = isiFrame.getContentPane();
+            chart = new XYChart();
+            activitySeries = new Series(2,nBins);
+
+            binAxis = new Axis(0,nBins);
+            binAxis.setTitle("bin");
+           binAxis.setUnit(String.format("%d,%d us",minIsiUs,maxIsiUs));
+
+            activityAxis = new Axis(0,1); // will be normalized
+            activityAxis.setTitle("count");
+
+            activityCategory = new Category(activitySeries,new Axis[]{ binAxis,activityAxis });
+            activityCategory.setColor(new float[]{ 1f,1f,1f }); // white for visibility
+            activityCategory.setLineWidth(3f);
+
+            chart = new XYChart("ISIs");
+            chart.setBackground(Color.black);
+            chart.setForeground(Color.white);
+            chart.setGridEnabled(false);
+            chart.addCategory(activityCategory);
+
+            pane.setLayout(new BorderLayout());
+            pane.add(chart,BorderLayout.CENTER);
+
+            isiFrame.setVisible(yes);
+        }
+
+    }
+
+    /**
+     * @return the logPlotEnabled
+     */
+    public boolean isLogPlotEnabled (){
+        return logPlotEnabled;
+    }
+
+    /**
+     * @param logPlotEnabled the logPlotEnabled to set
+     */
+    public void setLogPlotEnabled (boolean logPlotEnabled){
+        this.logPlotEnabled = logPlotEnabled;
+        getPrefs().putBoolean("ISIHistogrammer.logPlotEnabled",logPlotEnabled);
+    }
+}
