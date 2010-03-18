e648a874f9671e6f0e784ee63a70f3c7de4a1458
Tobi Delbruck <tobi@ini.uzh.ch>
2010-03-18 10:19:27 +0000

    added javadoc about timestamp wrapping.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@1761 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/aemonitor/AEConstants.java
+++ src/net/sf/jaer/aemonitor/AEConstants.java
@@ -8,13 +8,18 @@
 
 package net.sf.jaer.aemonitor;
 
+import net.sf.jaer.eventio.AEFileInputStream;
+
 /**
  * Holds constants related to AE interface.
  * @author tobi
  */
 public class AEConstants {
     
-    /** The default timestamp tick value in microseconds */
+    /** The default timestamp tick value in microseconds.
+     * The timestamp is a signed 32-bit int value. If the tick is 1us, the timestamp wraps every int32 us, which is about 4295 seconds which is 71 minutes. Time is negative, then positive, then negative again.
+     @see AEFileInputStream
+     */
     public static final int TICK_DEFAULT_US=1;
     
 //    /** The most positive possible timestamp time in seconds */
