8b9ccffcde7219264f7cb97c4adc285b83026c6a
Tobi Delbruck <tobi@ini.uzh.ch>
2010-04-29 14:22:08 +0000

    added check for addresses outside range because ATIS at capocaccia makes y addresses outside real range, presume some bias on arbiter static logic not quite right.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@1844 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/at/ait/dss/sni/jaer/chip/atis/ATIS304.java
+++ src/at/ait/dss/sni/jaer/chip/atis/ATIS304.java
@@ -2,7 +2,7 @@ package at.ait.dss.sni.jaer.chip.atis;
 /*
 created 8.5.2009 for ARC ATIS chip in sardinia at capo caccio workshop on cognitive neuromorphic engineering.
 
-  * This is part of jAER
+ * This is part of jAER
 <a href="http://jaer.wiki.sourceforge.net">jaer.wiki.sourceforge.net</a>,
 licensed under the LGPL (<a href="http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License">http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License</a>.
 
@@ -14,20 +14,20 @@ import net.sf.jaer.event.EventPacket;
 import net.sf.jaer.event.OutputEventIterator;
 import net.sf.jaer.event.PolarityEvent;
 import net.sf.jaer.graphics.DisplayMethod;
-
 /**
- * ARC ATIS 320x240
- * @author tobi delbruck, martin lizzenberger
+ * ARC ATIS 304x240
+ * @author tobi delbruck, martin lizzenberger, chr posch
  *
  */
 public class ATIS304 extends AERetina{
-    public static String getDescription() {
+    public static String getDescription (){
         return "ATIS304 304x240 Asynchronous Time Based Image Sensor";
     }
-    public ATIS304(){
+
+    public ATIS304 (){
         setName("ATIS304");
         setSizeX(304);
-        setSizeY(240);
+        setSizeY(256);
         setNumCellTypes(2); // two are polarity and last is intensity
         setPixelHeightUm(30);
         setPixelWidthUm(30);
@@ -41,11 +41,10 @@ public class ATIS304 extends AERetina{
      *The bits in the raw data coming from the device are as follows.
      * <p>
      */
-    public class ATIS304xtractor extends RetinaExtractor {
+    public class ATIS304xtractor extends RetinaExtractor{
+        public static final int XMASK = 0x03fe, XSHIFT = 1, YMASK = 0x03fc00, YSHIFT = 10, POLARITY_MASK = 0x01;
 
-        public static final int XMASK = 0x03fe,  XSHIFT = 1,  YMASK = 0x03fc00,  YSHIFT = 10, POLARITY_MASK=0x01;
-
-        public ATIS304xtractor(ATIS304 chip) {
+        public ATIS304xtractor (ATIS304 chip){
             super(chip);
         }
 
@@ -54,37 +53,40 @@ public class ATIS304 extends AERetina{
          *@return out the processed events. these are partially processed in-place. empty packet is returned if null is supplied as in.
          */
         @Override
-        synchronized public EventPacket extractPacket(AEPacketRaw in) {
-            if (out == null) {
+        synchronized public EventPacket extractPacket (AEPacketRaw in){
+            if ( out == null ){
                 out = new EventPacket<PolarityEvent>(chip.getEventClass());
-            } else {
+            } else{
                 out.clear();
             }
-            if (in == null) {
+            if ( in == null ){
                 return out;
             }
             int n = in.getNumEvents(); //addresses.length;
 
             int skipBy = 1;
-            if (isSubSamplingEnabled()) {
-                while (n / skipBy > getSubsampleThresholdEventCount()) {
+            if ( isSubSamplingEnabled() ){
+                while ( n / skipBy > getSubsampleThresholdEventCount() ){
                     skipBy++;
                 }
             }
             int[] a = in.getAddresses();
             int[] timestamps = in.getTimestamps();
             OutputEventIterator outItr = out.outputIterator();
-            for (int i = 0; i < n; i += skipBy) { // bug here
+            for ( int i = 0 ; i < n ; i += skipBy ){ // bug here
                 int addr = a[i];
-                PolarityEvent e = (PolarityEvent) outItr.nextOutput();
-                e.timestamp = (timestamps[i]);
-                e.x = (short) (((addr & XMASK) >>> XSHIFT));
-                e.y = (short) ((addr & YMASK) >>> YSHIFT);
-                e.type = (byte) (addr & 1);
+                PolarityEvent e = (PolarityEvent)outItr.nextOutput();
+                e.timestamp = ( timestamps[i] );
+                e.x = (short)( ( ( addr & XMASK ) >>> XSHIFT ) );
+                e.y = (short)( ( addr & YMASK ) >>> YSHIFT );
+                e.type = (byte)( addr & 1 );
                 e.polarity = e.type == 1 ? PolarityEvent.Polarity.Off : PolarityEvent.Polarity.On;
+                if ( e.x < 0 || e.x >= sizeX || e.y < 0 || e.y >=sizeY ){
+                    log.warning(e.toString() + " is outside size of chip"); // TODO should remove for speed once problems with y address outside legal range are sorted out
+                }
+
             }
             return out;
         }
     }
-
 }
