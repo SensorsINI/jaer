19e7620c26a3405172fad10ef4f3eae0309a4809
Michael Pfeiffer <mitch.pfeiffer@gmail.com>
2010-07-02 14:01:40 +0000

    VirtualSlotCar: New class for storing curvatures from the track
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@2070 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/ch/unizh/ini/jaer/projects/virtualslotcar/UpcomingCurvature.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.unizh.ini.jaer.projects.virtualslotcar;
+
+/**
+ * Class which reports the upcoming curvature on the track.
+ * @author Michael Pfeiffer
+ */
+public class UpcomingCurvature {
+
+    /** Vector of upcoming curvatures */
+    private float curvature[];
+
+    /** Number of stored curvature objects */
+    private int numPoints;
+
+    public UpcomingCurvature(float[] curvature) {
+        if ((curvature != null) && (curvature.length > 0)) {
+            this.numPoints = curvature.length;
+            this.curvature = new float[numPoints];
+            for (int i=0; i<numPoints; i++) {
+                this.curvature[i] = curvature[i];
+            }
+        } else {
+            this.curvature = new float[1];
+            numPoints = 1;
+            this.curvature[0] = 0;
+        }
+    }
+
+    public UpcomingCurvature(float curvature) {
+        numPoints = 1;
+        this.curvature = new float[1];
+        this.curvature[0] = curvature;
+    }
+
+    /** Returns the next upcoming curvature */
+    public float getNextCurvature() {
+        return curvature[0];
+    }
+
+    /** Sets the next upcoming curvature */
+    public void setNextCurvature(float curvature) {
+        this.curvature[0] = curvature;
+    }
+
+    /** Returns the number of stored upcoming curvatures */
+    public int getNumPoints() {
+        return numPoints;
+    }
+
+    /** Sets a new array of curvatures */
+    public void setCurvature(float[] curvature) {
+        if ((curvature != null) && (curvature.length > 0)) {
+            numPoints = curvature.length;
+            for (int i=0; i<numPoints; i++) {
+                this.curvature[i] = curvature[i];
+            }
+        }
+    }
+
+    /** Returns the next upcoming curvatures */
+    public float[] getCurvature() {
+        return curvature;
+    }
+
+    /** Returns the curvature at a given index */
+    public float getCurvature(int idx) {
+        if ((idx >= 0) && (idx < numPoints))
+            return curvature[idx];
+        else
+            return 0.0f;
+    }
+
+    /** Sets the curvature at a given index */
+    public void setCurvature(int idx, float newCurvature) {
+        if ((idx >= 0) && (idx < numPoints))
+            curvature[idx] = newCurvature;
+    }
+}
