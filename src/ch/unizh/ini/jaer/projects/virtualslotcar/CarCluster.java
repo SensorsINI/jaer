0e26249d7eb7e426ffa46e3d6f5bcff1a7f480b8
Tobi Delbruck <tobi@ini.uzh.ch>
2010-07-29 05:07:24 +0000

    new interface defined for slot car project
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@2184 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/ch/unizh/ini/jaer/projects/virtualslotcar/CarCluster.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.unizh.ini.jaer.projects.virtualslotcar;
+
+import net.sf.jaer.eventprocessing.tracking.ClusterInterface;
+
+/**
+ * Interface for a tracked car.
+ * @author tobi
+ */
+public interface CarCluster extends ClusterInterface{
+
+    /**
+     * @return the segmentIdx
+     */
+    int getSegmentIdx();
+
+    /**
+     * @return the crashed
+     */
+    boolean isCrashed();
+
+    /**
+     * @param crashed the crashed to set
+     */
+    void setCrashed(boolean crashed);
+
+    /**
+     * @param segmentIdx the segmentIdx to set
+     */
+    void setSegmentIdx(int segmentIdx);
+
+}
