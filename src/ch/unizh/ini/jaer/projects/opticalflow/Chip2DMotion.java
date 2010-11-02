4cc93402c9f591bfa07000f773682a155fe06b1f
rthuerer <rthuerer@users.sf.net>
2010-11-02 16:50:47 +0000

    Forgot to commit this file
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@2303 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/ch/unizh/ini/jaer/projects/opticalflow/Chip2DMotion.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.unizh.ini.jaer.projects.opticalflow;
+
+import ch.unizh.ini.jaer.projects.opticalflow.usbinterface.MotionChipInterface;
+import net.sf.jaer.biasgen.VDAC.DAC;
+import net.sf.jaer.chip.Chip2D;
+
+/**
+ *
+ * @author reto
+ */
+public  class Chip2DMotion extends Chip2D {
+
+    public static float VDD;
+    public static int NUM_ROWS;
+    public static int NUM_COLUMNS;
+    public static int NUM_MOTION_PIXELS;
+    public static int NUM_CHANNELS;
+    public static DAC dac;
+    /** A "magic byte" marking the start of each frame */
+    public static final byte FRAME_START_MARKER = (byte)0xac;
+    
+
+    /** the data to get for the chip */
+    public int acquisitionMode;
+
+    /** can be used to hold reference to last motion data */
+    public MotionData lastMotionData=null;
+
+    public  Chip2DMotion() {
+
+    }
+
+    /** Converts 10 bits signed ADC output value to a float ranged 0-1.
+     * 0 represents most negative value, .5 is zero value, 1 is most positive value.
+     *@param value the 10 bit value.
+     *@return the float value, ranges from 0 to 1023/1024 inclusive.
+     */
+    public static float convert10bitToFloat(int value) {
+        //     See http://en.wikipedia.org/wiki/Twos_complement
+        if ((value & 512) != 0) {
+            value |= -1024; // Add the upper sign bits
+        }
+        float r = (value + 512) / 1023.0F; // value will range from -512 to 511 - add 512 to it to get 0-1023 and divide by 1023
+        return r;
+        //        return ((value & 0x03FF)/1023f);
+    }
+
+    public  boolean isBorder(int x, int y) {
+        return (x == 0) || (y == 0) || (x == (this.NUM_COLUMNS - 1)) || (y == (this.NUM_ROWS - 1));
+    }
+
+    /** returns the current acquisition mode - some combination of bits from MotionData
+    @return mode bits
+     */
+    public int getCaptureMode() {
+        return acquisitionMode;
+    }
+
+    public void setCaptureMode(int acquisitionMode) {
+        this.acquisitionMode = acquisitionMode;
+        if (hardwareInterface != null && hardwareInterface.isOpen()) {
+            ((MotionChipInterface) hardwareInterface).setCaptureMode(acquisitionMode);
+        }
+    }
+
+    public int convertVtoBitValue (int set_mV){
+        int bitvalue= (int)((4095*set_mV)/5000)&0xFF;
+        return bitvalue;
+    }
+
+}
