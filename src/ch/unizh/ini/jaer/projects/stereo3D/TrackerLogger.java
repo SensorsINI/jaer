2c2648097bfe1fcc296295ae655cf67e56730c10
paulrogister <paulrogister@users.sf.net>
2010-11-08 10:58:17 +0000

    adding TrackerLogger file for Binoclar3DTracker in stereo3D project
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@2310 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/ch/unizh/ini/jaer/projects/stereo3D/TrackerLogger.java
@@ -0,0 +1,82 @@
+/*
+ * TrackerLogger.java
+ * logs positions of trackers
+ * Created on December 7, 2009, 10:32 AM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package ch.unizh.ini.jaer.projects.stereo3D;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * logs positions of trackers
+ * @author rogister
+ */
+public class TrackerLogger {
+    
+    File logFile;
+    BufferedWriter logWriter;
+    boolean recordingStarted = false;
+    /** Creates a new instance of EventRaw */
+    public TrackerLogger() {
+        
+    }
+     
+
+    public void init( String filename ){
+
+        try {
+
+
+            logFile=new File(filename);
+            logWriter = new BufferedWriter(new FileWriter(logFile));
+
+            recordingStarted=true;
+
+
+        }catch(IOException e){
+
+            e.printStackTrace();
+        }
+    }
+
+     public void log( String s ){
+
+         if(recordingStarted&&logWriter!=null){
+             try {
+                 logWriter.write( s );
+             }catch(IOException e){
+
+                 e.printStackTrace();
+             }
+         }
+
+     }
+
+     public void close(  ){
+         recordingStarted = false;
+         if(logWriter!=null){
+             try {
+                 logWriter.close( );
+             }catch(IOException e){
+
+                 e.printStackTrace();
+             }
+             logWriter = null;
+         }
+
+
+     }
+
+   
+
+    public String toString(){
+        return "TrackerLogger";
+    }
+    
+}
