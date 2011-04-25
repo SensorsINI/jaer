22e80816f9f1c592fece96dc0e7b341cf017621c
Tobi Delbruck <tobi@ini.uzh.ch>
2011-04-25 13:59:06 +0000

    starting to render map of maze now
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@2633 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/ch/unizh/ini/jaer/projects/labyrinth/LabyrinthMap.java
+++ src/ch/unizh/ini/jaer/projects/labyrinth/LabyrinthMap.java
@@ -6,9 +6,16 @@ package ch.unizh.ini.jaer.projects.labyrinth;
 
 import com.kitfox.svg.*;
 import com.kitfox.svg.SVGUniverse;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.*;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import javax.media.opengl.*;
 import javax.media.opengl.GLAutoDrawable;
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
@@ -27,17 +34,22 @@ public class LabyrinthMap extends EventFilter2D implements FrameAnnotater {
     public static String getDescription() {
         return "Handles SVG maps of Labyrinth game";
     }
-    SVGUniverse svg = new SVGUniverse();
+    SVGUniverse svgUniverse = new SVGUniverse();
     SVGDiagram svgDiagram = null;
+    ArrayList<Ellipse2D.Float> holes=new ArrayList();
+    ArrayList<Line2D.Float> lines=new ArrayList();
+    GeneralPath path=null;
+    Rectangle2D.Float outline=null;
+    Rectangle2D bounds=null;
 
     public LabyrinthMap(AEChip chip) {
         super(chip);
         setPropertyTooltip("loadMap", "opens file dialog to select SVG file of map");
         File f = getLastFilePrefs();
         try {
-            loadMapFromFile(f);
+           loadMapFromFile(f);
         } catch (Exception ex) {
-            log.warning("couldn't load map information from file " + f + ", caught " + ex + "; you are missing the SVG file describing the Labyrinth maze");
+            log.warning("couldn't load map information from file " + f + ", caught " + ex + "; you are missing the SVG file describing the Labyrinth maze or it is corrupted");
         }
     }
 
@@ -97,8 +109,44 @@ public class LabyrinthMap extends EventFilter2D implements FrameAnnotater {
 
     private void loadMapFromFile(File file) throws MalformedURLException {
         log.info("loading map file "+file);
-        URI svgURI = svg.loadSVG(file.toURI().toURL());
-        svgDiagram = svg.getDiagram(svgURI);
+        svgUniverse.setVerbose(true);
+        URI svgURI = svgUniverse.loadSVG(file.toURI().toURL());
+
+        svgDiagram = svgUniverse.getDiagram(svgURI);
+        float width=svgDiagram.getWidth();
+        float height=svgDiagram.getHeight();
+        SVGRoot root=svgDiagram.getRoot();
+        loadChildren(root.getChildren(null));
+
+    }
+
+   private void loadChildren(List children) {
+        for(Object o:children){
+            if(o instanceof Circle){
+                Circle c=(Circle)o;
+                holes.add((Ellipse2D.Float)c.getShape());
+            }else if(o instanceof Line){
+                Line l=(Line)o;
+                lines.add((Line2D.Float)l.getShape());
+            }else if(o instanceof Polyline){
+                Polyline l=(Polyline)o;
+                l.getShape();
+//                lines.add(l.getShape());
+            }else if(o instanceof Rect){
+                Rect r=(Rect)o;
+                outline=(Rectangle2D.Float)r.getShape();
+                bounds=outline.getBounds2D();
+           }else if(o instanceof Path){
+                Path r=(Path)o;
+                path=(GeneralPath)r.getShape();
+            }else if(o instanceof List){
+                loadChildren((List)o);
+            }else if(o instanceof Group){
+                Group g=(Group)o;
+                List l=g.getChildren(null);
+                loadChildren(l);
+            }
+        }
     }
 
     private File getLastFilePrefs() {
@@ -111,5 +159,22 @@ public class LabyrinthMap extends EventFilter2D implements FrameAnnotater {
 
     @Override
     public void annotate(GLAutoDrawable drawable) {
+        GL gl=drawable.getGL();
+        gl.glPushMatrix();
+        gl.glColor4f(0,0,.2f,0.3f);
+        gl.glLineWidth(1);
+        // scale and translate to match bounds of all shapes
+        float s=(float)(chip.getSizeX()/bounds.getWidth());
+        gl.glScalef(s, s,s);
+        gl.glTranslated(-bounds.getMinX(), -bounds.getMinY(), 0);
+        gl.glBegin(GL.GL_LINES);
+        for(Line2D.Float l:lines){
+            gl.glVertex2f(l.x1,l.y1);
+            gl.glVertex2f(l.x2,l.y2);
+        }
+        gl.glEnd();
+        gl.glPopMatrix();
     }
+
+
 }
