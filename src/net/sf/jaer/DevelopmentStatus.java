45df1f91c4e132cdbccf4e5eb6121aae9d227fac
Tobi Delbruck <tobi@ini.uzh.ch>
2011-05-09 08:51:08 +0000

    new annotation type to tag development status of a class
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@2689 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/net/sf/jaer/DevelopmentStatus.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sf.jaer;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotates classes with development status, e.g. Stable, Experimental.
+ * Use it like this, just before a class declaration: 
+ * <pre>
+ * @DevelopmentStatus(DevelopmentStatus.Status.Experimental)
+ * public class FlickerSuppessor extends EventFilter2D implements FrameAnnotater {
+ * </pre>
+ * This annotation is used in constructing GUIs in class chooser dialogs and other places.
+ * @author tobi
+ */
+@Retention(RetentionPolicy.RUNTIME) // retain at runtime
+@Target(ElementType.TYPE) // can only annotate classes, not methods of fields
+public @interface DevelopmentStatus {
+    Status value();
+    public enum Status {Stable, Experimental}
+    
+}
+
+
