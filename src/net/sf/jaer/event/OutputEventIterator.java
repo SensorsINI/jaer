71b0a92cc301b5619298888ba5d08ecd11ee2cc3
Tobi Delbruck <tobi@ini.uzh.ch>
2012-11-06 14:26:53 +0000

    clarify javadoc for output event iterator
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@3557 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/event/OutputEventIterator.java
+++ src/net/sf/jaer/event/OutputEventIterator.java
@@ -16,8 +16,12 @@ package net.sf.jaer.event;
 public interface OutputEventIterator<T extends BasicEvent> {
 
     /** Provide the next output event instance, which then should be copied from the input event and then modified according to function of desired operation.
+     * If you want to add a new event to the output packet, call this method. <code>nextOutput</code> will return a 
+     * reference to a previously allocated event object (which might have been used
+     * earlier). You can then modify this event as you like, e.g. by copying the fields from another event, using  <code>copyFrom</code>.
      *
      * @return the next output event.
+     * @see net.sf.jaer.event.BasicEvent#copyFrom(net.sf.jaer.event.BasicEvent) 
      */
     public T nextOutput();
     
