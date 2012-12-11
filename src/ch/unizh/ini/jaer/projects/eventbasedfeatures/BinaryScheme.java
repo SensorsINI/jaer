d97c6f80be71018432f1bf602b37c408091ad68f
vgunjal <vgunjal@users.sf.net>
2012-12-11 10:42:46 +0000

    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@3610 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/ch/unizh/ini/jaer/projects/eventbasedfeatures/BinaryScheme.java
+++ src/ch/unizh/ini/jaer/projects/eventbasedfeatures/BinaryScheme.java
@@ -1,113 +1,92 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package ch.unizh.ini.jaer.projects.eventbasedfeatures;
-
-import java.awt.Point;
-import java.util.ArrayList;
-import javax.swing.JFrame;
-import net.sf.jaer.chip.AEChip;
-import net.sf.jaer.eventprocessing.FilterChain;
-import net.sf.jaer.graphics.ImageDisplay;
-
-/**
- *
- * @author Varad
- */
-public class BinaryScheme {
-    
-    AEChip chip;
-    public FilterChain filterchain;            
-    PixelBuffer pixelbuffer;
-    BinaryFeatureDetector bindetect;
-    
-    public double RelativeThreshold;
-    
-    public int sizex;
-    public int sizey;
-    
-    public float max ;
-    public float min ;
-    
-    public float[][] map;
-    public float[] grayvalue ;
-//    public ArrayList<Point> keypoints;
-    
-    ImageDisplay display;
-    JFrame featureFrame;
-    
-    public BinaryScheme( AEChip chip, BinaryFeatureDetector bindetect){
-        
-        this.chip = chip; 
-        
-        this.bindetect = bindetect;
-        this.pixelbuffer = bindetect.pixelbuffer;
-        this.filterchain = bindetect.filterchain;
-        this.display = bindetect.display;
-        this.featureFrame = bindetect.featureFrame;
-        
-        sizex = chip.getSizeX();
-        sizey = chip.getSizeY();
-        
-    }
-    
-    synchronized public void checkMaps(){
-        
-        if ( map == null || map.length != chip.getSizeX() || map[0].length != chip.getSizeY() )
-            resetMaps();        
-    }
-    
-    synchronized public void resetMaps(){
-        
-        int size = 128;
-        display.setImageSize(size, size); // set dimensions of image      
-        featureFrame.setVisible(true); // make the frame visible
-        
-        max = 1;
-        min = 0;
-        
-        sizex = chip.getSizeX();
-        sizey = chip.getSizeY();
-        
-        map = new float[sizex][sizey];   
-//        grayvalue = new float[3*sizex*sizey] ;
-//        keypoints = new ArrayList<Point>();
-    }
-    
-//    synchronized public void checkMax(float mapvalue){
-//                       
-//        if ( mapvalue > max ){
-//            max = mapvalue;            
-//        }                
+///*
+// * To change this template, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package ch.unizh.ini.jaer.projects.eventbasedfeatures;
+//
+//import java.awt.Point;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import javax.swing.JFrame;
+//import net.sf.jaer.chip.AEChip;
+//import net.sf.jaer.eventprocessing.FilterChain;
+//import net.sf.jaer.graphics.ImageDisplay;
+//
+///** Super class containing all required methods for implementing binary feature detection schemes
+// *  viz. FAST, FREAK etc.
+// * 
+// * @author Varad
+// */
+//public class BinaryScheme {
+//    
+//    AEChip chip;
+//    public FilterChain filterchain;            
+//    PixelBuffer pixelbuffer;
+//    BinaryFeatureDetector bindetect;
+//    
+//    
+//    public int sizex;
+//    public int sizey;
+//    
+//    public float[][] map;
+//    public float[] grayvalue ;
+//    public ArrayList<Point> keypointlist;
+//    
+////    ImageDisplay display;
+////    JFrame featureFrame;
+//    
+//    public BinaryScheme( AEChip chip, BinaryFeatureDetector bindetect){
+//        
+//        this.chip = chip; 
+//        
+//        this.bindetect = bindetect;
+//        this.pixelbuffer = bindetect.pixelbuffer;
+//        this.filterchain = bindetect.filterchain;
+////        this.display = bindetect.display;
+////        this.featureFrame = bindetect.featureFrame;
+//        
+//        sizex = chip.getSizeX();
+//        sizey = chip.getSizeY();
+//        
 //    }
 //    
-//    synchronized public void checkMin(float mapvalue){
+//    synchronized public void checkMaps(){
 //        
-//        if ( mapvalue < min ){
-//            min = mapvalue;            
-//        }
+//        if ( map == null || map.length != chip.getSizeX() || map[0].length != chip.getSizeY() )
+//            resetMaps();        
 //    }
-    
-    synchronized public void getFeatures(float[][] map){
-        
-    }
-    
-//    synchronized public void updateFeatures( ArrayList<Point> keypoints, double threshold){
+//    
+//    synchronized public void resetMaps(){        
+//
+//        sizex = chip.getSizeX();
+//        sizey = chip.getSizeY();
 //        
-//        for( int i = 0; i < keypoints.size(); i++){
-//            
-//            if(!( (map[keypoints.get(i).x][keypoints.get(i).y] >= (threshold*max)) && (map[keypoints.get(i).x][keypoints.get(i).y] <= max))){
-//                keypoints.remove(i);
-//            }
-//        }
+//        bindetect.kernel.resetMaps();
+//        map = bindetect.kernel.detectormap;
+//        grayvalue = bindetect.kernel.grayvalue;
+//        keypointlist = new ArrayList<Point>();
 //    }
-    
-    synchronized public void resetFilter() { 
-//        System.out.print(map[64][64]+"\n");
-        pixelbuffer.resetFilter();
-        display.clearImage();        
-        resetMaps();
-    }    
-    
-}
+//    
+//    
+//    synchronized public void getFeatures( int x, int y ){
+//        
+//    }
+//    
+////    synchronized public void updateFeatures( ArrayList<FASTPixel> keypoints){
+////        
+////        for( int i = 0; i < keypoints.size(); i++){
+////            
+////            if( !(keypoints.get(i).isCorner) ){
+////                keypoints.remove(i);
+////            }
+////        }
+////    }
+//    
+//    synchronized public void resetFilter() { 
+//        bindetect.kernel.resetFilter();
+////        pixelbuffer.resetFilter();
+////        display.clearImage();        
+//        resetMaps();
+//    }    
+//    
+//}
