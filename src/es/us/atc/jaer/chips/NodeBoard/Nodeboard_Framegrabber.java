736817c696705de638dcaae9c9bd10213c9bed6a
Christian Brändli <chbraen@ini.uzh.ch>
2013-01-07 09:26:11 +0000

    package name changed to the right name
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@3639 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/es/us/atc/jaer/chips/NodeBoard/Nodeboard_Framegrabber.java
+++ src/es/us/atc/jaer/chips/NodeBoard/Nodeboard_Framegrabber.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package es.us.atc.Nodeboard.Framegrabber;
+package es.us.atc.jaer.chips.NodeBoard;
 
 import net.sf.jaer.chip.*;
 import net.sf.jaer.event.*;
