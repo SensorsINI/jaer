a04802a7e2c1c91d1f665057462624f03342a515
Tobi Delbruck <tobi@ini.uzh.ch>
2013-02-01 15:49:37 +0000

    added activity arrays
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@3701 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/eu/seebetter/ini/retinamodel/ApproachCell.java
+++ src/eu/seebetter/ini/retinamodel/ApproachCell.java
@@ -4,12 +4,18 @@
  */
 package eu.seebetter.ini.retinamodel;
 
+import java.util.Iterator;
+import java.util.Observable;
+import java.util.Observer;
 import javax.media.opengl.GLAutoDrawable;
 import net.sf.jaer.Description;
 import net.sf.jaer.DevelopmentStatus;
 import net.sf.jaer.chip.AEChip;
 import net.sf.jaer.event.EventPacket;
+import net.sf.jaer.event.PolarityEvent;
+import net.sf.jaer.event.PolarityEvent.Polarity;
 import net.sf.jaer.eventprocessing.EventFilter2D;
+import net.sf.jaer.eventprocessing.filter.SubSampler;
 import net.sf.jaer.graphics.FrameAnnotater;
 
 /**
@@ -38,16 +44,35 @@ The importance of the nonlinearity is that this system will
  */
 @Description("Models approach cell discovered by Botond Roska group")
 @DevelopmentStatus(DevelopmentStatus.Status.Experimental)
-public class ApproachCell extends EventFilter2D implements FrameAnnotater{
+public class ApproachCell extends EventFilter2D implements FrameAnnotater, Observer{
 
+    private boolean showSubunits=getBoolean("showSubunits", true);
+    private boolean showApproachCell=getBoolean("showApproachCell", true);
+    private int subunitSubsamplingBits=getInt("subunitSubsamplingBits",4);
+    
+    private SubSampler subSampler=new SubSampler(chip);
+    
+    private Subunits onSubunits, offSubunits;
+    
     public ApproachCell(AEChip chip) {
         super(chip);
+        chip.addObserver(this);
+        setPropertyTooltip("showSubunits", "Enables showing subunit activity annotation over retina output");
+        setPropertyTooltip("showApproachCell", "Enables showing approach cell activity annotation over retina output");
+        setPropertyTooltip("subunitSubsamplingBits", "Each subunit integrates events from 2^n by 2^n pixels, where n=subunitSubsamplingBits.");
+        
     }
 
     
     @Override
     public EventPacket<?> filterPacket(EventPacket<?> in) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(!(in.getEventPrototype() instanceof PolarityEvent)) return in;
+        for(Object o:in){
+            PolarityEvent e=(PolarityEvent)o;
+            offSubunits.update(e);
+            onSubunits.update(e);
+        }
+        return in;
     }
 
     @Override
@@ -56,10 +81,32 @@ public class ApproachCell extends EventFilter2D implements FrameAnnotater{
 
     @Override
     public void initFilter() {
+        onSubunits=new Subunits();
+        offSubunits=new Subunits();
     }
 
     @Override
     public void annotate(GLAutoDrawable drawable) {
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (arg != null && (arg == AEChip.EVENT_SIZEX || arg == AEChip.EVENT_SIZEY) && chip.getNumPixels()>0) {
+            initFilter();
+        }
+    }
+
+    private class Subunits {
+
+        float[][] activity=new float[(chip.getSizeX()>>subunitSubsamplingBits)+1][(chip.getSizeY()>>subunitSubsamplingBits)+1];
+        
+        public Subunits() {
+            
+        }
+        public void update(PolarityEvent e){
+            int x=e.x>>subunitSubsamplingBits, y=e.y>>subunitSubsamplingBits;
+            activity[x][y]=activity[x][y]+(e.polarity==Polarity.Off?-1:+1);
+        }
     }
     
 }
