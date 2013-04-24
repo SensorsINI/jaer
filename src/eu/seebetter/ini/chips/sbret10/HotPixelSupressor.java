44287acb5804e8a609ea19dc4149ed3bfd72ca60
Tobi Delbruck <tobi@ini.uzh.ch>
2013-04-24 16:44:24 +0000

    improved filter interface but still not really working
    git-svn-id: https://svn.code.sf.net/p/jaer/code/trunk/host/java@3850 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/eu/seebetter/ini/chips/sbret10/HotPixelSupressor.java
+++ src/eu/seebetter/ini/chips/sbret10/HotPixelSupressor.java
@@ -4,9 +4,12 @@
  */
 package eu.seebetter.ini.chips.sbret10;
 
+import java.awt.Point;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
 import net.sf.jaer.Description;
 import net.sf.jaer.DevelopmentStatus;
 import net.sf.jaer.chip.AEChip;
@@ -15,6 +18,9 @@ import net.sf.jaer.event.BasicEvent;
 import net.sf.jaer.event.EventPacket;
 import net.sf.jaer.event.OutputEventIterator;
 import net.sf.jaer.eventprocessing.EventFilter2D;
+import net.sf.jaer.eventprocessing.FilterChain;
+import net.sf.jaer.eventprocessing.filter.EventRateEstimator;
+import net.sf.jaer.graphics.FrameAnnotater;
 
 /**
  * Cheaply suppresses hot pixels from DVS; ie pixels that continuously fire a
@@ -28,36 +34,50 @@ import net.sf.jaer.eventprocessing.EventFilter2D;
  *
  * @author tobi
  */
-@Description("Cheaply suppresses hot pixels from DVS; ie pixels that continuously fire a sustained stream of events.")
+@Description("Cheaply suppresses hot pixels from DVS; ie pixels that continuously fire events when when the visual input is idle.")
 @DevelopmentStatus(DevelopmentStatus.Status.Experimental)
-public class HotPixelSupressor extends EventFilter2D {
+public class HotPixelSupressor extends EventFilter2D implements FrameAnnotater{
 
     private int numHotPixels = getInt("numHotPixels", 30);
     private float baselineProbability = getFloat("baselineProbability", 1e-3f);
     private float thresholdEventRate=getFloat("thresholdEventRate",10e3f);
-    private HashSet<Integer> hotPixelMap = new HashSet();
-    Random r = new Random();
+    private HashSet<Integer> hotPixelSet = new HashSet();
+    private HashMap<Integer,Point> hotPixPointMap=new HashMap();
+    private boolean showHotPixels=getBoolean("showHotPixels", true);
+    
+    private Random r = new Random();
+    private EventRateEstimator eventRateEstimator;
 
     public HotPixelSupressor(AEChip chip) {
         super(chip);
+        setPropertyTooltip("numHotPixels", "maximum number of hot pixels");
+        setPropertyTooltip("baselineProbability","probability that an address is written to the hot pixel set");
+        setPropertyTooltip("thresholdEventRate","pixels are only stored to the hot pixel list when the event rate is below this threshold, \n so that normal activity of the sensor does not overwrite the hot pixels");
+        eventRateEstimator=new EventRateEstimator(chip);
+        setEnclosedFilterChain(new FilterChain(chip));
+        eventRateEstimator.setEnclosed(true, this);
+        getEnclosedFilterChain().add(eventRateEstimator);
     }
 
     @Override
     synchronized public EventPacket<?> filterPacket(EventPacket<?> in) {
         checkOutputPacketEventType(in);
         OutputEventIterator outItr=out.outputIterator();
+        float rate=eventRateEstimator.getFilteredEventRate();
         for (BasicEvent e : in) {
-            if (r.nextFloat() < getBaselineProbability()) {
-                hotPixelMap.add(e.address);
-                for (Integer i : hotPixelMap) {
-                    if (i != e.address && hotPixelMap.size() >= numHotPixels) {
-                        hotPixelMap.remove(i); // remove some other element, not known which
+            if (rate<thresholdEventRate && r.nextFloat() < getBaselineProbability()) {
+                hotPixelSet.add(e.address);
+                hotPixPointMap.put(e.address,new Point(e.x,e.y));
+                for (Integer i : hotPixelSet) {
+                    if (i != e.address && hotPixelSet.size() >= numHotPixels) {
+                        hotPixelSet.remove(i); // remove some other element, not known which
+                        hotPixPointMap.remove(i);
                         break;
                     }
                 }
             }
-            if (hotPixelMap.contains(e.address)) {
-                log.info("filtered out "+e);
+            if (hotPixelSet.contains(e.address)) {
+//                log.info("filtered out "+e);
                 continue;
             }
                 ApsDvsEvent a = (ApsDvsEvent) outItr.nextOutput();
@@ -69,7 +89,8 @@ public class HotPixelSupressor extends EventFilter2D {
 
     @Override
     synchronized public void resetFilter() {
-        hotPixelMap.clear();
+        hotPixelSet.clear();
+        hotPixPointMap.clear();
         
     }
 
@@ -105,5 +126,46 @@ public class HotPixelSupressor extends EventFilter2D {
     public void setBaselineProbability(float baselineProbability) {
         this.baselineProbability = baselineProbability;
         putFloat("baselineProbability",baselineProbability);
+    }
+
+    /**
+     * @return the thresholdEventRate
+     */
+    public float getThresholdEventRate() {
+        return thresholdEventRate;
+    }
+
+    /**
+     * @param thresholdEventRate the thresholdEventRate to set
+     */
+    public void setThresholdEventRate(float thresholdEventRate) {
+        this.thresholdEventRate = thresholdEventRate;
+        putFloat("thresholdEventRate",thresholdEventRate);
+    }
+
+    @Override
+    public void annotate(GLAutoDrawable drawable) {
+        if(!showHotPixels) return;
+        GL gl = drawable.getGL();
+        gl.glColor4f(1, 1, 1,0.2f);
+        gl.glLineWidth(1f);
+        for (Point p : hotPixPointMap.values()) {
+            gl.glRectf(p.x - 1, p.y - 1, p.x + 1, p.y + 1);
+        }
+    }
+
+    /**
+     * @return the showHotPixels
+     */
+    public boolean isShowHotPixels() {
+        return showHotPixels;
+    }
+
+    /**
+     * @param showHotPixels the showHotPixels to set
+     */
+    public void setShowHotPixels(boolean showHotPixels) {
+        this.showHotPixels = showHotPixels;
+        putBoolean("showHotPixels", showHotPixels);
     }
 }
