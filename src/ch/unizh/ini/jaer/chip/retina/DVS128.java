e20168fe4d6505132378bcf2411b46888e30d8bf
Tobi Delbruck <tobi@ini.uzh.ch>
2014-07-03 12:35:19 +0000

    fixed menu to show that timestamp master and external sync event INPUT are enabled by this option, not sync event OUTPUT
    git-svn-id: https://svn.code.sf.net/p/jaer/code/branches/jAER_1.5@4893 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/ch/unizh/ini/jaer/chip/retina/DVS128.java
+++ src/ch/unizh/ini/jaer/chip/retina/DVS128.java
@@ -255,7 +255,7 @@ public class DVS128 extends AETemporalConstastRetina implements Serializable, Ob
             }
 
             if (syncEnabledMenuItem == null) {
-                syncEnabledMenuItem = new JCheckBoxMenuItem("Timestamp master / Enable sync event output");
+                syncEnabledMenuItem = new JCheckBoxMenuItem("Timestamp master / Enable sync event input");
                 syncEnabledMenuItem.setToolTipText("<html>Sets this device as timestamp master and enables sync event generation on external IN pin falling edges (disables slave clock input).<br>Falling edges inject special sync events with bitmask " + HexString.toString(CypressFX2DVS128HardwareInterface.SYNC_EVENT_BITMASK) + " set<br>These events are not rendered but are logged and can be used to synchronize an external signal to the recorded data.<br>If you are only using one camera, enable this option.<br>If you want to synchronize two DVS128, disable this option in one of the cameras and connect the OUT pin of the master to the IN pin of the slave and also connect the two GND pins.");
                 HasSyncEventOutput h = (HasSyncEventOutput) getHardwareInterface();
 
