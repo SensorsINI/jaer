c821de620a035f69f8ef18b025aae492fa04b0c2
Tobi Delbruck <tobi@ini.uzh.ch>
2014-09-23 14:09:32 +0000

    changed interface name for capturing frames from adcEnabled to captureFramesEnabled to be consistent with captureEventsEnabled and captureImuEnabled.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/branches/jAER_1.5@5258 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/config/ApsDvsConfig.java
+++ src/net/sf/jaer/config/ApsDvsConfig.java
@@ -40,7 +40,54 @@ public interface ApsDvsConfig {
     abstract public void setExposureDelayMs(int us);
     abstract public int getExposureDelayMs();
     
+    /** Sets threshold number of events to trigger new image frame.
+     * 
+     * @param threshold number of events for frame capture trigger. 0 to disable.
+     */
     abstract public void setAutoShotEventThreshold(int threshold);
+     /** Gets threshold number of events to trigger new image frame.
+     * 
+     * @return threshold number of events for frame capture trigger. 0 to disable.
+     */
     abstract public int getAutoShotEventThreshold();
+
+    /** Determines if ADC is enabled to capture images from APS output.
+     * 
+     * @return true if enabled
+     */
+    abstract public boolean isCaptureFramesEnabled();
+    /** Sets whether ADC is enabled to capture images from APS output.
+     * 
+     * @param yes true to enable
+     */
+    abstract public void setCaptureFramesEnabled(boolean yes);
+    
+    /** Determines if IMU is enabled to capture inertial data.
+     * 
+     * @return true if enabled
+     */
+    abstract public boolean isImuEnabled();
+    /** Sets whether IMU is enabled to capture inertial data.
+     * 
+     * @param yes true to enable
+     */
+    abstract public void setImuEnabled(boolean yes);
+    
+    abstract public boolean isDisplayImu();
+    abstract public void setDisplayImu(boolean yes);
+
+    /** Sets whether AER monitor interface runs to capture DVS output from sensor
+     * 
+     * @param selected true to enable DVS event capture 
+     */
+    public void setCaptureEvents(boolean selected);
+
+    /** Checks whether AER monitor interface runs to capture DVS output from sensor
+     * 
+     * @return true if enabled
+     */
+    public boolean isCaptureEventsEnabled();
+    
+    
     
 }
