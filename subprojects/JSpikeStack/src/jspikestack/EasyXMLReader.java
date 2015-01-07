2da0bb8c502a91b94ee37e364b27c6555ce62648
Luca Longinotti <luca.longinotti@inilabs.com>
2015-01-07 14:50:30 +0000

    jAER: fix typo in error message.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@5815 b7f4320f-462c-0410-a916-d9f35bb82d52

--- subprojects/JSpikeStack/src/jspikestack/EasyXMLReader.java
+++ subprojects/JSpikeStack/src/jspikestack/EasyXMLReader.java
@@ -5,23 +5,20 @@
 package jspikestack;
 
 import java.io.File;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
 import javax.xml.bind.DatatypeConverter;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -31,9 +28,9 @@ import org.xml.sax.SAXException;
 /**
  * Get and parse an xml file.  Provides a wrapper around the default xml parsing
  * utility which I think is easier to use.
- * 
+ *
  * EasyXMLReader reader=new EasyXMLReader();
- * 
+ *
  * @author oconnorp
  */
 public class EasyXMLReader {
@@ -45,38 +42,42 @@ public class EasyXMLReader {
     public static File grabFile(String startPath)
     {
         try {
-            if (startPath==null)
-                return getfile(null);
-            else
-                return getfile(new File(startPath));
+            if (startPath==null) {
+				return getfile(null);
+			}
+			else {
+				return getfile(new File(startPath));
+			}
         } catch (FileNotFoundException ex) {
             Logger.getLogger(EasyXMLReader.class.getName()).log(Level.SEVERE, null, ex);
             return null;
         }
     }
-    
+
     public EasyXMLReader() throws Exception
     {   this(grabFile(null));
     }
-    
+
     public EasyXMLReader(File infile){
-        
-        
-        if (infile!=null && infile.isDirectory())
-            file=grabFile(infile.toString());
-        else if (infile!=null && infile.isFile())
-//        else if (infile!=null && !infile.isFile())
+
+
+        if ((infile!=null) && infile.isDirectory()) {
+			file=grabFile(infile.toString());
+		}
+		else if ((infile!=null) && infile.isFile()) {
+			//        else if (infile!=null && !infile.isFile())
             file=infile;
-        else{
-            System.out.println("The file you gave s neither a file not a directory");
+		}
+		else{
+            System.out.println("The file you gave is neither a file nor a directory");
             file=grabFile(null);
         }
-        
+
         if (file==null)
-        {   return;            
+        {   return;
         }
-        
-        
+
+
         try {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder;
@@ -99,15 +100,15 @@ public class EasyXMLReader {
             Logger.getLogger(EasyXMLReader.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     /** Return whether the system has a file */
     public boolean hasFile()
     {   return file!=null;
-        
+
     }
-    
+
     public File getFile()
-    {   return file;        
+    {   return file;
     }
 
     public EasyXMLReader(Node db) {
@@ -123,9 +124,9 @@ public class EasyXMLReader {
     }
 
     public int getNodeCount(String name)
-    {   return get(name).getLength();        
+    {   return get(name).getLength();
     }
-    
+
     public NodeList get(String name) {
         if (doc instanceof Element) {
             return ((Element) doc).getElementsByTagName(name);
@@ -177,29 +178,29 @@ public class EasyXMLReader {
 
         // Cast to float array
         float[] floatValues = new float[binArray.length / 4];
-        // Iterate until parse each float int 
+        // Iterate until parse each float int
         int floatIndex = 0;
-        for (int nextFloatPosition = 0; nextFloatPosition < binArray.length; nextFloatPosition += 4) { // Read in the bytes 
+        for (int nextFloatPosition = 0; nextFloatPosition < binArray.length; nextFloatPosition += 4) { // Read in the bytes
             char c1 = (char) binArray[nextFloatPosition + 3];
             char c2 = (char) binArray[nextFloatPosition + 2];
             char c3 = (char) binArray[nextFloatPosition + 1];
             char c4 = (char) binArray[nextFloatPosition + 0];
-            // Bitwise AND to make sure only first 2 bytes are included 
-            int b1 = (int) (c1 & 0xFF);
-            int b2 = (int) (c2 & 0xFF);
-            int b3 = (int) (c3 & 0xFF);
-            int b4 = (int) (c4 & 0xFF);
-            // Build the four-byte floating-point "single format" representation 
+            // Bitwise AND to make sure only first 2 bytes are included
+            int b1 = c1 & 0xFF;
+            int b2 = c2 & 0xFF;
+            int b3 = c3 & 0xFF;
+            int b4 = c4 & 0xFF;
+            // Build the four-byte floating-point "single format" representation
             int intBits = (b4) | (b3 << 8) | (b2 << 16) | (b1 << 24);
             floatValues[floatIndex] = Float.intBitsToFloat(intBits);
-            // Increment counter used to populate array 
+            // Increment counter used to populate array
             floatIndex++;
         }
 
         return floatValues;
     }
-    
-    // ===== File IO Functions =====    
+
+    // ===== File IO Functions =====
     static class FileChoice implements Runnable {
 
         File file;
@@ -216,18 +217,19 @@ public class EasyXMLReader {
             fc.setDialogTitle("Choose network weight XML file");
 
             fc.showOpenDialog(null);
-            file = fc.getSelectedFile();            
+            file = fc.getSelectedFile();
         }
     }
 
     static public File getfile(File startDir) throws FileNotFoundException {
 
         FileChoice fc = new FileChoice();
-        if (startDir!=null && startDir.isDirectory())
-            fc.startDir=startDir;
-        
+        if ((startDir!=null) && startDir.isDirectory()) {
+			fc.startDir=startDir;
+		}
+
         if (SwingUtilities.isEventDispatchThread()) {
-            fc.run();            
+            fc.run();
         } else {
             try {
                 SwingUtilities.invokeAndWait(fc);
@@ -238,5 +240,5 @@ public class EasyXMLReader {
         }
         return fc.file;
     }
-    
+
 }
