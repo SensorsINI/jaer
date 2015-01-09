36f6c8c0bb3e005bb4e331d6f165b924aa4c6791
Tobi Delbruck <tobi@ini.uzh.ch>
2015-01-09 09:26:10 +0000

    added class to hold target locations and am filling arraylist with locations, but this needs to be hashlist or something that can be indexed better for interframe times.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@5823 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/eu/visualize/ini/convnet/TargetLabeler.java
+++ src/eu/visualize/ini/convnet/TargetLabeler.java
@@ -5,24 +5,22 @@
  */
 package eu.visualize.ini.convnet;
 
+import eu.seebetter.ini.chips.ApsDvsChip;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import javax.media.opengl.GL;
+import java.util.ArrayList;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
-import javax.media.opengl.glu.GLU;
 import javax.media.opengl.glu.GLUquadric;
 import net.sf.jaer.Description;
 import net.sf.jaer.DevelopmentStatus;
 import net.sf.jaer.chip.AEChip;
 import net.sf.jaer.event.EventPacket;
-import net.sf.jaer.eventio.AEFileInputStream;
 import net.sf.jaer.eventio.AEInputStream;
 import net.sf.jaer.eventprocessing.EventFilter2DMouseAdaptor;
 import net.sf.jaer.graphics.ChipCanvas;
-import net.sf.jaer.graphics.FrameAnnotater;
 
 /**
  * Labels location of target using mouse GUI in recorded data for later
@@ -34,12 +32,13 @@ import net.sf.jaer.graphics.FrameAnnotater;
 @Description("Labels location of target using mouse GUI in recorded data for later supervised learning.")
 public class TargetLabeler extends EventFilter2DMouseAdaptor implements PropertyChangeListener {
 
-    private boolean mousePressed=false;
-    private Point mousePoint=new Point();
-    private ChipCanvas glCanvas=null;
-    final float labelRadius=5f;
-    private GLUquadric mouseQuad=null;
-    
+    private boolean mousePressed = false;
+    private Point mousePoint = new Point();
+    private ChipCanvas glCanvas = null;
+    final float labelRadius = 5f;
+    private GLUquadric mouseQuad = null;
+    TargetLocations targetLocations=new TargetLocations();
+
     private boolean propertyChangeListenerAdded = false;
 
     public TargetLabeler(AEChip chip) {
@@ -49,26 +48,34 @@ public class TargetLabeler extends EventFilter2DMouseAdaptor implements Property
 
     @Override
     public void mouseDragged(MouseEvent e) {
-       if(glCanvas!=null){
-            mousePoint.setLocation(glCanvas.getPixelFromMouseEvent(e));
+        Point p = (getMousePixel(e));
+        if (p != null) {
+            mousePoint.setLocation(p);
+        } else {
+            mousePoint = null;
         }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        mousePressed=false;
+        mousePressed = false;
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        mousePressed=true;
-            mousePoint.setLocation(getMousePixel(e));
+        mousePressed = true;
+        Point p = (getMousePixel(e));
+        if (p != null) {
+            mousePoint.setLocation(p);
+        } else {
+            mousePoint = null;
+        }
     }
 
     @Override
     public void annotate(GLAutoDrawable drawable) {
-        super.annotate(drawable); 
-        GL2 gl=drawable.getGL().getGL2();
+        super.annotate(drawable);
+        GL2 gl = drawable.getGL().getGL2();
 //        if(mousePoint!=null){
 //                gl.glTranslatef(mousePoint.x, mousePoint.y, 0f);
 //                gl.glColor4f(0, 1, 0, .5f);
@@ -86,10 +93,15 @@ public class TargetLabeler extends EventFilter2DMouseAdaptor implements Property
                 propertyChangeListenerAdded = true;
             }
         }
-        if(glCanvas==null){
-            if(chip.getCanvas()!=null) {
-                glCanvas=chip.getCanvas();
+        if (!in.isEmpty() && mousePoint != null && mousePressed) {
+            int ts = in.getFirstTimestamp();
+            Point p = mousePoint;
+            int frame = -1;
+            if (chip instanceof ApsDvsChip) {
+                frame = ((ApsDvsChip) chip).getFrameCount();
             }
+            TargetLocation location=new TargetLocation(ts,frame,p);
+            targetLocations.add(location);
         }
         return in;
     }
@@ -104,14 +116,32 @@ public class TargetLabeler extends EventFilter2DMouseAdaptor implements Property
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        switch(evt.getPropertyName()){
+        switch (evt.getPropertyName()) {
             case AEInputStream.EVENT_REWIND:
                 break;
             case AEInputStream.EVENT_POSITION:
                 break;
             case AEInputStream.EVENT_EOF:
-                
+
         }
+    }
+
+    private class TargetLocation {
+
+        int timestamp;
+        int frame;
+        Point location;
+
+        public TargetLocation(int timestamp, int frame, Point location) {
+            this.timestamp = timestamp;
+            this.frame = frame;
+            this.location = new Point(location);
+        }
+
+    }
+
+    private class TargetLocations extends ArrayList<TargetLocation> {
+
     }
 
 }
