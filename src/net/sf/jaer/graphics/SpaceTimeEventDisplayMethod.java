f6852b1d2d4a59ebfdfefbd86c9d363578c72694
Luca Longinotti <luca.longinotti@inilabs.com>
2015-01-27 11:51:06 +0000

    jAER: correctly filter out IMU samples in SpaceTime view. Also make it sensitive to displayEvents and displayFrames settings in configuration, to allow for better performance.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@5982 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/graphics/SpaceTimeEventDisplayMethod.java
+++ src/net/sf/jaer/graphics/SpaceTimeEventDisplayMethod.java
@@ -1,16 +1,15 @@
 /*
  * ChipRendererDisplayMethod.java
- *
+ * 
  * Created on May 4, 2006, 9:07 PM
- *
+ * 
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
- *
- *
- *Copyright May 4, 2006 Tobi Delbruck, Inst. of Neuroinformatics, UNI-ETH Zurich
+ * 
+ * 
+ * Copyright May 4, 2006 Tobi Delbruck, Inst. of Neuroinformatics, UNI-ETH Zurich
  */
 package net.sf.jaer.graphics;
-
 
 import java.util.Iterator;
 
@@ -21,6 +20,8 @@ import javax.media.opengl.glu.GLU;
 
 import net.sf.jaer.aemonitor.AEConstants;
 import net.sf.jaer.chip.Chip2D;
+import net.sf.jaer.config.ApsDvsConfig;
+import net.sf.jaer.event.ApsDvsEvent;
 import net.sf.jaer.event.ApsDvsEventPacket;
 import net.sf.jaer.event.BasicEvent;
 import net.sf.jaer.event.EventPacket;
@@ -28,38 +29,48 @@ import net.sf.jaer.util.EngineeringFormat;
 
 import com.jogamp.opengl.util.gl2.GLUT;
 
+import eu.seebetter.ini.chips.ApsDvsChip;
+import eu.seebetter.ini.chips.DAViS.IMUSample;
+
 /**
  * Displays events in space time
+ *
  * @author tobi
  */
 public class SpaceTimeEventDisplayMethod extends DisplayMethod implements DisplayMethod3D {
 
 	EngineeringFormat engFmt = new EngineeringFormat();
 
+	private ApsDvsConfig config;
+	private boolean displayEvents = true;
+	private boolean displayFrames = true;
+
 	/**
 	 * Creates a new instance of SpaceTimeEventDisplayMethod
 	 */
-	public SpaceTimeEventDisplayMethod(ChipCanvas chipCanvas) {
+	public SpaceTimeEventDisplayMethod(final ChipCanvas chipCanvas) {
 		super(chipCanvas);
 	}
+
 	boolean spikeListCreated = false;
 	int spikeList = 1;
 	GLUT glut = null;
 	GLU glu = null;
 	final boolean useCubeEnabled = true; // true is too false or uses GPU improperly
 
-	// TODO it doesn't make sense to redraw all data for each mouse drag transform change. All we should have to do is to change the projection matrix.
+	// TODO it doesn't make sense to redraw all data for each mouse drag transform change. All we should have to do is
+	// to change the projection matrix.
 
 	@Override
-	public void display(GLAutoDrawable drawable) {
-		//        GL2 gl=setupGL(drawable);
-		//        AEChipRenderer renderer = (AEChipRenderer) (getChipCanvas().getRenderer());
-		//        log.info("display");
-		Chip2D chip = getChipCanvas().getChip();
+	public void display(final GLAutoDrawable drawable) {
+		// GL2 gl=setupGL(drawable);
+		// AEChipRenderer renderer = (AEChipRenderer) (getChipCanvas().getRenderer());
+		// log.info("display");
+		final Chip2D chip = getChipCanvas().getChip();
 		if (glut == null) {
 			glut = new GLUT();
 		}
-		GL2 gl = drawable.getGL().getGL2();
+		final GL2 gl = drawable.getGL().getGL2();
 		if (gl == null) {
 			log.warning("null GL context - not displaying");
 			return;
@@ -76,8 +87,8 @@ public class SpaceTimeEventDisplayMethod extends DisplayMethod implements Displa
 					{
 						gl.glScalef(1, 1, 1); // tobi make it a unit cube so we can see it from the side
 						glut.glutSolidCube(1);
-						spikeListCreated=true;
-						//            gl.glRectf(.5f,.5f, .5f,.5f);
+						spikeListCreated = true;
+						// gl.glRectf(.5f,.5f, .5f,.5f);
 					}
 					gl.glEndList();
 				}
@@ -94,7 +105,7 @@ public class SpaceTimeEventDisplayMethod extends DisplayMethod implements Displa
 			gl.glLineWidth(.4f);
 
 			{
-				gl.glBegin(GL2.GL_LINES);
+				gl.glBegin(GL.GL_LINES);
 				gl.glVertex3f(0, 0, 0);
 				gl.glVertex3f(chip.getSizeX(), 0, 0);
 
@@ -106,42 +117,58 @@ public class SpaceTimeEventDisplayMethod extends DisplayMethod implements Displa
 				gl.glEnd();
 			}
 
-
 			// render events
 
-			//        AEPacket2D ae = renderer.getAe();
-			EventPacket packet = (EventPacket) chip.getLastData();
+			// AEPacket2D ae = renderer.getAe();
+			final EventPacket packet = (EventPacket) chip.getLastData();
 			if (packet == null) {
 				log.warning("null packet to render");
 				gl.glPopMatrix();
 				return;
 			}
-			int n = packet.getSize();
+			final int n = packet.getSize();
 			if (n == 0) {
 				gl.glPopMatrix();
 				return;
 			}
-			//        if(ae==null || ae.getNumEvents()==0) return;
-			//        int n = ae.getNumEvents();
-			int t0 = packet.getFirstTimestamp();
-			int dt = packet.getDurationUs() + 1;
-			//        int t0 = ae.getFirstTimestamp();
-			//        int dt = ae.getLastTimestamp()-t0+1;
+			// if(ae==null || ae.getNumEvents()==0) return;
+			// int n = ae.getNumEvents();
+			final int t0 = packet.getFirstTimestamp();
+			final int dt = packet.getDurationUs() + 1;
+			// int t0 = ae.getFirstTimestamp();
+			// int dt = ae.getLastTimestamp()-t0+1;
 			float z;
-			float zfac = chip.getMaxSize();
-			//            int count=0;
+			final float zfac = chip.getMaxSize();
+			// int count=0;
+
 			Iterator evItr = packet.iterator();
-			if(packet instanceof ApsDvsEventPacket){
-				ApsDvsEventPacket apsPacket = (ApsDvsEventPacket) packet;
+			if (packet instanceof ApsDvsEventPacket) {
+				final ApsDvsEventPacket apsPacket = (ApsDvsEventPacket) packet;
 				evItr = apsPacket.fullIterator();
+
+				if ((config == null) && (chip != null) && (chip instanceof ApsDvsChip)) {
+					config = (ApsDvsConfig) chip.getBiasgen();
+				}
+
+				if (config != null) {
+					displayEvents = config.isDisplayEvents();
+					displayFrames = config.isDisplayFrames();
+				}
 			}
 
-
 			while (evItr.hasNext()) {
-				BasicEvent ev = (BasicEvent) evItr.next();
-				//                if(count++%1000==0)log.info("1000 events");
-				//        for (int i = 0; i<n; i++){
-				//            EventXYType ev = ae.getEvent2D(i);
+				final BasicEvent ev = (BasicEvent) evItr.next();
+
+				// Check if event needs to be rendered (APS/DVS).
+				if (ev instanceof ApsDvsEvent) {
+					final ApsDvsEvent apsEv = (ApsDvsEvent) ev;
+
+					if ((!displayFrames && apsEv.isSampleEvent()) || (!displayEvents && apsEv.isDVSEvent())
+						|| ((apsEv instanceof IMUSample) && ((IMUSample) apsEv).imuSampleEvent)) {
+						continue;
+					}
+				}
+
 				z = (float) (ev.timestamp - t0) / dt; // z goes from 0 (oldest) to 1 (youngest)
 				{
 					gl.glPushMatrix();
@@ -151,7 +178,8 @@ public class SpaceTimeEventDisplayMethod extends DisplayMethod implements Displa
 					if (useCubeEnabled) {
 						gl.glTranslatef(ev.x, ev.y, z * zfac);
 						gl.glCallList(spikeList);
-					} else {
+					}
+					else {
 						gl.glTranslatef(0, 0, z * zfac);
 						gl.glRectf(ev.x - .5f, ev.y - .5f, ev.x + .5f, ev.y + .5f);
 					}
@@ -159,7 +187,7 @@ public class SpaceTimeEventDisplayMethod extends DisplayMethod implements Displa
 				}
 			}
 			// draw axes labels x,y,t. See tutorial at http://jerome.jouvie.free.fr/OpenGl/Tutorials/Tutorial18.php
-			int font = GLUT.BITMAP_HELVETICA_18;
+			final int font = GLUT.BITMAP_HELVETICA_18;
 			{
 				gl.glPushMatrix();
 				final int FS = 1; // distance in pixels of text from endZoom of axis
@@ -168,20 +196,20 @@ public class SpaceTimeEventDisplayMethod extends DisplayMethod implements Displa
 				gl.glRasterPos3f(0, chip.getSizeY() + FS, 0);
 				glut.glutBitmapString(font, "y=" + chip.getSizeY());
 				// label time end value
-				//            gl.glRasterPos3f(0, -2 , chip.getMaxSize() + FS);
-				//            glut.glutBitmapCharacter(font, '0');
+				// gl.glRasterPos3f(0, -2 , chip.getMaxSize() + FS);
+				// glut.glutBitmapCharacter(font, '0');
 				gl.glRasterPos3f(0, 0, chip.getMaxSize() + FS);
 				glut.glutBitmapString(font, "t=" + engFmt.format(dt * AEConstants.TICK_DEFAULT_US * 1e-6f) + "s");
 				gl.glPopMatrix();
 			}
 
-			//            log.info("done rendering");
+			// log.info("done rendering");
 			checkGLError(gl);
 			gl.glPopMatrix();
 		}
 	}
 
-	void checkGLError(GL2 gl) {
+	void checkGLError(final GL2 gl) {
 		int error = gl.glGetError();
 		int nerrors = 10;
 		while ((error != GL.GL_NO_ERROR) && (nerrors-- != 0)) {
@@ -192,9 +220,10 @@ public class SpaceTimeEventDisplayMethod extends DisplayMethod implements Displa
 			error = gl.glGetError();
 		}
 	}
+
 	protected float[] rgb = new float[3];
 
-	protected final void computeRGBFromZ(float z) {
+	protected final void computeRGBFromZ(final float z) {
 		rgb[0] = z;
 		rgb[1] = 1 - z;
 		rgb[2] = 0;
