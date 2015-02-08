afd255c5cd48006732896bdbbce39e22b801da77
Tobi Delbruck <tobi@ini.uzh.ch>
2015-02-08 19:38:20 +0000

    attempt to preserve IMU samples when filtering out APS samples, but not working yet.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@6045 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/eventprocessing/filter/ApsDvsEventFilter.java
+++ src/net/sf/jaer/eventprocessing/filter/ApsDvsEventFilter.java
@@ -4,6 +4,7 @@
  */
 package net.sf.jaer.eventprocessing.filter;
 
+import eu.seebetter.ini.chips.DAViS.IMUSample;
 import java.util.Iterator;
 
 import net.sf.jaer.Description;
@@ -17,42 +18,56 @@ import net.sf.jaer.eventprocessing.EventFilter2D;
 
 /**
  * Enables filtering out of either DVS or APS events from ApsDvsEventPacket
+ *
  * @author tobi
  */
-@Description("Enables filtering out of either DVS or APS events from ApsDvsEventPacket and also filtering out of transient events caused by frame capture")
+@Description("Enables filtering out of either DVS events, APS samples, or IMU samples from ApsDvsEventPacket, and also filtering out of transient events caused by global shutter closing")
 @DevelopmentStatus(DevelopmentStatus.Status.Stable)
 public class ApsDvsEventFilter extends EventFilter2D {
 
-    private boolean filterDVSEvents=getBoolean("filterDVSEvents",false);
-    private boolean filterAPSEvents=getBoolean("filterAPSEvents",false);
-    private boolean filterFrameTransientEvents=getBoolean("filterFrameTransientEvents",true);
-    private int filterFrameTransientEventsTimeUs=getInt("filterFrameTransientEventsTimeUs",1000);
-    private int lastFrameStartTimestamp=0;
-    
+    private boolean filterDVSEvents = getBoolean("filterDVSEvents", false);
+    private boolean filterAPSEvents = getBoolean("filterAPSEvents", false);
+    private boolean filterIMUSamples = getBoolean("filterIMUSamples", false);
+    private boolean filterFrameTransientEvents = getBoolean("filterFrameTransientEvents", false);
+    private int filterFrameTransientEventsTimeUs = getInt("filterFrameTransientEventsTimeUs", 2000);
+    private int lastFrameStartTimestamp = Integer.MIN_VALUE;
+
     public ApsDvsEventFilter(AEChip chip) {
         super(chip);
-        setPropertyTooltip("filterFrameTransientEventsTimeUs", "Events within this time in us after end-of-frame are filtered out");
+        setPropertyTooltip("filterFrameTransientEventsTimeUs", "Events within this time in us after end-of-frame are filtered out; used for filtering DVS events possibly tirggered by APS global shutter");
         setPropertyTooltip("filterFrameTransientEvents", "Filter out events caused by global shutter in DAVIS240b");
         setPropertyTooltip("filterAPSEvents", "Filter out APS intensity samples");
         setPropertyTooltip("filterDVSEvents", "Filter out DVS events");
+        setPropertyTooltip("filterIMUSamples", "Filter out IMU (intertial measurement unit) samples");
     }
 
     @Override
     public EventPacket<?> filterPacket(EventPacket<?> in) {
-        if(!(in instanceof ApsDvsEventPacket) || (!filterAPSEvents && !filterDVSEvents && !filterFrameTransientEvents)) return in;
-        ApsDvsEventPacket apsDvsEventPacket=(ApsDvsEventPacket)in;
-        Iterator fullIterator=apsDvsEventPacket.fullIterator();
+        if (!(in instanceof ApsDvsEventPacket) || (!filterAPSEvents && !filterDVSEvents && !filterFrameTransientEvents && !filterIMUSamples)) {
+            return in;
+        }
+        ApsDvsEventPacket apsDvsEventPacket = (ApsDvsEventPacket) in;
+        Iterator fullIterator = apsDvsEventPacket.fullIterator();
         checkOutputPacketEventType(in);
-        OutputEventIterator outItr=out.outputIterator();
+        OutputEventIterator outItr = out.outputIterator();
         while (fullIterator.hasNext()) {
             ApsDvsEvent event = (ApsDvsEvent) fullIterator.next();
             if (event.isEndOfFrame()) {
                 lastFrameStartTimestamp = event.getTimestamp();
             }
-            if(filterFrameTransientEvents && event.getTimestamp()-lastFrameStartTimestamp<filterFrameTransientEventsTimeUs){
+            if (filterFrameTransientEvents && lastFrameStartTimestamp != Integer.MIN_VALUE && event.getTimestamp() - lastFrameStartTimestamp < filterFrameTransientEventsTimeUs) {
                 continue;
             }
-            if ((filterAPSEvents && event.isSampleEvent()) || (filterDVSEvents && !event.isSampleEvent())) {
+            if (filterAPSEvents && event.isSampleEvent() && !((event instanceof IMUSample) && !(((IMUSample) event).imuSampleEvent)))  {
+                continue;
+            }
+            if (filterDVSEvents && !event.isSampleEvent()) {
+                continue;
+            }
+            if (filterIMUSamples && (event instanceof IMUSample) && ((IMUSample) event).imuSampleEvent) {
+                continue;
+            }
+            if (filterIMUSamples && event.isSpecial()) {
                 continue;
             }
             outItr.nextOutput().copyFrom(event);
@@ -62,12 +77,12 @@ public class ApsDvsEventFilter extends EventFilter2D {
 
     @Override
     public void resetFilter() {
-        
+        lastFrameStartTimestamp = Integer.MIN_VALUE;
     }
 
     @Override
     public void initFilter() {
-        
+
     }
 
     /**
@@ -82,7 +97,7 @@ public class ApsDvsEventFilter extends EventFilter2D {
      */
     public void setFilterDVSEvents(boolean filterDVSEvents) {
         this.filterDVSEvents = filterDVSEvents;
-        putBoolean("filterDVSEvents",filterDVSEvents);
+        putBoolean("filterDVSEvents", filterDVSEvents);
     }
 
     /**
@@ -97,7 +112,7 @@ public class ApsDvsEventFilter extends EventFilter2D {
      */
     public void setFilterAPSEvents(boolean filterAPSEvents) {
         this.filterAPSEvents = filterAPSEvents;
-        putBoolean("filterAPSEvents",filterAPSEvents);
+        putBoolean("filterAPSEvents", filterAPSEvents);
     }
 
     /**
@@ -112,7 +127,7 @@ public class ApsDvsEventFilter extends EventFilter2D {
      */
     public void setFilterFrameTransientEvents(boolean filterFrameTransientEvents) {
         this.filterFrameTransientEvents = filterFrameTransientEvents;
-        putBoolean("filterFrameTransientEvents",filterFrameTransientEvents);
+        putBoolean("filterFrameTransientEvents", filterFrameTransientEvents);
     }
 
     /**
@@ -123,11 +138,27 @@ public class ApsDvsEventFilter extends EventFilter2D {
     }
 
     /**
-     * @param filterFrameTransientEventsTimeUs the filterFrameTransientEventsTimeUs to set
+     * @param filterFrameTransientEventsTimeUs the
+     * filterFrameTransientEventsTimeUs to set
      */
     public void setFilterFrameTransientEventsTimeUs(int filterFrameTransientEventsTimeUs) {
         this.filterFrameTransientEventsTimeUs = filterFrameTransientEventsTimeUs;
-        putInt("filterFrameTransientEventsTimeUs",filterFrameTransientEventsTimeUs);
+        putInt("filterFrameTransientEventsTimeUs", filterFrameTransientEventsTimeUs);
     }
-    
+
+    /**
+     * @return the filterIMUSamples
+     */
+    public boolean isFilterIMUSamples() {
+        return filterIMUSamples;
+    }
+
+    /**
+     * @param filterIMUSamples the filterIMUSamples to set
+     */
+    public void setFilterIMUSamples(boolean filterIMUSamples) {
+        this.filterIMUSamples = filterIMUSamples;
+        putBoolean("filterIMUSamples", filterIMUSamples);
+    }
+
 }
