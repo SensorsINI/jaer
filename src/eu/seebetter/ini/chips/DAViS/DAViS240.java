abbf664fe34c857c4f04fcc67e32d252690a1d7f
Tobi Delbruck <tobi@ini.uzh.ch>
2015-02-24 12:15:54 +0000

    set line width of exposure histogram explicitly now, so it is not inherited from previous drawing call
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@6097 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/eu/seebetter/ini/chips/DAViS/DAViS240.java
+++ src/eu/seebetter/ini/chips/DAViS/DAViS240.java
@@ -630,6 +630,7 @@ public class DAViS240 extends ApsDvsChip implements RemoteControlled, Observer {
 				final GL2 gl = drawable.getGL().getGL2();
 				gl.glPushAttrib(GL.GL_COLOR_BUFFER_BIT);
 				gl.glColor3f(0, 0, 1);
+                                gl.glLineWidth(1f);
 				hist.draw(drawable, exposureRenderer, (sizeX / 2) - (size / 2), (sizeY / 2) + (size / 2), size, size);
 				gl.glPopAttrib();
 			}
