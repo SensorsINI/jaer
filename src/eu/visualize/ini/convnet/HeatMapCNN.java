3dcfcce7387a92921d3d5702a627320ad91179dd
Hongjie Liu <hongjie@ini.uzh.ch>
2015-03-28 18:24:02 +0000

    created heatmap cnn for CNN-tracking by detection
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@6314 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/eu/visualize/ini/convnet/HeatMapCNN.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.visualize.ini.convnet;
+
+import java.beans.PropertyChangeEvent;
+import javax.media.opengl.GLAutoDrawable;
+import net.sf.jaer.Description;
+import net.sf.jaer.DevelopmentStatus;
+import net.sf.jaer.chip.AEChip;
+import net.sf.jaer.event.EventPacket;
+import net.sf.jaer.graphics.AEFrameChipRenderer;
+
+/**
+ * Computes heat map by running CNN using ROI over the frame.
+ * @author hongjie
+ */
+@Description("Computes heat map by running CNN using ROI over the frame")
+@DevelopmentStatus(DevelopmentStatus.Status.InDevelopment)
+public class HeatMapCNN extends DavisDeepLearnCnnProcessor{
+
+    public HeatMapCNN(AEChip chip) {
+        super(chip);
+    }
+
+    @Override
+    public void annotate(GLAutoDrawable drawable) {
+        super.annotate(drawable); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        super.propertyChange(evt); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public synchronized EventPacket<?> filterPacket(EventPacket<?> in) {
+         if (!addedPropertyChangeListener) {
+            ((AEFrameChipRenderer) chip.getRenderer()).getSupport().addPropertyChangeListener(AEFrameChipRenderer.EVENT_NEW_FRAME_AVAILBLE, this);
+            addedPropertyChangeListener = true;
+        }
+         return in;
+   }
+    
+    
+    
+    
+}
