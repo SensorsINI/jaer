12e99e6b7dab582bf36ede459d4fa698545192e8
Tobi Delbruck <tobi@ini.uzh.ch>
2015-04-13 19:36:42 +0000

    more development, but not working; very mysterious mixing in of live camera data to the recorded output, which I cannot see where it comes from. I.e. you trigger a recording, and the output packet events come only from this recording, yet they now mix in the live input events. could be caused by reused event objects, but the events in the playback packet are totally separate from the input packet events.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@6417 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/eventprocessing/filter/Oscilloscope.java
+++ src/net/sf/jaer/eventprocessing/filter/Oscilloscope.java
@@ -5,6 +5,11 @@
  */
 package net.sf.jaer.eventprocessing.filter;
 
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.util.awt.TextRenderer;
+import java.awt.Color;
+import java.awt.Font;
 import java.util.Iterator;
 import java.util.Observable;
 import java.util.Observer;
@@ -15,23 +20,41 @@ import net.sf.jaer.event.BasicEvent;
 import net.sf.jaer.event.EventPacket;
 import net.sf.jaer.event.OutputEventIterator;
 import net.sf.jaer.eventprocessing.EventFilter2D;
+import net.sf.jaer.graphics.FrameAnnotater;
 
 /**
  * A real-time oscilloscope, which can play back selected time or event slices
  * during live or recorded playback.
  *
+ * It works a bit like a scope: It waits for a trigger, then starts recording
+ * events into a recording packet. During recording, live data is shown in real
+ * time.
+ *
+ * Once the recordingPacket is full, the playback goes to slow motion replay,
+ * where instead of returning the input packet, the recordingPacket is returned
+ * in small chunks, either in time or event number, until it is completely
+ * played out. Then the recording is rewound to start and played again.
+ *
+ * In the meantime, the filterPacket waits for another trigger.
+ *
+ * Triggers can be manual, via Trigger button, or by a special event, e.g. from
+ * external input pin, or periodic by event number or by time interval.
+ *
+ *
+ *
  * @author tobi
  */
-@Description("<html>A real-time oscilloscope, which can play back selected time or event slices during live or recorded playback.<p>Trigger input provide possibilites for synchronizing on special events")
+@Description("<html>A real-time oscilloscope, which can play back selected time or event slices during live or recorded playback in slow motion."
+        + "<p>Trigger input provide possibilites for synchronizing on special events.")
 @DevelopmentStatus(DevelopmentStatus.Status.InDevelopment)
-public class Oscilloscope extends EventFilter2D implements Observer {
+public class Oscilloscope extends EventFilter2D implements Observer, FrameAnnotater {
 
     public enum TriggerType {
 
         TimeInterval, EventInterval, Manual, SpecialEvent
     };
 
-    public enum BufferType {
+    public enum CaptureType {
 
         TimeInterval, EventNumber
     };
@@ -42,7 +65,7 @@ public class Oscilloscope extends EventFilter2D implements Observer {
     }
 
     protected TriggerType triggerType = TriggerType.valueOf(getString("triggerType", TriggerType.Manual.toString()));
-    protected BufferType bufferType = BufferType.valueOf(getString("bufferType", BufferType.EventNumber.toString()));
+    protected CaptureType captureType = CaptureType.valueOf(getString("captureType", CaptureType.EventNumber.toString()));
     private PlaybackType playbackType = PlaybackType.valueOf(getString("playbackType", PlaybackType.TimeInterval.toString()));
 
     protected int numberOfEventsToCapture = getInt("numberOfEventsToCapture", 10000);
@@ -54,15 +77,18 @@ public class Oscilloscope extends EventFilter2D implements Observer {
 
     private boolean manualTrigger = false;
     private int triggerTimestamp;
-    
-    private int lastPlaybackTimestamp=0;
-    private int lastPlaybackEventNumber=0;
 
-    private boolean sampling = false; // is new data being recorded?
+    private int lastPacketPlaybackEndTimestamp = 0;
+    private int lastPacketPlaybackEndEventNumber = 0;
+
+    private boolean triggered = false; // is new data being recorded?
     private boolean playing = false; // is recorded buffer being output still?
-    private EventPacket<BasicEvent> scopeOutputPacket;
+    private EventPacket<BasicEvent> recordingPacket, playbackPacket;
     private OutputEventIterator recordingIterator;
     private Iterator<BasicEvent> playingIterator;
+
+    private TextRenderer textRenderer = null;
+    private String statusText = null;
 
     public Oscilloscope(AEChip chip) {
         super(chip);
@@ -71,63 +97,136 @@ public class Oscilloscope extends EventFilter2D implements Observer {
 
     @Override
     synchronized public EventPacket<?> filterPacket(EventPacket<?> in) {
-        checkOutputPacketEventType(in);
-        if (sampling) {
-            sampling = !sampleEvents(in.inputIterator(), recordingIterator);
-        } else {
+        if (triggered) {
+//            System.out.println(" recording additional " + in.getSize() + " events to "+recordingIterator.toString());
+            triggered = recordEvents(in.inputIterator(), recordingIterator);
+            if (!triggered) {
+                playing = true;
+            }
+        } else if (!playing) {
             Iterator<?> inputIterator = in.inputIterator();
             while (inputIterator.hasNext()) {
                 BasicEvent e = (BasicEvent) inputIterator.next();
+                if (e.isSpecial()) {
+                    continue;
+                }
+                if (e.isFilteredOut()) {
+                    continue;
+                }
                 if (isTrigger(e)) {
                     triggerTimestamp = e.timestamp;
-                    recordingIterator = scopeOutputPacket.outputIterator();
-                    recordingIterator.writeToNextOutput(e);
-                    log.info("triggered on " + e.toString());
-                    sampling = !sampleEvents(inputIterator, recordingIterator);
+                    recordingIterator = recordingPacket.outputIterator(); // clears recording packet
+//                    log.info("triggered, recording first " + in.getSize() + " events to "+recordingIterator.toString());
+                    triggered = recordEvents(inputIterator, recordingIterator);
+                    if (!triggered) {
+                        playing = true;
+                        break;
+                    }
                 }
             }
         }
 
-        if (playingIterator == null) {
-            playingIterator = scopeOutputPacket.inputIterator();
-        }
-        if (!sampling) {
-            OutputEventIterator outItr = getOutputPacket().outputIterator();
-            while (playingIterator.hasNext()) {
+        if (playing) {
+            if(playbackPacket==null){
+                playbackPacket=new EventPacket<>(recordingPacket.getEventClass());
+            }
+            if (playingIterator == null) {
+                playingIterator = recordingPacket.inputIterator();
+            }
+            OutputEventIterator outItr = playbackPacket.outputIterator();
+            int nPlaybackEventsThisPacket = 0;
+            int startTimestamp = lastPacketPlaybackEndTimestamp;
+            boolean breakout = false;
+            while (playingIterator.hasNext() && !breakout) {
+                if (!playingIterator.hasNext()) {
+                    playingIterator = recordingPacket.inputIterator();
+                    break;
+                }
                 BasicEvent e = playingIterator.next();
+                if (e.isSpecial()) {
+                    continue;
+                }
+                if (e.isFilteredOut()) {
+                    continue;
+                }
                 BasicEvent oe = outItr.nextOutput();
                 oe.copyFrom(e);
-                oe.timestamp = triggerTimestamp + (int) (getPlaybackTimeScale() * (e.timestamp - triggerTimestamp));
+                nPlaybackEventsThisPacket++;
+
+//                oe.timestamp = triggerTimestamp + (int) (getPlaybackTimeScale() * (e.timestamp - triggerTimestamp));
+                switch (playbackType) {
+                    case EventNumber:
+                        if (nPlaybackEventsThisPacket >= playbackNumEvents) {
+                            breakout = true;
+                        }
+                        break;
+                    case TimeInterval:
+                        if (e.timestamp - startTimestamp > playbackTimeIntervalUs) {
+                            breakout = true;
+                        }
+                        break;
+                    default:
+                }
             }
-            return getOutputPacket();
+            statusText = String.format("playing %d events at %s", playbackPacket.getSize(), playingIterator.toString());
+            if (!playingIterator.hasNext()) {
+                playingIterator = recordingPacket.inputIterator();
+            }
+            lastPacketPlaybackEndTimestamp = getOutputPacket().getLastTimestamp();
+            return playbackPacket;
         } else {
-            return null;
+            if (triggered) {
+                statusText = String.format("triggered: %d events", recordingPacket.getSize());
+            } else {
+                statusText = String.format("live");
+            }
+            return in;
         }
 
     }
 
-    private boolean sampleEvents(Iterator<?> in, OutputEventIterator recordingIterator) {
+    /**
+     * Returns true if sampling (recording) should continue
+     *
+     * @param inItr iterator for incoming events
+     * @param recordingIterator iterator for recorded events
+     * @return true if sampling (recording) should continue
+     */
+    private boolean recordEvents(Iterator<?> inItr, OutputEventIterator recordingIterator) {
         boolean finishedSampling = false;
-        while (in.hasNext()) {
-            BasicEvent e = (BasicEvent) in.next();
+        while (inItr.hasNext()) {
+            BasicEvent e = (BasicEvent) inItr.next();
+            if (e.isSpecial()) {
+                continue;
+            }
+            if (e.isFilteredOut()) {
+                continue;
+            }
             recordingIterator.writeToNextOutput(e);
-            if (finishedSampling = finishedSampling(e)) {
-                log.info("finished sampling at " + e);
+            if (finishedSampling = isFinishedRecording(e)) {
                 break;
             }
         }
-        return finishedSampling;
+        return !finishedSampling;
     }
 
-    private boolean finishedSampling(BasicEvent e) {
-        switch (getBufferType()) {
+    /**
+     * Determines if recording is finished
+     *
+     * @param e
+     * @return true if finished
+     */
+    private boolean isFinishedRecording(BasicEvent e) {
+        switch (getCaptureType()) {
             case EventNumber:
-                if (scopeOutputPacket.getSize() > getNumberOfEventsToCapture()) {
+                if (recordingPacket.getSize() > getNumberOfEventsToCapture()) {
+                    log.info(getCaptureType().toString() + ": recorded " + recordingPacket.getSize() + " events");
                     return true;
                 }
                 break;
             case TimeInterval:
-                if (scopeOutputPacket.getLastTimestamp() > triggerTimestamp + getLengthOfTimeToCaptureUs()) {
+                if (recordingPacket.getLastTimestamp() > triggerTimestamp + getLengthOfTimeToCaptureUs()) {
+                    log.info(getCaptureType().toString() + ": recorded " + recordingPacket.getSize() + " events over " + recordingPacket.getDurationUs() + " us");
                     return true;
                 }
                 break;
@@ -149,7 +248,9 @@ public class Oscilloscope extends EventFilter2D implements Observer {
             case TimeInterval:
                 return false;
             case SpecialEvent:
-                if(e.isSpecial()) return true;
+                if (e.isSpecial()) {
+                    return true;
+                }
                 return false;
             default:
                 return false;
@@ -158,21 +259,36 @@ public class Oscilloscope extends EventFilter2D implements Observer {
 
     @Override
     synchronized public void resetFilter() {
-        scopeOutputPacket.clear();
-        sampling = false;
+        recordingPacket.clear();
+        triggered = false;
         playing = false;
     }
 
     @Override
     public void initFilter() {
-        this.scopeOutputPacket = new EventPacket<>(chip.getEventClass());
-        this.recordingIterator = this.scopeOutputPacket.outputIterator();
+        this.recordingPacket = new EventPacket<>(chip.getEventClass());
+        recordingPacket.allocate(numberOfEventsToCapture);
+        this.recordingIterator = this.recordingPacket.outputIterator();
     }
 
     @Override
     public void update(Observable o, Object arg) {
         if (o instanceof AEChip && arg == AEChip.EVENT_NUM_CELL_TYPES) { // lazy construction, after the actual AEChip subclass has been constructed
             initFilter();
+        }
+    }
+
+    @Override
+    synchronized public void annotate(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
+        if (textRenderer == null) {
+            textRenderer = new TextRenderer(new Font("SansSerif", Font.PLAIN, 10));
+        }
+        if (statusText != null) {
+            textRenderer.begin3DRendering();
+            textRenderer.setColor(Color.yellow);
+            textRenderer.draw3D(statusText, 0, 0, 0, .5f);
+            textRenderer.end3DRendering();
         }
     }
 
@@ -196,18 +312,18 @@ public class Oscilloscope extends EventFilter2D implements Observer {
     }
 
     /**
-     * @return the bufferType
+     * @return the captureType
      */
-    public BufferType getBufferType() {
-        return bufferType;
+    public CaptureType getCaptureType() {
+        return captureType;
     }
 
     /**
-     * @param bufferType the bufferType to set
+     * @param captureType the captureType to set
      */
-    public void setBufferType(BufferType bufferType) {
-        this.bufferType = bufferType;
-        putString("bufferType", bufferType.toString());
+    public void setCaptureType(CaptureType captureType) {
+        this.captureType = captureType;
+        putString("captureType", captureType.toString());
     }
 
     /**
@@ -223,6 +339,7 @@ public class Oscilloscope extends EventFilter2D implements Observer {
     public void setNumberOfEventsToCapture(int numberOfEventsToCapture) {
         this.numberOfEventsToCapture = numberOfEventsToCapture;
         putInt("numberOfEventsToCapture", numberOfEventsToCapture);
+        recordingPacket.allocate(numberOfEventsToCapture);
     }
 
     /**
@@ -297,7 +414,7 @@ public class Oscilloscope extends EventFilter2D implements Observer {
      */
     public void setPlaybackType(PlaybackType playbackType) {
         this.playbackType = playbackType;
-        putString("playbackType",playbackType.toString());
+        putString("playbackType", playbackType.toString());
     }
 
 }
