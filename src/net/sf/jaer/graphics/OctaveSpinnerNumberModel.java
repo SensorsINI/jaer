814100c6cfb3b1aab2207610944e499797128416
Luca Longinotti <luca.longinotti@inilabs.com>
2015-04-28 13:42:27 +0000

    jAER: fix compilation on older Macs.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@6523 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/graphics/OctaveSpinnerNumberModel.java
+++ src/net/sf/jaer/graphics/OctaveSpinnerNumberModel.java
@@ -1,26 +1,32 @@
 package net.sf.jaer.graphics;
+
 import javax.swing.SpinnerNumberModel;
 
-/** Spinner model with octave increments/decrements.
+/**
+ * Spinner model with octave increments/decrements.
  *
  * @author tobi
  */
-public class OctaveSpinnerNumberModel extends SpinnerNumberModel{
-    public OctaveSpinnerNumberModel (int value,int minimum,int maximum,int stepSize){
-        super(value,minimum,maximum,stepSize);
-    }
+public class OctaveSpinnerNumberModel extends SpinnerNumberModel {
+	public OctaveSpinnerNumberModel(int value, int minimum, int maximum, int stepSize) {
+		super(value, minimum, maximum, stepSize);
+	}
 
-    @Override
-    public Object getNextValue (){
-        int n= 2 * (Integer)getValue();
-        if(n>(Integer)getMaximum()) n=(Integer) getMaximum();
-        return Integer.valueOf(n);
-    }
+	@Override
+	public Object getNextValue() {
+		int n = 2 * (Integer) getValue();
+		if (n > (Integer) getMaximum()) {
+			n = (Integer) getMaximum();
+		}
+		return Integer.valueOf(n);
+	}
 
-    @Override
-    public Object getPreviousValue (){
-        int n=(Integer)getValue() / 2;
-        if(n<(Integer)getMinimum())n=(int)getMinimum();
-        return n;
-    }
+	@Override
+	public Object getPreviousValue() {
+		int n = (Integer) getValue() / 2;
+		if (n < (Integer) getMinimum()) {
+			n = (Integer) getMinimum();
+		}
+		return n;
+	}
 }
