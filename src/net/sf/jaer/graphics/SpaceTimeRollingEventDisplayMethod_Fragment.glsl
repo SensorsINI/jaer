4bcd6c4651250a73a4fb6e195232d9c9a21a4dc8
Tobi Delbruck <tobi@ini.uzh.ch>
2015-05-16 17:50:28 +0000

    depth rendering of events is now faded nicely to darkness for old events, and has BGR shading
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@6708 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/graphics/SpaceTimeRollingEventDisplayMethod_Fragment.glsl
+++ src/net/sf/jaer/graphics/SpaceTimeRollingEventDisplayMethod_Fragment.glsl
@@ -7,10 +7,15 @@ in float frag_t1;
 out vec4 frag_color;
 
 void main() {
-    float z=-frag_v.z;
+    float z=-frag_v.z; // 0 at most recent time, dt at most distant past
     float dt=(frag_t1-frag_t0);
-    float f=z/dt;
-    float f1=1-f;
-    frag_color = f1 * vec4(1-f1, 0.3, f1, 1.0);
+    float f=z/dt; // fraction of total time in window, 0 at now, 1 at most distant past
+    float f1=1-f; 
+    float b=max(1-2*f,0);
+    float r=max(2*(f-.5),0);
+    float g=f;
+    if(f>.5)
+        g=1-g;
+    frag_color =  (.75*f1+.25) * vec4(r, g, b, 1.0); 
 }
 
