eecd84be9e47432bdc0ee0852a4e904ed9d6bec7
Tobi Delbruck <tobi@ini.uzh.ch>
2015-05-16 05:38:54 +0000

    home key (unzoom) puts 3d origin at 0,0, as well as start and centerpoint of zoom.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@6701 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/graphics/ChipCanvas.java
+++ src/net/sf/jaer/graphics/ChipCanvas.java
@@ -1297,6 +1297,7 @@ public class ChipCanvas implements GLEventListener, Observer {
 			getZoom().setStartPoint(new Point(0, 0));
 			final int sx = chip.getSizeX(), sy = chip.getSizeY(); // chip size
                         centerPoint.setLocation(sx/2, sy/2);
+                        set3dOrigin(0, 0);
 			// getZoom().setEndPoint(new Point(getChip().getSizeX(), getChip().getSizeY()));
 			// if (!System.getProperty("os.name").contains("Mac")) {//crashes on mac os x 10.5
 			// GL g = drawable.getGL();
