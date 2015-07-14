04f6a785de05adf402dc52d59ecc0fb33471723a
Tobi Delbruck <tobi@ini.uzh.ch>
2015-07-14 16:40:05 +0000

    try new handling of uncaught exceptions to ensure that stack trace is printed, at least to console.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@6944 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/util/LoggingThreadGroup.java
+++ src/net/sf/jaer/util/LoggingThreadGroup.java
@@ -1,20 +1,25 @@
 package net.sf.jaer.util;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/** A thread group that implements uncaughtException to log uncaught exceptions */
+/**
+ * A thread group that implements uncaughtException to log uncaught exceptions
+ */
 public class LoggingThreadGroup extends ThreadGroup {
+
     private static Logger logger;
+
     public LoggingThreadGroup(String name) {
         super(name);
     }
 
-    /** Thread and Throwable passed in here are passed to a
-     * Logger named "UncaughtExceptionLogger"
-     * which has the handler LoggingWindowHandler.
+    /**
+     * Thread and Throwable passed in here are passed to a Logger named
+     * "UncaughtExceptionLogger" which has the handler LoggingWindowHandler.
      */
     public void uncaughtException(Thread t, Throwable e) {
         // Initialize logger once
@@ -23,22 +28,29 @@ public class LoggingThreadGroup extends ThreadGroup {
             Handler handler = LoggingWindowHandler.getInstance();
             logger.addHandler(handler);
         }
-        logger.log(Level.WARNING, t==null?"(null thread supplied)":t.getName(), e==null?"(null exception)":e.toString());
+        try {
+            logger.log(Level.WARNING, t == null ? "(null thread supplied)" : t.toString(), e == null ? "(null exception)" : e.toString());
+        } catch (RuntimeException rte) {
+            System.out.println((t == null ? "(null thread supplied)" : t.toString()) + ": " + (e == null ? "(null exception)" : e.toString()));
+            if(e!=null){
+                e.printStackTrace();
+            }
+        }
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
-        if(e!=null){
+        if (e != null) {
             e.printStackTrace(pw);
-        }else{
+        } else {
             sw.write("(null exception, cannot provide stack trace)");
         }
         pw.flush();
-        String st=sw.toString();
-        logger.log(Level.WARNING,st);
+        String st = sw.toString();
+        logger.log(Level.WARNING, st);
     }
 //    public static void main(String args[]) throws Exception {
 //        Thread.UncaughtExceptionHandler handler = new LoggingThreadGroup("Logger");
 //        Thread.currentThread().setUncaughtExceptionHandler(handler);
-//        System.out.println(1 / 0);
+////        System.out.println(1 / 0);
+//        throw new RuntimeException("test RuntimeException");
 //    }
 }
-
