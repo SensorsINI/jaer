09520733ebe33becfca3d7320d1c4c648f6fa2f1
Tobi Delbruck <tobi@ini.uzh.ch>
2015-08-27 07:19:12 +0000

    replaced synchronized(drawable) with synchronized(drawable.getTreeLock()) to resolve a kind of deadlock when trying to open FilterFrame, since some filters add or remove components from glCanvas such as mouseListeners. The underlying Swing AWT uses this treelock and it was ending up that AWT and ViewLoop were waiting on each other, one with lock on glCanvas and the other with lock on AWT invocation tree lock. Now the deadlock does not appear anymore. This may only have affected things in active rendering mode.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@7101 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/graphics/ChipCanvas.java
+++ src/net/sf/jaer/graphics/ChipCanvas.java
@@ -606,7 +606,7 @@ public class ChipCanvas implements GLEventListener, Observer {
             // log.warning("null Point (outside entire canvas?), returning center pixel");
             return new Point(chip.getSizeX() / 2, chip.getSizeY() / 2);
         }
-        synchronized (drawable) {
+        synchronized (drawable.getTreeLock()) {
             try {
                 if (hasAppleRetinaDisplay()) {
                     mp.x *= 2;
@@ -841,7 +841,7 @@ public class ChipCanvas implements GLEventListener, Observer {
      * @see #display(com.jogamp.opengl.GLAutoDrawable)
      */
     public void paintFrame() {
-        synchronized (drawable) {
+        synchronized (drawable.getTreeLock()) {
             try {
 //                drawable.getContext().makeCurrent();
                 drawable.display(); // we call the drawable's display method that ends up calling us back via our local
