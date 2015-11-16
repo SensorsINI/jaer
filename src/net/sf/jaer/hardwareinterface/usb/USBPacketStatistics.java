0a26e46d868195dcfde4c1f81444bdbbd1043a82
Tobi Delbruck <tobi@ini.uzh.ch>
2015-11-16 16:39:11 +0000

    added basic USB statistics collection to CypressFX2 classes to show statistics of USB packets for libusb and thesycon implementations. This statistics reporting is enabled in AEViewer USB menu as checkboxmenuitem.  Not fully tested but works on DVS128.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@7599 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/net/sf/jaer/hardwareinterface/usb/USBPacketStatistics.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sf.jaer.hardwareinterface.usb;
+
+import de.thesycon.usbio.UsbIoBuf;
+import li.longi.USBTransferThread.RestrictedTransfer;
+import net.sf.jaer.util.EngineeringFormat;
+import net.sf.jaer.util.filter.LowpassFilter;
+import net.sf.jaer.util.histogram.Histogram;
+import net.sf.jaer.util.histogram.SimpleHistogram;
+
+/**
+ * Tracks low level USB packet transfer statistics
+ * 
+ * @author tobi
+ */
+public class USBPacketStatistics implements HasUsbStatistics {
+    public LowpassFilter packetIntevalUsFilter=new LowpassFilter(100);
+    public LowpassFilter packetSizeBytesFilter=new LowpassFilter(100);
+    private long lastTimeNs;
+    private boolean initialized=false;
+    public Histogram packetSizeHistogram=new SimpleHistogram(0,128,30,0), packetIntervalUsHistogram=new SimpleHistogram(0, 125, 30, 0);
+    int nSamples=0;
+    EngineeringFormat engFmt=new EngineeringFormat();
+    boolean printEnabled=false;
+    boolean showEnabled=false;
+    private static int INTERVAL=300; // interval to print or show
+    
+    public void addSample(final RestrictedTransfer transfer) {
+        int nBytes=transfer.actualLength();
+        addValues(nBytes);
+    }
+
+    private void addValues(int nBytes) {
+        if(!(showEnabled || printEnabled)) return;
+        if(!initialized){
+            lastTimeNs=System.nanoTime();
+            initialized=true;
+            return;
+        }
+        nSamples++;
+        long timeNowNs=System.nanoTime();
+        int dtNs=(int)(timeNowNs-lastTimeNs);
+        int dtUs=dtNs/1000;
+        lastTimeNs=timeNowNs;
+        packetSizeBytesFilter.filter(nBytes, dtUs);
+        packetIntevalUsFilter.filter(dtUs, dtUs);
+        packetIntervalUsHistogram.add(dtUs);
+        packetSizeHistogram.add(nBytes);
+        if(printEnabled) printStatistics();
+        if(showEnabled) showStatistics();
+    }
+    
+   public void addSample(final UsbIoBuf transfer) {
+       int nBytes=transfer.BytesTransferred;
+         addValues(nBytes);
+    }
+    
+   public void reset(){
+       nSamples=0;
+       initialized=false;
+       packetIntevalUsFilter.reset();
+       packetSizeBytesFilter.reset();
+       packetSizeHistogram.reset();
+       packetIntervalUsHistogram.reset();
+   }
+   
+   public String toString(){
+       StringBuilder s=new StringBuilder("USBPacketStatistics: \n ");
+       s.append(String.format("Average packet size: \t%s bytes\n ",engFmt.format(packetSizeBytesFilter.getValue())));
+       s.append(String.format("Average packet interval: \t%s ms\n",engFmt.format(packetIntevalUsFilter.getValue()/1000)));
+       return s.toString();
+   }
+   
+   public void printStatistics(){
+       if(nSamples%INTERVAL!=0) return;
+       System.out.println("Packet size (bytes) histogram:");
+       packetSizeHistogram.print();
+       System.out.println("\nPacket interval (us) histogram:");
+       packetIntervalUsHistogram.print();
+       System.out.println(this.toString());
+   }
+   
+   public void showStatistics(){
+       if(nSamples%INTERVAL!=0) return;
+   }
+
+
+    @Override
+    public void setShowUsbStatistics(boolean yes) {
+        showEnabled=yes;
+        if(yes) reset();
+    }
+
+    @Override
+    public void setPrintUsbStatistics(boolean yes) {
+        printEnabled=yes;
+        if(yes) reset();
+    }
+
+ 
+    @Override
+    public boolean isShowUsbStatistics() {
+        return showEnabled;
+    }
+
+    @Override
+    public boolean isPrintUsbStatistics() {
+        return printEnabled;
+    }
+   
+   
+}
