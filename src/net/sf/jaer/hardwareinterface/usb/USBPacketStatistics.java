8ddbea26a6518548c00aa76e9df40c25d600344f
Tobi Delbruck <tobi@ini.uzh.ch>
2015-11-16 17:44:06 +0000

    changed histogram to log2 bins to cover larger range
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@7603 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/hardwareinterface/usb/USBPacketStatistics.java
+++ src/net/sf/jaer/hardwareinterface/usb/USBPacketStatistics.java
@@ -14,94 +14,107 @@ import net.sf.jaer.util.histogram.SimpleHistogram;
 
 /**
  * Tracks low level USB packet transfer statistics
- * 
+ *
  * @author tobi
  */
 public class USBPacketStatistics implements HasUsbStatistics {
-    public LowpassFilter packetIntevalUsFilter=new LowpassFilter(100);
-    public LowpassFilter packetSizeBytesFilter=new LowpassFilter(100);
+
+    public LowpassFilter packetIntevalUsFilter = new LowpassFilter(100);
+    public LowpassFilter packetSizeBytesFilter = new LowpassFilter(100);
     private long lastTimeNs;
-    private boolean initialized=false;
-    public Histogram packetSizeHistogram=new SimpleHistogram(0,128,30,0), packetIntervalUsHistogram=new SimpleHistogram(0, 125, 30, 0);
-    int nSamples=0;
-    EngineeringFormat engFmt=new EngineeringFormat();
-    boolean printEnabled=false;
-    boolean showEnabled=false;
-    private static int INTERVAL=300; // interval to print or show
-    
+    private boolean initialized = false;
+    public Histogram packetSizeHistogram = new SimpleHistogram(0, 3, 8, 0), packetIntervalUsHistogram = new SimpleHistogram(0, 3, 8, 0);
+    int nSamples = 0;
+    EngineeringFormat engFmt = new EngineeringFormat();
+    boolean printEnabled = false;
+    boolean showEnabled = false;
+    private static int INTERVAL = 300; // interval to print or show
+
     public void addSample(final RestrictedTransfer transfer) {
-        int nBytes=transfer.actualLength();
+        int nBytes = transfer.actualLength();
         addValues(nBytes);
     }
 
     private void addValues(int nBytes) {
-        if(!(showEnabled || printEnabled)) return;
-        if(!initialized){
-            lastTimeNs=System.nanoTime();
-            initialized=true;
+        if (!(showEnabled || printEnabled)) {
+            return;
+        }
+        if (!initialized) {
+            lastTimeNs = System.nanoTime();
+            initialized = true;
             return;
         }
         nSamples++;
-        long timeNowNs=System.nanoTime();
-        int dtNs=(int)(timeNowNs-lastTimeNs);
-        int dtUs=dtNs/1000;
-        lastTimeNs=timeNowNs;
+        long timeNowNs = System.nanoTime();
+        int dtNs = (int) (timeNowNs - lastTimeNs);
+        int dtUs = dtNs / 1000;
+        lastTimeNs = timeNowNs;
         packetSizeBytesFilter.filter(nBytes, dtUs);
         packetIntevalUsFilter.filter(dtUs, dtUs);
-        packetIntervalUsHistogram.add(dtUs);
-        packetSizeHistogram.add(nBytes);
-        if(printEnabled) printStatistics();
-        if(showEnabled) showStatistics();
+        packetIntervalUsHistogram.add(log2(dtUs));
+        packetSizeHistogram.add(log2(nBytes));
+        if (printEnabled) {
+            printStatistics();
+        }
+        if (showEnabled) {
+            showStatistics();
+        }
     }
-    
-   public void addSample(final UsbIoBuf transfer) {
-       int nBytes=transfer.BytesTransferred;
-         addValues(nBytes);
-    }
-    
-   public void reset(){
-       nSamples=0;
-       initialized=false;
-       packetIntevalUsFilter.reset();
-       packetSizeBytesFilter.reset();
-       packetSizeHistogram.reset();
-       packetIntervalUsHistogram.reset();
-   }
-   
-   public String toString(){
-       StringBuilder s=new StringBuilder("USBPacketStatistics: \n ");
-       s.append(String.format("Average packet size: \t%s bytes\n ",engFmt.format(packetSizeBytesFilter.getValue())));
-       s.append(String.format("Average packet interval: \t%s ms\n",engFmt.format(packetIntevalUsFilter.getValue()/1000)));
-       return s.toString();
-   }
-   
-   public void printStatistics(){
-       if(nSamples%INTERVAL!=0) return;
-       System.out.println("Packet size (bytes) histogram:");
-       packetSizeHistogram.print();
-       System.out.println("\nPacket interval (us) histogram:");
-       packetIntervalUsHistogram.print();
-       System.out.println(this.toString());
-   }
-   
-   public void showStatistics(){
-       if(nSamples%INTERVAL!=0) return;
-   }
 
+    public void addSample(final UsbIoBuf transfer) {
+        int nBytes = transfer.BytesTransferred;
+        addValues(nBytes);
+    }
+
+    public void reset() {
+        nSamples = 0;
+        initialized = false;
+        packetIntevalUsFilter.reset();
+        packetSizeBytesFilter.reset();
+        packetSizeHistogram.reset();
+        packetIntervalUsHistogram.reset();
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder("USBPacketStatistics: \n ");
+        s.append(String.format("Average packet size: \t%s bytes\n ", engFmt.format(packetSizeBytesFilter.getValue())));
+        s.append(String.format("Average packet interval: \t%s ms\n", engFmt.format(packetIntevalUsFilter.getValue() / 1000)));
+        return s.toString();
+    }
+
+    public void printStatistics() {
+        if (nSamples % INTERVAL != 0) {
+            return;
+        }
+        System.out.println("Packet size (log2(bytes)) histogram:");
+        packetSizeHistogram.print();
+        System.out.println("\nPacket interval (log2(us)) histogram:");
+        packetIntervalUsHistogram.print();
+        System.out.println(this.toString());
+    }
+
+    public void showStatistics() {
+        if (nSamples % INTERVAL != 0) {
+            return;
+        }
+    }
 
     @Override
     public void setShowUsbStatistics(boolean yes) {
-        showEnabled=yes;
-        if(yes) reset();
+        showEnabled = yes;
+        if (yes) {
+            reset();
+        }
     }
 
     @Override
     public void setPrintUsbStatistics(boolean yes) {
-        printEnabled=yes;
-        if(yes) reset();
+        printEnabled = yes;
+        if (yes) {
+            reset();
+        }
     }
 
- 
     @Override
     public boolean isShowUsbStatistics() {
         return showEnabled;
@@ -111,6 +124,13 @@ public class USBPacketStatistics implements HasUsbStatistics {
     public boolean isPrintUsbStatistics() {
         return printEnabled;
     }
-   
-   
+
+    /** from http://stackoverflow.com/questions/3305059/how-do-you-calculate-log-base-2-in-java-for-integers */
+    public static int log2(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException();
+        }
+        return 31 - Integer.numberOfLeadingZeros(n);
+    }
+
 }
