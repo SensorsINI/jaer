4365fa113cf7a0e34328ccc63d06a67db7e058c8
Tobi Delbruck <tobi@ini.uzh.ch>
2016-01-21 10:19:44 +0000

    added ROS publisher node object for interfacing to Ulster ROS running on predator robot
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@7909 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/eu/visualize/ini/convnet/VisualliseSteeringNetRosNodePublisher.java
@@ -0,0 +1,36 @@
+package eu.visualize.ini.convnet;
+import eu.visualize.ini.retinamodel.*;
+import net.sf.jaer.Description;
+
+/**
+ * RosJava node supplying data to ROS master.
+ * This publisher supplies steering CNN network output units.
+ * These units encode the activity of the four outputs
+ * [left, center, right, invisible}
+ * 
+ @author Tobi
+*/
+@Description("RosJava node supplying data to ROS master")
+public class VisualliseSteeringNetRosNodePublisher {
+    /** Steering net winning output
+     * 0=left, 1=center, 2=right, 3=no target
+     */
+    private int output;
+
+    /**
+     * 0=left, 1=center, 2=right, 3=no target
+     * @return the output
+     */
+    public int getOutput() {
+        return output;
+    }
+
+    /**
+     * 0=left, 1=center, 2=right, 3=no target
+     * @param output the output to set
+     */
+    public void setOutput(int output) {
+        this.output = output;
+    }
+    
+}
