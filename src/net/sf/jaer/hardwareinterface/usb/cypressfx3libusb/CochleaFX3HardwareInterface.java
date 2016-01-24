cb45a40104ad3856e5a26e8dbf31102b3a579f0f
Tobi Delbruck <tobi@ini.uzh.ch>
2016-01-24 18:39:34 +0000

    made the timestamp wrap events special events that ARE passed on from CochleaFX3, to allow jAER to know that time is passing. Currently these are decoded as a cochlea event with channel=0, which should be modified.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@7956 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/hardwareinterface/usb/cypressfx3libusb/CochleaFX3HardwareInterface.java
+++ src/net/sf/jaer/hardwareinterface/usb/cypressfx3libusb/CochleaFX3HardwareInterface.java
@@ -14,200 +14,209 @@ import java.nio.ShortBuffer;
 import org.usb4java.Device;
 
 import net.sf.jaer.aemonitor.AEPacketRaw;
+import net.sf.jaer.event.BasicEvent;
 import net.sf.jaer.hardwareinterface.HardwareInterfaceException;
 
 /**
  * Adds functionality of apsDVS sensors to based CypressFX3Biasgen class. The
- * key method is translateEvents that parses
- * the data from the sensor to construct jAER raw events.
+ * key method is translateEvents that parses the data from the sensor to
+ * construct jAER raw events.
  *
  * @author Christian/Tobi
  */
 public class CochleaFX3HardwareInterface extends CypressFX3Biasgen {
 
-	protected CochleaFX3HardwareInterface(final Device device) {
-		super(device);
-	}
+    protected CochleaFX3HardwareInterface(final Device device) {
+        super(device);
+    }
 
-	/** The USB product ID of this device */
-	static public final short PID = (short) 0x841C;
-	static public final int REQUIRED_FIRMWARE_VERSION = 2;
-	static public final int REQUIRED_LOGIC_REVISION = 0;
+    /**
+     * The USB product ID of this device
+     */
+    static public final short PID = (short) 0x841C;
+    static public final int REQUIRED_FIRMWARE_VERSION = 2;
+    static public final int REQUIRED_LOGIC_REVISION = 0;
 
-	/**
-	 * Starts reader buffer pool thread and enables in endpoints for AEs. This
-	 * method is overridden to construct
-	 * our own reader with its translateEvents method
-	 */
-	@Override
-	public void startAEReader() throws HardwareInterfaceException {
-		setAeReader(new RetinaAEReader(this));
-		allocateAEBuffers();
+    /**
+     * Starts reader buffer pool thread and enables in endpoints for AEs. This
+     * method is overridden to construct our own reader with its translateEvents
+     * method
+     */
+    @Override
+    public void startAEReader() throws HardwareInterfaceException {
+        setAeReader(new RetinaAEReader(this));
+        allocateAEBuffers();
 
-		getAeReader().startThread(); // arg is number of errors before giving up
-		HardwareInterfaceException.clearException();
-	}
+        getAeReader().startThread(); // arg is number of errors before giving up
+        HardwareInterfaceException.clearException();
+    }
 
-	public static final int CHIP_COCHLEALP = 11;
-	public static final int CHIP_COCHLEA4EAR = 12;
-	public static final int CHIP_SAMPLEPROB = 13; // Reuse Cochlea code for SampleProb chip.
+    public static final int CHIP_COCHLEALP = 11;
+    public static final int CHIP_COCHLEA4EAR = 12;
+    public static final int CHIP_SAMPLEPROB = 13; // Reuse Cochlea code for SampleProb chip.
 
-	/**
-	 * This reader understands the format of raw USB data and translates to the
-	 * AEPacketRaw
-	 */
-	public class RetinaAEReader extends CypressFX3.AEReader implements PropertyChangeListener {
-		private final int chipID;
+    /**
+     * This reader understands the format of raw USB data and translates to the
+     * AEPacketRaw
+     */
+    public class RetinaAEReader extends CypressFX3.AEReader implements PropertyChangeListener {
 
-		private final int aerMaxAddress;
+        private final int chipID;
 
-		private int wrapAdd;
-		private int lastTimestamp;
-		private int currentTimestamp;
+        private final int aerMaxAddress;
 
-		public RetinaAEReader(final CypressFX3 cypress) throws HardwareInterfaceException {
-			super(cypress);
+        private int wrapAdd;
+        private int lastTimestamp;
+        private int currentTimestamp;
 
-			checkFirmwareLogic(CochleaFX3HardwareInterface.REQUIRED_FIRMWARE_VERSION, CochleaFX3HardwareInterface.REQUIRED_LOGIC_REVISION);
+        public RetinaAEReader(final CypressFX3 cypress) throws HardwareInterfaceException {
+            super(cypress);
 
-			chipID = spiConfigReceive(CypressFX3.FPGA_SYSINFO, (short) 1);
+            checkFirmwareLogic(CochleaFX3HardwareInterface.REQUIRED_FIRMWARE_VERSION, CochleaFX3HardwareInterface.REQUIRED_LOGIC_REVISION);
 
-			if (chipID == CochleaFX3HardwareInterface.CHIP_COCHLEALP) {
-				aerMaxAddress = 256;
-			}
-			else if (chipID == CochleaFX3HardwareInterface.CHIP_COCHLEA4EAR) {
-				aerMaxAddress = 1024;
-			}
-			else {
-				// CHIP_SAMPLEPROB
-				aerMaxAddress = 16;
-			}
-		}
+            chipID = spiConfigReceive(CypressFX3.FPGA_SYSINFO, (short) 1);
 
-		private void checkMonotonicTimestamp() {
-			if (currentTimestamp <= lastTimestamp) {
-				CypressFX3.log.severe(toString() + ": non strictly-monotonic timestamp detected: lastTimestamp=" + lastTimestamp
-					+ ", currentTimestamp=" + currentTimestamp + ", difference=" + (lastTimestamp - currentTimestamp) + ".");
-			}
-		}
+            if (chipID == CochleaFX3HardwareInterface.CHIP_COCHLEALP) {
+                aerMaxAddress = 256;
+            } else if (chipID == CochleaFX3HardwareInterface.CHIP_COCHLEA4EAR) {
+                aerMaxAddress = 1024;
+            } else {
+                // CHIP_SAMPLEPROB
+                aerMaxAddress = 16;
+            }
+        }
 
-		private boolean ensureCapacity(final AEPacketRaw buffer, final int capacity) {
-			if (buffer.getCapacity() > getAEBufferSize()) {
-				if (buffer.overrunOccuredFlag || (capacity > buffer.getCapacity())) {
-					buffer.overrunOccuredFlag = true;
-					return (false);
-				}
+        private void checkMonotonicTimestamp() {
+            if (currentTimestamp <= lastTimestamp) {
+                CypressFX3.log.severe(toString() + ": non strictly-monotonic timestamp detected: lastTimestamp=" + lastTimestamp
+                        + ", currentTimestamp=" + currentTimestamp + ", difference=" + (lastTimestamp - currentTimestamp) + ".");
+            }
+        }
 
-				return (true);
-			}
+        private boolean ensureCapacity(final AEPacketRaw buffer, final int capacity) {
+            if (buffer.getCapacity() > getAEBufferSize()) {
+                if (buffer.overrunOccuredFlag || (capacity > buffer.getCapacity())) {
+                    buffer.overrunOccuredFlag = true;
+                    return (false);
+                }
 
-			buffer.ensureCapacity(capacity);
-			return (true);
-		}
+                return (true);
+            }
 
-		@Override
-		protected void translateEvents(final ByteBuffer b) {
-			synchronized (aePacketRawPool) {
-				final AEPacketRaw buffer = aePacketRawPool.writeBuffer();
+            buffer.ensureCapacity(capacity);
+            return (true);
+        }
 
-				// Truncate off any extra partial event.
-				if ((b.limit() & 0x01) != 0) {
-					CypressFX3.log.severe(b.limit() + " bytes received via USB, which is not a multiple of two.");
-					b.limit(b.limit() & ~0x01);
-				}
+        @Override
+        protected void translateEvents(final ByteBuffer b) {
+            synchronized (aePacketRawPool) {
+                final AEPacketRaw buffer = aePacketRawPool.writeBuffer();
 
-				buffer.lastCaptureIndex = eventCounter;
+                // Truncate off any extra partial event.
+                if ((b.limit() & 0x01) != 0) {
+                    CypressFX3.log.severe(b.limit() + " bytes received via USB, which is not a multiple of two.");
+                    b.limit(b.limit() & ~0x01);
+                }
 
-				final ShortBuffer sBuf = b.order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
+                buffer.lastCaptureIndex = eventCounter;
 
-				for (int i = 0; i < sBuf.limit(); i++) {
-					final short event = sBuf.get(i);
+                final ShortBuffer sBuf = b.order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
 
-					// Check if timestamp
-					if ((event & 0x8000) != 0) {
-						// Is a timestamp! Expand to 32 bits. (Tick is 1us already.)
-						lastTimestamp = currentTimestamp;
-						currentTimestamp = wrapAdd + (event & 0x7FFF);
+                for (int i = 0; i < sBuf.limit(); i++) {
+                    final short event = sBuf.get(i);
 
-						// Check monotonicity of timestamps.
-						checkMonotonicTimestamp();
-					}
-					else {
-						// Look at the code, to determine event and data
-						// type
-						final byte code = (byte) ((event & 0x7000) >>> 12);
-						final short data = (short) (event & 0x0FFF);
+                    // Check if timestamp
+                    if ((event & 0x8000) != 0) {
+                        // Is a timestamp! Expand to 32 bits. (Tick is 1us already.)
+                        lastTimestamp = currentTimestamp;
+                        currentTimestamp = wrapAdd + (event & 0x7FFF);
 
-						switch (code) {
-							case 0: // Special event
-								switch (data) {
-									case 0: // Ignore this, but log it.
-										CypressFX3.log.severe("Caught special reserved event!");
-										break;
+                        // Check monotonicity of timestamps.
+                        checkMonotonicTimestamp();
+                    } else {
+                        // Look at the code, to determine event and data
+                        // type
+                        final byte code = (byte) ((event & 0x7000) >>> 12);
+                        final short data = (short) (event & 0x0FFF);
 
-									case 1: // Timetamp reset
-										wrapAdd = 0;
-										lastTimestamp = 0;
-										currentTimestamp = 0;
+                        switch (code) {
+                            case 0: // Special event
+                                switch (data) {
+                                    case 0: // Ignore this, but log it.
+                                        CypressFX3.log.severe("Caught special reserved event!");
+                                        break;
 
-										CypressFX3.log.info("Timestamp reset event received on " + super.toString());
-										break;
+                                    case 1: // Timetamp reset
+                                        wrapAdd = 0;
+                                        lastTimestamp = 0;
+                                        currentTimestamp = 0;
 
-									default:
-										CypressFX3.log.severe("Caught special event that can't be handled.");
-										break;
-								}
-								break;
+                                        CypressFX3.log.info("Timestamp reset event received on " + super.toString());
+                                        break;
 
-							case 1: // AER address
-								// Check range conformity.
-								if (data >= aerMaxAddress) {
-									CypressFX3.log.severe("AER: address out of range (0-" + (aerMaxAddress - 1) + "): " + data + ".");
-									break; // Skip invalid AER address.
-								}
+                                    default:
+                                        CypressFX3.log.severe("Caught special event that can't be handled.");
+                                        break;
+                                }
+                                break;
 
-								// Check that the buffer has space for this event. Enlarge if needed.
-								if (ensureCapacity(buffer, eventCounter + 1)) {
-									buffer.getAddresses()[eventCounter] = data;
+                            case 1: // AER address
+                                // Check range conformity.
+                                if (data >= aerMaxAddress) {
+                                    CypressFX3.log.severe("AER: address out of range (0-" + (aerMaxAddress - 1) + "): " + data + ".");
+                                    break; // Skip invalid AER address.
+                                }
 
-									buffer.getTimestamps()[eventCounter++] = currentTimestamp;
-								}
+                                // Check that the buffer has space for this event. Enlarge if needed.
+                                if (ensureCapacity(buffer, eventCounter + 1)) {
+                                    buffer.getAddresses()[eventCounter] = data;
 
-								break;
+                                    buffer.getTimestamps()[eventCounter++] = currentTimestamp;
+                                }
 
-							case 7: // Timestamp wrap
-								// Each wrap is 2^15 us (~32ms), and we have
-								// to multiply it with the wrap counter,
-								// which is located in the data part of this
-								// event.
-								wrapAdd += (0x8000L * data);
+                                break;
 
-								lastTimestamp = currentTimestamp;
-								currentTimestamp = wrapAdd;
+                            case 7: // Timestamp wrap
+                                // Each wrap is 2^15 us (~32ms), and we have
+                                // to multiply it with the wrap counter,
+                                // which is located in the data part of this
+                                // event.
+                                wrapAdd += (0x8000L * data);
 
-								// Check monotonicity of timestamps.
-								checkMonotonicTimestamp();
+                                lastTimestamp = currentTimestamp;
+                                currentTimestamp = wrapAdd;
 
-								CypressFX3.log.fine(
-									String.format("Timestamp wrap event received on %s with multiplier of %d.", super.toString(), data));
-								break;
+                                // Check monotonicity of timestamps.
+                                checkMonotonicTimestamp();
 
-							default:
-								CypressFX3.log.severe("Caught event that can't be handled.");
-								break;
-						}
-					}
-				} // end loop over usb data buffer
+                                // TODO generate timer event to advance clock on host side even with low event rate of cochlea
+                                CypressFX3.log.fine(
+                                        String.format("Timestamp wrap event received on %s with multiplier of %d.", super.toString(), data));
 
-				buffer.setNumEvents(eventCounter);
-				// write capture size
-				buffer.lastCaptureLength = eventCounter - buffer.lastCaptureIndex;
-			} // sync on aePacketRawPool
-		}
+                                if (ensureCapacity(buffer, eventCounter + 1)) {
+                                    buffer.getAddresses()[eventCounter] = ((int)(data&0xFFFF)) | BasicEvent.SPECIAL_EVENT_BIT_MASK;
 
-		@Override
-		public void propertyChange(final PropertyChangeEvent arg0) {
-			// Do nothing here, IMU comes directly via event-stream.
-		}
-	}
+                                    buffer.getTimestamps()[eventCounter++] = currentTimestamp;
+                                }
+
+                                break;
+
+                            default:
+                                CypressFX3.log.severe("Caught event that can't be handled.");
+                                break;
+                        }
+                    }
+                } // end loop over usb data buffer
+
+                buffer.setNumEvents(eventCounter);
+                // write capture size
+                buffer.lastCaptureLength = eventCounter - buffer.lastCaptureIndex;
+            } // sync on aePacketRawPool
+        }
+
+        @Override
+        public void propertyChange(final PropertyChangeEvent arg0) {
+            // Do nothing here, IMU comes directly via event-stream.
+        }
+    }
 }
