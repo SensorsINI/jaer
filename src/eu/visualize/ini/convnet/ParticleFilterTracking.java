d54aa1bbb99eac47f3ea5e0b62b770be62fc5c40
Min Liu <minliu@ini.uzh.ch>
2016-04-01 10:38:14 +0000

    Continue the particle filter for tracking. Didn't change much, just added some initialize step for the filter. By Hongjie and Min.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@8423 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/eu/visualize/ini/convnet/ParticleFilterTracking.java
+++ src/eu/visualize/ini/convnet/ParticleFilterTracking.java
@@ -8,11 +8,14 @@ package eu.visualize.ini.convnet;
 import com.jogamp.opengl.GLAutoDrawable;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Random;
 import net.sf.jaer.Description;
 import net.sf.jaer.DevelopmentStatus;
 import net.sf.jaer.chip.AEChip;
+import net.sf.jaer.event.ApsDvsEventPacket;
 import net.sf.jaer.event.EventPacket;
 import net.sf.jaer.eventprocessing.EventFilter2D;
+import net.sf.jaer.eventprocessing.tracking.RectangularClusterTrackerEvent;
 import net.sf.jaer.graphics.FrameAnnotater;
 import net.sf.jaer.util.filter.ParticleFilter.DynamicEvaluator;
 import net.sf.jaer.util.filter.ParticleFilter.MeasurmentEvaluator;
@@ -29,14 +32,42 @@ public class ParticleFilterTracking extends EventFilter2D implements PropertyCha
     DynamicEvaluator dynamic;
     MeasurmentEvaluator measurement;
     ParticleFilter filter = new ParticleFilter(dynamic, measurement);
+    
+    protected boolean filterEventsEnabled = getBoolean("filterEventsEnabled", false); // enables filtering events so
+    
 
+    
     public ParticleFilterTracking(AEChip chip) {
         super(chip);
     }
 
     @Override
     public EventPacket<?> filterPacket(EventPacket<?> in) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (!filterEnabled) {
+                return in;
+        }
+        // added so that packets don't use a zero length packet to set last
+        // timestamps, etc, which can purge clusters for no reason
+        if (in.getSize() == 0) {
+                return in;
+        }
+
+        if (enclosedFilter != null) {
+                in = enclosedFilter.filterPacket(in);
+        }
+
+        if (in instanceof ApsDvsEventPacket) {
+                checkOutputPacketEventType(in); // make sure memory is allocated to avoid leak
+        }
+        else if (filterEventsEnabled) {
+                checkOutputPacketEventType(RectangularClusterTrackerEvent.class);
+        }
+        
+        
+        Random r = new Random();
+        measurement = new MeasurmentEvaluator();
+        
+        return in;
     }
 
     @Override
