bda38fcc80062c555faf6aa96c0c052e98384cca
Luca Longinotti <luca.longinotti@inilabs.com>
2016-05-19 11:03:33 +0000

    jAER: fix Linux start script. Comments cannot be inside the command itself.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@8757 b7f4320f-462c-0410-a916-d9f35bb82d52

--- jAERViewer1.5_linux.sh
+++ jAERViewer1.5_linux.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# If you get "UnsatisfiedLinkError: /tmp/usb4java... failed to map segment from shared object: Operation not permitted",
+# add the following line to the command below:
+# -Djava.io.tmpdir=~/tmpdir/
+
 java \
 -classpath "dist/jAER.jar:jars/*:jars/javacv/*:jars/jogl/*:jars/usb4java/*" \
 -splash:SplashScreen.gif \
 -Djava.util.logging.config.file=conf/Logging.properties -Dsun.java2d.noddraw=true -Dsun.java2d.opengl=false \
-#-Djava.io.tmpdir=~/tmpdir/ \ # use this if you get "UnsatisfiedLinkError: /tmp/usb4java... failed to map segment from shared object: Operation not permitted"
 net.sf.jaer.JAERViewer
