12c6a9c067df73df205b9ff0508c8720dbc701a8
Shih-Chii Liu <shih@ini.uzh.ch>
2016-08-31 15:44:28 +0000

    Changed the description on the Config Chain Panel.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@9067 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/ch/unizh/ini/jaer/chip/cochlea/CochleaTow4EarChannelConfig.java
+++ src/ch/unizh/ini/jaer/chip/cochlea/CochleaTow4EarChannelConfig.java
@@ -21,35 +21,35 @@ public class CochleaTow4EarChannelConfig extends CochleaChannelConfig {
 		CHANNEL_FIELDS = new MultiBitValue[12];
 		// Leak current for neurons 0-1
 		CHANNEL_FIELDS[0] = new MultiBitValue(1, 19, 
-			"Clear = Double leak current for neurons 0-1");
+			"Clear = Adds 0.5X more leak current for neurons 0-1");
 		// Current Gain for neurons 0-1
 		CHANNEL_FIELDS[1] = new MultiBitValue(3, 16, 
-			"PGA gain control for neurons 0-1, 3 bits, effectively 4 levels, uses only values 0,1,3,7. 0 is highest gain, 7 is lowest");
+			"Input current control for neurons 0-1, 3 bits, Q0=1 toggles 0.5X more input current,Q1=1 or Q2=1 toggles 0.25X more input current, code is Q2Q1Q0");
 		// Leak current for neurons 2-3
 		CHANNEL_FIELDS[2] = new MultiBitValue(1, 15, 
-			"Clear = Double leak current for neurons 2-3");
+			"Clear = Adds 0.5X more leak current for neurons 2-3");
 		// Current Gain for neurons 2-3
 		CHANNEL_FIELDS[3] = new MultiBitValue(3, 12, 
-			"PGA gain control for neurons 2-3, 3 bits, effectively 4 levels, uses only values 0,1,3,7. 0 is highest gain, 7 is lowest");
+			"Input current control for neurons 2-3, 3 bits,Q0=1 toggles 0.5X more input current,Q1=1 or Q2=1 toggles 0.25X more input current, code is Q2Q1Q0");
 		// Double leak current
 		CHANNEL_FIELDS[4] = new MultiBitValue(1, 11, 
-			"Double leak current for all 4 neurons. nQ3=0 adds one more NeuronVleak branch to neuron");
+			"Double leak current for all 4 neurons. nQ3=0 adds 1X more leak current to neuron");
 		// Diff1
 		CHANNEL_FIELDS[5] = new MultiBitValue(3, 8, 
-			"diff1. nQ0=0 adds 25fF to feedback branch, nQ1=0 adds 33.3fF to FB branch, nQ2=0 adds 100fF to FB branch");
+			"Diff1. nQ0=0 adds 25fF to feedback branch, nQ1=0 adds 33.3fF to FB branch, nQ2=0 adds 100fF to FB branch");
 		// qTuning
 		CHANNEL_FIELDS[6] = new MultiBitValue(3, 5, 
-			"Gain for SOS. Q tuning for SO. Q5, Q6, Q7 adds one xtoron the other leg of diffpair.");
+			"Gain for SOS. Q tuning for SO. Q5, Q6, Q7 adds one xtor on the other leg of diffpair. Increases Q");
 		CHANNEL_FIELDS[7] = new MultiBitValue(1, 4, 
-			"Gain for SOS. Q tuning for SO. nQ4 picks chain with 1 xtor in series");
+			"Gain for SOS. Q tuning for SO. nQ4 picks chain with 1 xtor in series. Decreases Q");
 		CHANNEL_FIELDS[8] = new MultiBitValue(1, 3, 
-			"Gain for SOS. Q tuning for SO. nQ3 picks chain with 2 xtor in series");
+			"Gain for SOS. Q tuning for SO. nQ3 picks chain with 2 xtor in series. Decreases Q");
 		CHANNEL_FIELDS[9] = new MultiBitValue(1, 2, 
-			"Gain for SOS. Q tuning for SO. nQ2 picks chain with 3 xtor in series");
+			"Gain for SOS. Q tuning for SO. nQ2 picks chain with 3 xtor in series. Decreases Q");
 		CHANNEL_FIELDS[10] = new MultiBitValue(1, 1, 
-			"Gain for SOS. Q tuning for SO. nQ1 picks chain with 4 xtor in series");
+			"Gain for SOS. Q tuning for SO. nQ1 picks chain with 4 xtor in series. Decreases Q");
 		CHANNEL_FIELDS[11] = new MultiBitValue(1, 0, 
-			"Gain for SOS. Q tuning for SO. nQ0 picks chain with 5 xtor in series");
+			"Gain for SOS. Q tuning for SO. nQ0 picks chain with 5 xtor in series. Decreases Q");
 	}
 	public CochleaTow4EarChannelConfig(final String configName, final String toolTip, final int channelAddress, final AEChip chip) {
 		super(CHANNEL_FIELDS, configName, toolTip, channelAddress, chip);
