1e5de8a5af619047d8d750c12318665d7c652d8f
Iulia-Alexandra Lungu <iulialexandra@ini.uzh.ch>
2016-11-28 21:48:30 +0000

    fixed dvs-avi-slice-writer.sh to propagate arguments
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@9259 b7f4320f-462c-0410-a916-d9f35bb82d52

--- dvs-slice-avi-writer.sh
+++ dvs-slice-avi-writer.sh
@@ -4,7 +4,5 @@
 # add the following line to the command below:
 # -Djava.io.tmpdir=~/tmpdir/
 
-java \
--classpath "dist/jAER.jar:jars/*:jars/javacv/*:jars/jogl/*:jars/usb4java/*" \
--Dsun.java2d.noddraw=true -Dsun.java2d.opengl=false \
-net.sf.jaer.util.avioutput.DvsSliceAviWriter 
+JHOME="/media/iulialexandra/Storage/packages/jaer/code/jAER/trunk"
+java -classpath "${JHOME}/dist/jAER.jar:${JHOME}/jars/*:${JHOME}/jars/javacv/*:${JHOME}/jars/jogl/*:${JHOME}/jars/usb4java/*" -Dsun.java2d.noddraw=true -Dsun.java2d.opengl=false net.sf.jaer.util.avioutput.DvsSliceAviWriter "$@"
