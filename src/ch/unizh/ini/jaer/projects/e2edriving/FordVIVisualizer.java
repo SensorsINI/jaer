a1e4dd3fc43c3f01a925a430c1ae946c9101775a
Tobi Delbruck <tobi@ini.uzh.ch>
2016-12-15 15:45:29 +0000

    more fiddiling to read datastarttime
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@9304 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/ch/unizh/ini/jaer/projects/e2edriving/FordVIVisualizer.java
+++ src/ch/unizh/ini/jaer/projects/e2edriving/FordVIVisualizer.java
@@ -12,6 +12,11 @@ import java.beans.PropertyChangeListener;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
@@ -20,6 +25,7 @@ import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import net.sf.jaer.chip.AEChip;
 import net.sf.jaer.event.EventPacket;
+import net.sf.jaer.eventio.AEFileInputStream;
 import net.sf.jaer.eventprocessing.EventFilter2D;
 import net.sf.jaer.graphics.AEViewer;
 import net.sf.jaer.graphics.FrameAnnotater;
@@ -32,7 +38,7 @@ import net.sf.jaer.graphics.FrameAnnotater;
  */
 public class FordVIVisualizer extends EventFilter2D implements FrameAnnotater, PropertyChangeListener {
 
-    private long recordingStartTimeMs = 0;
+    private long aeDatStartTimeMs = 0;
     private File fordViFile = null;
     String lastFordVIFile = getString("lastFordVIFile", null);
     private boolean addedPropertyChangeListener=false;
@@ -72,6 +78,23 @@ public class FordVIVisualizer extends EventFilter2D implements FrameAnnotater, P
     public void annotate(GLAutoDrawable drawable) {
     }
 
+    private long parseDataStartTimeFromAeDatFile(AEFileInputStream aeis) throws FileNotFoundException, IOException{
+        // # DataStartTime: System.currentTimeMillis() 1481800498468
+        File f=aeis.getFile();
+        LineNumberReader is=new LineNumberReader(new InputStreamReader(new FileInputStream(f)));
+        while(is.getLineNumber()<5000){
+            String line=is.readLine();
+            if(line.contains("DataStartTime")){
+                Scanner s=new Scanner(line);
+                aeDatStartTimeMs=s.nextLong();
+                return aeDatStartTimeMs;
+            }
+        }
+        log.warning("could not find data start time DataStartTime in AEDAT file");
+        return -1;
+        
+    }
+   
    
     synchronized public void doOpenFordVIFile() {
         JFileChooser c = new JFileChooser(lastFordVIFile);
