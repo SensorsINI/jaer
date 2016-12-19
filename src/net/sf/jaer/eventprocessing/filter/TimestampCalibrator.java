bc8d3f3474b0b062b6ad962ee6c171887aac3741
Tobi Delbruck <tobi@ini.uzh.ch>
2016-12-19 08:06:14 +0000

    messing with log file for calibrator
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@9315 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/eventprocessing/filter/TimestampCalibrator.java
+++ src/net/sf/jaer/eventprocessing/filter/TimestampCalibrator.java
@@ -13,7 +13,11 @@ import java.awt.Font;
 import java.awt.geom.Rectangle2D;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.File;
 import java.util.Iterator;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
 import net.sf.jaer.Description;
 import net.sf.jaer.DevelopmentStatus;
 import net.sf.jaer.chip.AEChip;
@@ -24,6 +28,7 @@ import net.sf.jaer.eventprocessing.EventFilter2D;
 import net.sf.jaer.graphics.AEViewer;
 import net.sf.jaer.graphics.AEViewer.PlayMode;
 import net.sf.jaer.graphics.FrameAnnotater;
+import net.sf.jaer.util.TobiLogger;
 
 /**
  * Measures and adjusts timestamps of events using the computer clock as the
@@ -45,16 +50,23 @@ public class TimestampCalibrator extends EventFilter2D implements FrameAnnotater
     int bigWraps = 0, lastPacketTs = Integer.MIN_VALUE; // number of times camera timestamp has wrapped around
     boolean propertyChangeListenersAdded = false;
 
+    private String lastLoggingFolder = getString("lastLoggingFolder", System.getProperty("user.home"));
+    private int loggingIntervalS = getInt("loggingIntervalS", 1);
+    private TobiLogger tobiLogger = null;
+    private long lastLoggingTimeMs=0;
+
     public TimestampCalibrator(AEChip chip) {
         super(chip);
-        setPropertyTooltip("minTimeForCalibrationS","minimum time elapsed before calibration is updated");
-        setPropertyTooltip("enableCalibration","a new calibration value is not computed unless enabled, allowing us of filter with pre-measured calibration");
-        setPropertyTooltip("ppmTimestampTooFastError","<html>the calibration factor: <br>parts per million that the timestamp clock is too fast relative to computer clock. <br> I.e. +1000 means timestamps advance 0.1% too fast; <br>after 1000 ticks of computer clock, timestamp will be 1 tick ahead");
-        setPropertyTooltip("correctTimestampEnabled","if enabled, the event timestamps are corrected by multiplying by ppmTimestampTooFastError");
+        setPropertyTooltip("minTimeForCalibrationS", "minimum time elapsed before calibration is updated");
+        setPropertyTooltip("enableCalibration", "a new calibration value is not computed unless enabled, allowing us of filter with pre-measured calibration");
+        setPropertyTooltip("ppmTimestampTooFastError", "<html>the calibration factor: <br>parts per million that the timestamp clock is too fast relative to computer clock. <br> I.e. +1000 means timestamps advance 0.1% too fast; <br>after 1000 ticks of computer clock, timestamp will be 1 tick ahead");
+        setPropertyTooltip("correctTimestampEnabled", "if enabled, the event timestamps are corrected by multiplying by ppmTimestampTooFastError");
+        setPropertyTooltip("selectLoggingFolder", "selects folder to store timestamp calibration log file");
+        setPropertyTooltip("loggingIntervalS", "interval in seconds for logging calibration data after selecting logging folder");
     }
 
     @Override
-    public EventPacket<?> filterPacket(EventPacket<?> in) {
+    synchronized public EventPacket<?> filterPacket(EventPacket<?> in) {
         if (in.isEmpty() || !(chip.getAeViewer().getPlayMode() == PlayMode.LIVE)) {
             return in;
         }
@@ -77,26 +89,30 @@ public class TimestampCalibrator extends EventFilter2D implements FrameAnnotater
         lastPacketTs = currentTs;
         long dtClockNs = currentClockNs - startingClockNs;
         long dtTsUs = (currentTs - startingTsUs) + bigWraps * ((long) Integer.MAX_VALUE - (long) Integer.MIN_VALUE);
-        if (dtClockNs/1000000000 <minTimeForCalibrationS  ) {
+        if (dtClockNs / 1000000000 < minTimeForCalibrationS) {
             return in;
         }
 
         driftTsMinusClockMs = dtTsUs / 1000 - dtClockNs / 1000000;
         float ratioTsToClock = (float) dtTsUs * 1000 / (float) dtClockNs;
-        float ppmTsTooFast=1e6f*(ratioTsToClock-1);
-        if(enableCalibration){
+        float ppmTsTooFast = 1e6f * (ratioTsToClock - 1);
+        if (enableCalibration) {
             setPpmTimestampTooFastError(ppmTsTooFast);
         }
-        if(correctTimestampEnabled){
-            Iterator<BasicEvent> i=null;
-            if(in instanceof ApsDvsEventPacket){
-                i=((ApsDvsEventPacket)in).fullIterator();
-            }else{
-                i=(Iterator<BasicEvent>)in.inputIterator();
+       if(tobiLogger!=null && tobiLogger.isEnabled() && (System.currentTimeMillis()-lastLoggingTimeMs) > 1000*loggingIntervalS){
+            lastLoggingTimeMs=System.currentTimeMillis();
+            tobiLogger.log(String.format("%d %d",dtClockNs,dtTsUs));
+        }
+        if (correctTimestampEnabled) {
+            Iterator<BasicEvent> i = null;
+            if (in instanceof ApsDvsEventPacket) {
+                i = ((ApsDvsEventPacket) in).fullIterator();
+            } else {
+                i = (Iterator<BasicEvent>) in.inputIterator();
             }
-            while(i.hasNext()){
-                BasicEvent e=i.next();
-                e.timestamp=(int)(e.timestamp*(1-1e-6f*ppmTimestampTooFastError)); // if ppmTimestampTooFastError is positive, timestamp will be reduced
+            while (i.hasNext()) {
+                BasicEvent e = i.next();
+                e.timestamp = (int) (e.timestamp * (1 - 1e-6f * ppmTimestampTooFastError)); // if ppmTimestampTooFastError is positive, timestamp will be reduced
             }
         }
         return in;
@@ -127,7 +143,7 @@ public class TimestampCalibrator extends EventFilter2D implements FrameAnnotater
         gl.glLineWidth(2);
         final float x = chip.getSizeX() * .3f, y = (chip.getSizeY()) * .9f, scale = .2f;
         textRenderer.begin3DRendering();
-        String s = String.format("TimestampCalibrator: ts-wall: %6dms, ppmTsTooFast: %6.0f", driftTsMinusClockMs,ppmTimestampTooFastError);
+        String s = String.format("TimestampCalibrator: ts-wall: %6dms, ppmTsTooFast: %6.0f", driftTsMinusClockMs, ppmTimestampTooFastError);
         Rectangle2D r = textRenderer.getBounds(s);
         textRenderer.draw3D(s, (float) (x - scale * r.getWidth() / 2), (float) (y - scale * r.getHeight() / 2), 0, scale);
         textRenderer.end3DRendering();
@@ -141,8 +157,7 @@ public class TimestampCalibrator extends EventFilter2D implements FrameAnnotater
     }
 
     /**
-     * @param ppmTimestampTooFastError the ppmTimestampTooFastError to
- set
+     * @param ppmTimestampTooFastError the ppmTimestampTooFastError to set
      */
     public void setPpmTimestampTooFastError(float ppmTimestampTooFastError) {
         float old = this.ppmTimestampTooFastError;
@@ -159,6 +174,22 @@ public class TimestampCalibrator extends EventFilter2D implements FrameAnnotater
         }
     }
 
+    synchronized public void doSelectLoggingFolder() {
+        JFileChooser c = new JFileChooser(lastLoggingFolder);
+        c.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int ret = c.showDialog(chip.getAeViewer().getFilterFrame(), "Select folder");
+        if (ret != JFileChooser.APPROVE_OPTION) {
+            return;
+        }
+        lastLoggingFolder = c.getSelectedFile().getPath();
+        putString("lastLoggingFolder", lastLoggingFolder);
+        tobiLogger= new TobiLogger(lastLoggingFolder+File.separator+"TimestampCalibrator","SystemTimeMillis dtClockNs dtTsUs");
+        tobiLogger.setAbsoluteTimeEnabled(true);
+        tobiLogger.setEnabled(true);// creates log file
+        tobiLogger.addComment("timestamp calibrator log file");
+        
+    }
+
     /**
      * @return the minTimeForCalibrationS
      */
@@ -171,7 +202,7 @@ public class TimestampCalibrator extends EventFilter2D implements FrameAnnotater
      */
     public void setMinTimeForCalibrationS(int minTimeForCalibrationS) {
         this.minTimeForCalibrationS = minTimeForCalibrationS;
-        putInt("minTimeForCalibrationS",minTimeForCalibrationS);
+        putInt("minTimeForCalibrationS", minTimeForCalibrationS);
     }
 
     /**
@@ -201,6 +232,21 @@ public class TimestampCalibrator extends EventFilter2D implements FrameAnnotater
     public void setCorrectTimestampEnabled(boolean correctTimestampEnabled) {
         this.correctTimestampEnabled = correctTimestampEnabled;
         putBoolean("correctTimestampEnabled", correctTimestampEnabled);
+    }
+
+    /**
+     * @return the loggingIntervalS
+     */
+    public int getLoggingIntervalS() {
+        return loggingIntervalS;
+    }
+
+    /**
+     * @param loggingIntervalS the loggingIntervalS to set
+     */
+    public void setLoggingIntervalS(int loggingIntervalS) {
+        this.loggingIntervalS = loggingIntervalS;
+        putInt("loggingIntervalS",loggingIntervalS);
     }
 
 }
