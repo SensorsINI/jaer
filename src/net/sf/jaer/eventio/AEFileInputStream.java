f6f599734afb07c26f10004f61552da80bd7517a
Tobi Delbruck <tobi@ini.uzh.ch>
2017-02-13 10:10:08 +0000

    fixed max chunk size for memory mapped file to be Integer.MAX_VALUE to avoid illegal argument exceptions when opening big DAVIS recordings.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@9502 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/net/sf/jaer/eventio/AEFileInputStream.java
+++ src/net/sf/jaer/eventio/AEFileInputStream.java
@@ -156,7 +156,7 @@ public class AEFileInputStream extends DataInputStream implements AEFileInputStr
      * the size of the memory mapped part of the input file. This window is
      * centered over the file position except at the start and end of the file.
      */
-    private long CHUNK_SIZE_EVENTS = Integer.MAX_VALUE;
+    private long CHUNK_SIZE_EVENTS = Integer.MAX_VALUE/EVENT32_SIZE;
     private long chunkSizeBytes = CHUNK_SIZE_EVENTS * EVENT32_SIZE; // size of memory mapped file chunk, depends on event 
     // size and number of events to map, initialized as
     // though we didn't have a file header. Max value is Integer.MAX_VALUE however. Will generate illegalargument exception if we try to allowcate larger chunk
