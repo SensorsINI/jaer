bef9a47edbd617c8de566b9831ae8a113813af47
Luca Longinotti <luca.longinotti@inilabs.com>
2017-02-24 11:07:21 +0000

    jAER: new chip class for Davis346mini camera.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/jAER/trunk@9578 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ src/eu/seebetter/ini/chips/davis/Davis346mini.java
@@ -0,0 +1,31 @@
+package eu.seebetter.ini.chips.davis;
+
+import java.awt.Point;
+
+import eu.seebetter.ini.chips.DavisChip;
+import net.sf.jaer.Description;
+import net.sf.jaer.DevelopmentStatus;
+import net.sf.jaer.graphics.AEFrameChipRenderer;
+import net.sf.jaer.hardwareinterface.HardwareInterface;
+
+@Description("DAVIS346 346x260 pixel APS-DVS DAVIS sensor (miniaturized version)")
+@DevelopmentStatus(DevelopmentStatus.Status.Experimental)
+public class Davis346mini extends Davis346BaseCamera {
+	public Davis346mini() {
+		setName("Davis346mini");
+		setDefaultPreferencesFile("biasgenSettings/Davis346b/DAVIS346b_Test.xml");
+
+		davisRenderer = new AEFrameChipRenderer(this);
+		davisRenderer.setMaxADC(DavisChip.MAX_ADC);
+		setRenderer(davisRenderer);
+
+		// Inverted with respect to other 346 cameras.
+		setApsFirstPixelReadOut(new Point(getSizeX() - 1, 0));
+		setApsLastPixelReadOut(new Point(0, getSizeY() - 1));
+	}
+
+	public Davis346mini(final HardwareInterface hardwareInterface) {
+		this();
+		setHardwareInterface(hardwareInterface);
+	}
+}
