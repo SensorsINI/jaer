49d590491ac246bd77aa64a0eb33936f07b0661f
Tobi Delbruck <todelbru@ethz.ch>
2017-05-31 10:34:30 +0200

    fixed so that wav files actually play and added sample sound for Main self test

--- src/net/sf/jaer/util/PlayWavFile.java
+++ src/net/sf/jaer/util/PlayWavFile.java
@@ -1,6 +1,9 @@
 package net.sf.jaer.util;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
@@ -13,121 +16,144 @@ import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
+
 /**
- * Plays sound samples which are specified by constructing the player and starting the thread.
- * 
+ * Plays sound samples which are specified by constructing the player and
+ * starting the thread. Note that this class loads the file from disk each time it plays it, so it is slow.
+ *
  * From http://www.anyexample.com/programming/java/java_play_wav_sound_file.xml.
  *
  * @author tobi
  *
  * This is part of jAER
-<a href="http://jaerproject.net/">jaerproject.net</a>,
-licensed under the LGPL (<a href="http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License">http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License</a>.
+ * <a href="http://jaerproject.net/">jaerproject.net</a>, licensed under the
+ * LGPL
+ * (<a href="http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License">http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License</a>.
+ * @see SoundWavFilePlayer for another class that keeps sound in memory
  */
-
 public class PlayWavFile extends Thread {
-    private static Logger log=Logger.getLogger("net.sf.jaer.util");
-	private String filename;
 
-	private Position curPosition;
+    private static Logger log = Logger.getLogger("net.sf.jaer.util");
+    private String filename;
 
-    /** The maximum buffer size: 128k samples. */
-	public final int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb
+    private Position curPosition;
 
-    /** A stereo position. */
-	public enum Position {
-		LEFT, RIGHT, NORMAL
-	};
-
-    /** Sets up to play a wav file at normal center position. To actually play the file, start the thread.
-     *
-     * @param wavfile filename (including path if not in startup folder), or resource path.
+    /**
+     * The maximum buffer size: 128k samples.
      */
-	public PlayWavFile(String wavfile) {
-		filename = wavfile;
-		curPosition = Position.NORMAL;
-	}
+    public final int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb
 
-    /** Sets up to play a wav file at specified stereo position. To actually play the file, start the thread.
+    /**
+     * A stereo position.
+     */
+    public enum Position {
+        LEFT, RIGHT, NORMAL
+    };
+
+    /**
+     * Sets up to play a wav file at normal center position. To actually play
+     * the file, start the thread.
      *
-     * @param wavfile filename (including path if not in startup folder), or resource path.
+     * @param wavfile filename (including path if not in startup folder), or
+     * resource path.
+     */
+    public PlayWavFile(String wavfile) {
+        filename = wavfile;
+        curPosition = Position.NORMAL;
+    }
+
+    /**
+     * Sets up to play a wav file at specified stereo position. To actually play
+     * the file, start the thread.
+     *
+     * @param wavfile filename (including path if not in startup folder), or
+     * resource path.
      * @param p the position.
      * @see Position
      */
-	public PlayWavFile(String wavfile, Position p) {
-		filename = wavfile;
-		curPosition = p;
-	}
+    public PlayWavFile(String wavfile, Position p) {
+        filename = wavfile;
+        curPosition = p;
+    }
 
-    /** This method plays the file. Start the thread to play. */
-	public void run() {
+    /**
+     * This method plays the file. Start the thread to play.
+     */
+    public void run() {
 
-		File soundFile = new File(filename);
-		if (!soundFile.exists()) {
-            InputStream is=getClass().getResourceAsStream(filename);
-            if(is==null){
+        InputStream is = null;
+        File soundFile = new File(filename);
+        try {
+            is = new FileInputStream(soundFile);
+        } catch (FileNotFoundException e) {
+            is = getClass().getResourceAsStream(filename);
+            if (is == null) {
                 log.warning("Wave file not found on filesystem or classpath: " + filename);
+                return;
             }
-			return;
-		}
+        }
 
-		AudioInputStream audioInputStream = null;
-		try {
-			audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-		} catch (UnsupportedAudioFileException e1) {
-			e1.printStackTrace();
-			return;
-		} catch (IOException e1) {
-			e1.printStackTrace();
-			return;
-		}
+        AudioInputStream audioInputStream = null;
+        try {
+            audioInputStream = AudioSystem.getAudioInputStream(is);
+        } catch (UnsupportedAudioFileException e1) {
+            e1.printStackTrace();
+            return;
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            return;
+        }
 
-		AudioFormat format = audioInputStream.getFormat();
-		SourceDataLine auline = null;
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+        AudioFormat format = audioInputStream.getFormat();
+        SourceDataLine auline = null;
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
 
-		try {
-			auline = (SourceDataLine) AudioSystem.getLine(info);
-			auline.open(format);
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-			return;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
+        try {
+            auline = (SourceDataLine) AudioSystem.getLine(info);
+            auline.open(format);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+            return;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
 
-		if (auline.isControlSupported(FloatControl.Type.PAN)) {
-			FloatControl pan = (FloatControl) auline
-					.getControl(FloatControl.Type.PAN);
-			if (curPosition == Position.RIGHT)
-				pan.setValue(1.0f);
-			else if (curPosition == Position.LEFT)
-				pan.setValue(-1.0f);
-		}
+        if (auline.isControlSupported(FloatControl.Type.PAN)) {
+            FloatControl pan = (FloatControl) auline
+                    .getControl(FloatControl.Type.PAN);
+            if (curPosition == Position.RIGHT) {
+                pan.setValue(1.0f);
+            } else if (curPosition == Position.LEFT) {
+                pan.setValue(-1.0f);
+            }
+        }
 
-		auline.start();
-		int nBytesRead = 0;
-		byte[] abData = new byte[EXTERNAL_BUFFER_SIZE];
+        auline.start();
+        int nBytesRead = 0;
+        byte[] abData = new byte[EXTERNAL_BUFFER_SIZE];
 
-		try {
-			while (nBytesRead != -1) {
-				nBytesRead = audioInputStream.read(abData, 0, abData.length);
-				if (nBytesRead >= 0)
-					auline.write(abData, 0, nBytesRead);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		} finally {
-			auline.drain();
-			auline.close();
-		}
+        try {
+            while (nBytesRead != -1) {
+                nBytesRead = audioInputStream.read(abData, 0, abData.length);
+                if (nBytesRead >= 0) {
+                    auline.write(abData, 0, nBytesRead);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        } finally {
+            auline.drain();
+            auline.close();
+        }
 
-	}
+    }
 
-    /** Tests the player with "sample.wav" in the startup folder. */
-    public static void main(String[] args){
+    /**
+     * Tests the player with "sample.wav" in the startup folder.
+     */
+    public static void main(String[] args) {
         new PlayWavFile("sample.wav").start();
     }
 }
