767d1bfb3ba83b29cb3620808c721864635e730a
Min Liu <minliu@ini.uzh.ch>
2017-06-06 13:59:39 +0200

    Added a file for importing opencv optical flow so it can be used to be compared with the patchmatchflow.

---
+++ src/ch/unizh/ini/jaer/projects/minliu/OpenCVFlow.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.unizh.ini.jaer.projects.minliu;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.opencv.core.Scalar;
+
+/**
+ *
+ * @author minliu
+ */
+public class OpenCVFlow {
+
+    // static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+
+    public static void main(String[] args) {
+    System.out.println("Welcome to OpenCV " + Core.VERSION);
+    Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
+    System.out.println("OpenCV Mat: " + m);
+    Mat mr1 = m.row(1);
+    mr1.setTo(new Scalar(1));
+    Mat mc5 = m.col(5);
+    mc5.setTo(new Scalar(5));
+    System.out.println("OpenCV Mat data:\n" + m.dump());
+  }
+    
+}
