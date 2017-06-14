073b19a90bd4ddefcaa45c6411448d4492864c4e
Tobi Delbruck <todelbru@ethz.ch>
2017-06-14 13:30:08 +0200

    added mean abs devivation as error metric

--- src/ch/unizh/ini/jaer/projects/rbodo/opticalflow/Measurand.java
+++ src/ch/unizh/ini/jaer/projects/rbodo/opticalflow/Measurand.java
@@ -3,19 +3,22 @@ package ch.unizh.ini.jaer.projects.rbodo.opticalflow;
 /**
  * Tracks mean and variance statistics for optical flow methods.
  *
- * @author rbodo
+ * @author rbodo / tobi delbruck
  */
 public class Measurand {
 
     /** Number of samples. */
-    private int n;
+    private int n=0;
 
-    /** Current mean. */
-    private float sum;
+    /** Current sum of values */
+    private float sum=0;
 
     // Sum of the square values.
-    private float sum2;
+    private float sum2=0;
 
+    // sum of absolute deviations from mean, for more robust statistic
+    private float sumdev=0;
+    
     public float getMean() {
         return sum / n;
     }
@@ -24,17 +27,28 @@ public class Measurand {
         float m=getMean();
         return (float) Math.sqrt((sum2 /n)- m*m);
     }
+    
+    /** Computes the mean absolute deviation of samples from mean value 
+     * 
+     * @return the mean absolute deviation
+     */
+    public float getMeanAbsDev(){
+        return sumdev/n;
+    }
 
     public void reset() {
         n = 0;
         sum = 0;
         sum2 = 0;
+        sumdev=0;
     }
 
     public void update(float x) {
+    // another possibility is to compute running average with https://stackoverflow.com/questions/28820904/how-to-efficiently-compute-average-on-the-fly-moving-average
         n++;
         sum += x;
         sum2 += x * x;
+        sumdev+=Math.abs(x-getMean());
     }
 
     @Override
@@ -47,12 +61,5 @@ public class Measurand {
      */
     public int getN() {
         return n;
-    }
-
-    /**
-     * @param n the n to set
-     */
-    private void setN(int n) {
-        this.n = n;
     }
 }
