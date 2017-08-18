71b93e6241a68df46489c05ba8c9068a4a10319f
Min Liu <minliu@ini.uzh.ch>
2017-08-19 01:21:59 +0200

    Fixed the read method based on hyperslab instead of using the fixed size data space. Added handle release code to release H5 sources. Fixed package import.

--- src/net/sf/jaer/eventio/Hdf5AedatFileInputReader.java
+++ src/net/sf/jaer/eventio/Hdf5AedatFileInputReader.java
@@ -5,33 +5,17 @@
  */
 package net.sf.jaer.eventio;
 
-import ch.systemsx.cisd.hdf5.HDF5DataClass;
-import ch.systemsx.cisd.hdf5.HDF5DataSetInformation;
-import ch.systemsx.cisd.hdf5.HDF5DataTypeInformation;
-import ch.systemsx.cisd.hdf5.HDF5FactoryProvider;
 import ch.systemsx.cisd.hdf5.IHDF5Reader;
 import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.DoubleBuffer;
-import java.nio.FloatBuffer;
-import java.nio.LongBuffer;
-import java.nio.ShortBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.BitSet;
-import java.util.Collection;
-import java.util.List;
 import java.util.logging.Logger;
 import ncsa.hdf.hdf5lib.H5;
 import ncsa.hdf.hdf5lib.HDF5Constants;
 import net.sf.jaer.chip.AEChip;
-import ncsa.hdf.hdf5lib.HDFArray;
 /**
  * Reads HDF5 AER data files
  * 
- * @author Tobi Delbruck
+ * @author Tobi Delbruck and Min Liu
  */
 public class Hdf5AedatFileInputReader  {
     protected static Logger log=Logger.getLogger("Hdf5AedatFileInputReader");
@@ -41,7 +25,8 @@ public class Hdf5AedatFileInputReader  {
        
         int file_id = -1;
         int dataset_id = -1;
-        int space = -1;
+        int eventPktSpace_id = -1;
+        int wholespace_id = -1;
         int ndims = 0;
         long dims[] = new long[2];
         long maxDims[] = new long[2];
@@ -61,8 +46,8 @@ public class Hdf5AedatFileInputReader  {
             
             
             if (dataset_id >= 0) {            
-                space = H5.H5Dget_space(dataset_id);
-                ndims = H5.H5Sget_simple_extent_dims(space, dims, maxDims);
+                wholespace_id = H5.H5Dget_space(dataset_id);
+                ndims = H5.H5Sget_simple_extent_dims(wholespace_id, dims, maxDims);
                 dcpl = H5.H5Dget_create_plist(dataset_id);
                 if( HDF5Constants.H5D_CHUNKED == H5.H5Pget_layout(dcpl)) {
                     H5.H5Pget_chunk(dcpl, ndims, chunkDims);
@@ -76,33 +61,78 @@ public class Hdf5AedatFileInputReader  {
             e.printStackTrace();
         }
         
-        // Allocate array of pointers to two-dimensional arrays (the
-        // elements of the dataset. String is a variable length array of char.
+
         long[] offset = {0, 0};
         long[] count = {1, 3};
         long[] stride = {1, 1};
         long[] block = {1, 1};
         int dataArrayLength = (int) dims[0] * (int) dims[1];
-        final String[] dataRead = new String[(int)(count[0] * count[1])];  
-        int filespace_id = H5.H5Screate_simple(2, count, null);
-
-        H5.H5Sselect_hyperslab(space, HDF5Constants.H5S_SELECT_SET, stride, stride, count, block);
- 
+        
+        // Create a new event packet data space to store one event packet.
+        eventPktSpace_id = H5.H5Screate_simple(2, new long[]{1,3}, null); 
+        long[] eventPktSpace_dims = new long[2];
+        H5.H5Sget_simple_extent_dims(eventPktSpace_id, eventPktSpace_dims, null);
+        
+        // Allocate array of pointers to two-dimensional arrays (the
+        // elements of the dataset. String is a variable length array of char.
+        final String[] eventPktData = new String[(int)(eventPktSpace_dims[0] * eventPktSpace_dims[1])]; 
+        
+        // Select the row data to read.
+        H5.H5Sselect_hyperslab(wholespace_id, HDF5Constants.H5S_SELECT_SET, offset, stride, count, block);
+        
         try {
-            if (space >= 0)
+            if (wholespace_id >= 0)
                 H5.H5Tdetect_class(memtype, HDF5Constants.H5T_STRING);
                 H5.H5DreadVL(dataset_id, memtype,
-                        HDF5Constants.H5S_ALL, filespace_id,
-                        HDF5Constants.H5P_DEFAULT, dataRead);
+                        HDF5Constants.H5S_ALL, wholespace_id,
+                        HDF5Constants.H5P_DEFAULT, eventPktData);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
+        // End access to the dataset and release resources used by it.
+        try {
+            if (dataset_id >= 0)
+                H5.H5Dclose(dataset_id);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        try {
+            if (eventPktSpace_id >= 0)
+                H5.H5Sclose(eventPktSpace_id);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        try {
+            if (wholespace_id >= 0)
+                H5.H5Sclose(wholespace_id);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Close the file.
+        try {
+            if (file_id >= 0)
+                H5.H5Fclose(file_id);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }        
     }
     
     public static void main(String[] args){
+        int[] libversion = new int[3];
+        H5.H5get_libversion(libversion);
+        System.out.println("The hdf5 library version is: " + 
+                String.valueOf(libversion[0]) + "." + String.valueOf(libversion[1]) + "."
+                + String.valueOf(libversion[2]) + ".");
         try {
             Hdf5AedatFileInputReader r=new Hdf5AedatFileInputReader(new File("rec1498945830.hdf5"),null);
-            
         } catch (IOException ex) {
             log.warning("caught "+ex.toString());
         }
