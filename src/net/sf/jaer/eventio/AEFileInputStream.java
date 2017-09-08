69aec08a11ec119f59b6ef15eb0bf42a55acafe2
Tobi Delbruck <todelbru@ethz.ch>
2017-09-08 12:55:19 +0200

    changed mapping size to make it 8x smaller so that it doesn't fill so much of RAM. reduced mark behind to 30k events so that mark can be set nearer to start of a file.

--- src/net/sf/jaer/eventio/AEFileInputStream.java
+++ src/net/sf/jaer/eventio/AEFileInputStream.java
@@ -110,7 +110,7 @@ public class AEFileInputStream extends DataInputStream implements AEFileInputStr
      * The offset of the marked position in events, to allow users to mark a
      * position before they hit the mark button
      */
-    public int MARK_OFFSET_EVENTS = 100000;
+    public int MARK_OFFSET_EVENTS = 30000;
 
     private PropertyChangeSupport support = new PropertyChangeSupport(this);
     static Logger log = Logger.getLogger("net.sf.jaer.eventio");
@@ -160,7 +160,7 @@ public class AEFileInputStream extends DataInputStream implements AEFileInputStr
      * the size of the memory mapped part of the input file. This window is
      * centered over the file position except at the start and end of the file.
      */
-    private long CHUNK_SIZE_EVENTS = Integer.MAX_VALUE / EVENT32_SIZE;
+    private long CHUNK_SIZE_EVENTS = Integer.MAX_VALUE / 8  / EVENT32_SIZE;
     private long chunkSizeBytes = CHUNK_SIZE_EVENTS * EVENT32_SIZE; // size of memory mapped file chunk, depends on event 
     // size and number of events to map, initialized as
     // though we didn't have a file header. Max value is Integer.MAX_VALUE however. Will generate illegalargument exception if we try to allowcate larger chunk
