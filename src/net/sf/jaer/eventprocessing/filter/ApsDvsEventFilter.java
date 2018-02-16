afe8b332b0128d9b7bdc507c4bfb6b5d899f3f11
Tobi Delbruck <todelbru@ethz.ch>
2018-02-16 14:13:03 +0100

    improved to allow filtering out events around all types of frame events

--- src/net/sf/jaer/eventprocessing/filter/ApsDvsEventFilter.java
+++ src/net/sf/jaer/eventprocessing/filter/ApsDvsEventFilter.java
@@ -29,16 +29,28 @@ public class ApsDvsEventFilter extends EventFilter2D {
     private boolean filterAPSEvents = getBoolean("filterAPSEvents", false);
     private boolean filterIMUSamples = getBoolean("filterIMUSamples", false);
     private boolean filterFrameTransientEvents = getBoolean("filterFrameTransientEvents", false);
+    private boolean startOfExposure = getBoolean("startOfExposure", true);
+    private boolean endOfExposure = getBoolean("endOfExposure", true);
+    private boolean startOfFrame = getBoolean("startOfFrame", true);
+    private boolean endOfFrame = getBoolean("endOfFrame", true);
     private int filterFrameTransientEventsTimeUs = getInt("filterFrameTransientEventsTimeUs", 2000);
-    private int lastFrameStartTimestamp = Integer.MIN_VALUE;
+    private int endOfExposureTime = Integer.MIN_VALUE;
+    private int startOfExposureTime = Integer.MIN_VALUE;
+    private int startOfFrameTime = Integer.MIN_VALUE;
+    private int endOfFrameTime = Integer.MIN_VALUE;
 
     public ApsDvsEventFilter(AEChip chip) {
         super(chip);
-        setPropertyTooltip("filterFrameTransientEventsTimeUs", "Events within this time in us after end-of-frame are filtered out; used for filtering DVS events possibly tirggered by APS global shutter");
-        setPropertyTooltip("filterFrameTransientEvents", "Filter out events caused by global shutter in DAVIS240b");
-        setPropertyTooltip("filterAPSEvents", "Filter out APS intensity samples");
-        setPropertyTooltip("filterDVSEvents", "Filter out DVS events");
-        setPropertyTooltip("filterIMUSamples", "Filter out IMU (intertial measurement unit) samples");
+        String types = "Event types", apsDvsCoupling = "Frame transients";
+        setPropertyTooltip(apsDvsCoupling, "filterFrameTransientEventsTimeUs", "Events within this time in us after end-of-frame are filtered out; used for filtering DVS events possibly tirggered by APS global shutter");
+        setPropertyTooltip(types, "filterAPSEvents", "Filter out APS intensity samples");
+        setPropertyTooltip(types, "filterDVSEvents", "Filter out DVS events");
+        setPropertyTooltip(types, "filterIMUSamples", "Filter out IMU (intertial measurement unit) samples");
+        setPropertyTooltip(apsDvsCoupling, "filterFrameTransientEvents", "Filter out events caused by global shutter in DAVIS240b");
+        setPropertyTooltip(apsDvsCoupling, "endOfFrame", "Filter out events just after this occurs");
+        setPropertyTooltip(apsDvsCoupling, "startOfFrame", "Filter out events just after this occurs");
+        setPropertyTooltip(apsDvsCoupling, "startOfExposure", "Filter out events just after this occurs");
+        setPropertyTooltip(apsDvsCoupling, "endOfExposure", "Filter out events just after this occurs");
     }
 
     @Override
@@ -53,12 +65,33 @@ public class ApsDvsEventFilter extends EventFilter2D {
         while (fullIterator.hasNext()) {
             ApsDvsEvent event = (ApsDvsEvent) fullIterator.next();
             if (event.isEndOfExposure()) {
-                lastFrameStartTimestamp = event.getTimestamp();
+                endOfExposureTime = event.getTimestamp();
+            } else if (event.isStartOfExposure()) {
+                startOfExposureTime = event.getTimestamp();
+
+            } else if (event.isStartOfFrame()) {
+                startOfFrameTime = event.getTimestamp();
+
+            } else if (event.isEndOfFrame()) {
+                endOfFrameTime = event.getTimestamp();
+
             }
-            if (  filterFrameTransientEvents && !event.isApsData() && !event.isImuSample() && lastFrameStartTimestamp != Integer.MIN_VALUE && event.getTimestamp() - lastFrameStartTimestamp < filterFrameTransientEventsTimeUs) {
-                continue;
+            if (filterFrameTransientEvents && !event.isApsData() && !event.isImuSample()) {
+                if (endOfExposure && endOfExposureTime != Integer.MIN_VALUE && event.getTimestamp() - endOfExposureTime < filterFrameTransientEventsTimeUs) {
+                    continue;
+                }
+                if (startOfExposure && startOfFrameTime != Integer.MIN_VALUE && event.getTimestamp() - startOfExposureTime < filterFrameTransientEventsTimeUs) {
+                    continue;
+                }
+                if (startOfFrame && startOfFrameTime != Integer.MIN_VALUE && event.getTimestamp() - startOfFrameTime < filterFrameTransientEventsTimeUs) {
+                    continue;
+                }
+                if (endOfFrame && endOfFrameTime != Integer.MIN_VALUE && event.getTimestamp() - endOfFrameTime < filterFrameTransientEventsTimeUs) {
+                    continue;
+                }
+
             }
-            if (filterAPSEvents && event.isApsData() /*&& !((event instanceof IMUSample) && !(((IMUSample) event).imuSampleEvent))*/)  {
+            if (filterAPSEvents && event.isApsData() /*&& !((event instanceof IMUSample) && !(((IMUSample) event).imuSampleEvent))*/) {
                 continue;
             }
             if (filterDVSEvents && !event.isApsData()) {
@@ -77,7 +110,7 @@ public class ApsDvsEventFilter extends EventFilter2D {
 
     @Override
     public void resetFilter() {
-        lastFrameStartTimestamp = Integer.MIN_VALUE;
+        endOfExposureTime = Integer.MIN_VALUE;
     }
 
     @Override
@@ -159,6 +192,66 @@ public class ApsDvsEventFilter extends EventFilter2D {
     public void setFilterIMUSamples(boolean filterIMUSamples) {
         this.filterIMUSamples = filterIMUSamples;
         putBoolean("filterIMUSamples", filterIMUSamples);
+    }
+
+    /**
+     * @return the startOfExposure
+     */
+    public boolean isStartOfExposure() {
+        return startOfExposure;
+    }
+
+    /**
+     * @param startOfExposure the startOfExposure to set
+     */
+    public void setStartOfExposure(boolean startOfExposure) {
+        this.startOfExposure = startOfExposure;
+        putBoolean("startOfExposure", startOfExposure);
+    }
+
+    /**
+     * @return the endOfExposure
+     */
+    public boolean isEndOfExposure() {
+        return endOfExposure;
+    }
+
+    /**
+     * @param endOfExposure the endOfExposure to set
+     */
+    public void setEndOfExposure(boolean endOfExposure) {
+        this.endOfExposure = endOfExposure;
+        putBoolean("endOfExposure", endOfExposure);
+    }
+
+    /**
+     * @return the startOfFrame
+     */
+    public boolean isStartOfFrame() {
+        return startOfFrame;
+    }
+
+    /**
+     * @param startOfFrame the startOfFrame to set
+     */
+    public void setStartOfFrame(boolean startOfFrame) {
+        this.startOfFrame = startOfFrame;
+        putBoolean("startOfFrame", startOfFrame);
+    }
+
+    /**
+     * @return the endOfFrame
+     */
+    public boolean isEndOfFrame() {
+        return endOfFrame;
+    }
+
+    /**
+     * @param endOfFrame the endOfFrame to set
+     */
+    public void setEndOfFrame(boolean endOfFrame) {
+        this.endOfFrame = endOfFrame;
+        putBoolean("endOfFrame", endOfFrame);
     }
 
 }
