1423b3a0919143484a7b9e97abe3eeaef3c3b56f
tobidelbruck <todelbru@ethz.ch>
2018-02-23 21:09:05 +0100

    fixed bugs in iniitialzing values

--- src/net/sf/jaer/util/filter/HighpassFilter.java
+++ src/net/sf/jaer/util/filter/HighpassFilter.java
@@ -1,40 +1,49 @@
 package net.sf.jaer.util.filter;
 
-/** A first-order highpass IIR filter.
- 
+/**
+ * A first-order highpass IIR filter.
+ *
  */
-public  class HighpassFilter extends Filter{
-    float lpVal=0, lastVal=0, value=0;
-    LowpassFilter lpFilter=new LowpassFilter();
-    
-    /** Applies a new input value at the time given and returns the new output value.
-     * 
+public class HighpassFilter extends Filter {
+
+    float lpVal = 0, lastVal = 0, value = 0;
+    LowpassFilter lpFilter = new LowpassFilter();
+
+    /**
+     * Applies a new input value at the time given and returns the new output
+     * value.
+     *
      * @param val the new sample
      * @param time the time in us
      * @return the filter output value
      */
     @Override
-    public float filter(float val, int time){
-        lpVal=lpFilter.filter(val,time);
-        lastVal=val;
-        value=val-lpVal;
+    public float filter(float val, int time) {
+        lpVal = lpFilter.filter(val, time);
+        lastVal = val;
+        value = val - lpVal;
+        initialized=false;
         return value;
     }
-    
+
     @Override
-    public String toString(){ return "HP tauMs="+tauMs+" lpVal="+lpVal+": "+lastVal+"->"+value; }
-    
+    public String toString() {
+        return "HP tauMs=" + tauMs + (initialized ? (" lpVal=" + lpVal + ": " + lastVal + "->" + value) : (" (unitialized)"));
+    }
+
     @Override
     public void setInternalValue(float value) {
-        this.value=value;
+        this.value = value;
         lpFilter.setInternalValue(value);
     }
-    
-    /** Returns internal lowpass filter value, i.e. offset or average DC of input.
-     * 
+
+    /**
+     * Returns internal lowpass filter value, i.e. offset or average DC of
+     * input.
+     *
      * @return the value of the lowpass filter.
      */
-    public float getInternalValue(){
+    public float getInternalValue() {
         return lpFilter.getValue();
     }
 
@@ -42,26 +51,26 @@ public  class HighpassFilter extends Filter{
     public float getValue() {
         return value;
     }
-    
-    @Override 
+
+    @Override
     public void setTauMs(float tauMs) {
         lpFilter.setTauMs(tauMs);
     }
+
     @Override
     public float getTauMs() {
         return lpFilter.getTauMs();
     }
 
-    /** Overridden to reset the enclosed lowpass filter.
-     * Apply this method to start from 0 output.
-     * 
+    /**
+     * Overridden to reset the enclosed lowpass filter. Apply this method to
+     * start from 0 output.
+     *
      */
     @Override
     public void reset() {
         super.reset();
         lpFilter.reset();
     }
-    
-    
-}
 
+}
