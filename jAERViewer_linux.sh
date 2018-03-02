ec90fcb855040da110362d45f557e378765a320f
Tobi Delbruck <Tobi Delbruck@tobidh87>
2018-03-02 09:30:38 +0100

    renamed linux launcher

---
+++ jAERViewer_linux.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# If you get "UnsatisfiedLinkError: /tmp/usb4java... failed to map segment from shared object: Operation not permitted",
+# add the following line to the command below:
+# -Djava.io.tmpdir=~/tmpdir/
+
+java \
+-classpath "dist/jAER.jar:jars/*:jars/javacv/*:jars/jogl/*:jars/usb4java/*:lib/*" \
+-splash:SplashScreen.gif \
+-Djava.util.logging.config.file=conf/Logging.properties -Dsun.java2d.noddraw=true -Dsun.java2d.opengl=false \
+net.sf.jaer.JAERViewer
