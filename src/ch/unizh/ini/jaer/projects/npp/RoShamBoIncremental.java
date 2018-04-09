9cb78e606e3d4d4d14345794bc6e83bd382cede0
tobi <tobi@tobi-dh87>
2018-04-09 17:09:48 +0200

    added UDP output to inform host/port that file is available and where it is on local host; added thread to read message from receiver

--- src/ch/unizh/ini/jaer/projects/npp/RoShamBoIncremental.java
+++ src/ch/unizh/ini/jaer/projects/npp/RoShamBoIncremental.java
@@ -18,14 +18,21 @@
  */
 package ch.unizh.ini.jaer.projects.npp;
 
-import java.io.File;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileFilter;
 import net.sf.jaer.Description;
 import net.sf.jaer.DevelopmentStatus;
 import net.sf.jaer.chip.AEChip;
+import static net.sf.jaer.eventprocessing.EventFilter.log;
 import net.sf.jaer.util.avioutput.DvsSliceAviWriter;
 
 /**
@@ -39,6 +46,14 @@ public class RoShamBoIncremental extends RoShamBoCNN {
 
     private DvsSliceAviWriter aviWriter = null;
     private Path lastSymbolsPath = Paths.get(getString("lastSymbolsPath", ""));
+    private DatagramChannel channel = null;
+    private DatagramSocket socket = null;
+    private InetSocketAddress client = null;
+    private String host = getString("hostname", "localhost");
+    public static final int DEFAULT_PORT = 14334;
+    private int port = getInt("port", DEFAULT_PORT);
+    private String NEW_SYMBOL_AVAILABLE = "newsymbol";
+    private Thread portListenerThread = null;
 
     public RoShamBoIncremental(AEChip chip) {
         super(chip);
@@ -50,6 +65,8 @@ public class RoShamBoIncremental extends RoShamBoCNN {
         setPropertyTooltip(learn, "LearnSymbol4", "Toggle collecting symbol data");
         setPropertyTooltip(learn, "LearnSymbol5", "Toggle collecting symbol data");
         setPropertyTooltip(learn, "ChooseSymbolsFolder", "Choose a folder to store the symbol AVI data files");
+        setPropertyTooltip(learn, "hostname", "learning host name (IP or DNS)");
+        setPropertyTooltip(learn, "port", "learning host port number");
         aviWriter = new DvsSliceAviWriter(chip);
         aviWriter.setEnclosed(true, this);
         aviWriter.setFrameRate(60);
@@ -68,46 +85,124 @@ public class RoShamBoIncremental extends RoShamBoCNN {
         if (ret != JFileChooser.APPROVE_OPTION) {
             return;
         }
-      
-        lastSymbolsPath = Paths.get(c.getSelectedFile().toString());
-        putString("lastSymbolsPath",lastSymbolsPath.toString());
 
+        lastSymbolsPath = Paths.get(c.getSelectedFile().toString());
+        putString("lastSymbolsPath", lastSymbolsPath.toString());
+
+    }
+
+    private void closeSymbolFileAndSendMessage() {
+        log.info("stopping symbol, starting training");
+        aviWriter.doCloseFile();
+        sendUDPMessage(NEW_SYMBOL_AVAILABLE + " " + aviWriter.getFile().toPath());
     }
 
     public void doToggleOnLearnSymbol0() {
-        log.info("recording symbol");
-        String sym="symbol0";
-        aviWriter.openAVIOutputStream(lastSymbolsPath.resolve(sym+".avi").toFile(), new String[]{"# "+sym});
+        openSymbolFileAndStartRecording("symbol0");
 
+    }
+
+    private void openSymbolFileAndStartRecording(String prefix) {
+        log.info("recording symbol");
+        aviWriter.openAVIOutputStream(lastSymbolsPath.resolve(prefix + ".avi").toFile(), new String[]{"# " + prefix});
     }
 
     public void doToggleOffLearnSymbol0() {
-        log.info("stopping symbol, starting training");
-        aviWriter.doCloseFile();
+        closeSymbolFileAndSendMessage();
     }
 
-   public void doToggleOnLearnSymbol1() {
-        log.info("recording symbol");
-        String sym="symbol1";
-        aviWriter.openAVIOutputStream(lastSymbolsPath.resolve(sym+".avi").toFile(), new String[]{"# "+sym});
+    public void doToggleOnLearnSymbol1() {
+        openSymbolFileAndStartRecording("symbol1");
 
     }
 
     public void doToggleOffLearnSymbol1() {
-        log.info("stopping symbol, starting training");
-        aviWriter.doCloseFile();
+        closeSymbolFileAndSendMessage();
     }
 
-   public void doToggleOnLearnSymbol2() {
-        log.info("recording symbol");
-        String sym="symbol2";
-        aviWriter.openAVIOutputStream(lastSymbolsPath.resolve(sym+".avi").toFile(), new String[]{"# "+sym});
-
+    public void doToggleOnLearnSymbol2() {
+        openSymbolFileAndStartRecording("symbol2");
     }
 
     public void doToggleOffLearnSymbol2() {
-        log.info("stopping symbol, starting training");
-        aviWriter.doCloseFile();
+        closeSymbolFileAndSendMessage();
     }
 
+    public String getHostname() {
+        return host;
+    }
+
+    /**
+     * You need to setHost before this will send events.
+     *
+     * @param host the host
+     */
+    synchronized public void setHostname(String host) {
+        this.host = host;
+//        if ( checkClient() ){
+        putString("hostname", host);
+//        }else{
+//            log.warning("checkClient() returned false, not storing "+host+" in preferences");
+//        }
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * You set the port to say which port the packet will be sent to.
+     *
+     * @param port the UDP port number.
+     */
+    public void setPort(int port) {
+        this.port = port;
+        putInt("port", port);
+    }
+
+    private void sendUDPMessage(String string) {
+        if (channel == null) {
+            try {
+                channel = DatagramChannel.open();
+                if (portListenerThread == null || !portListenerThread.isAlive()) {
+                    portListenerThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ByteBuffer udpBuf = ByteBuffer.allocate(1024);
+
+                            InetSocketAddress localSocketAddress = new InetSocketAddress(host, port);
+                            try {
+                                channel.bind(localSocketAddress);
+                            } catch (IOException ex) {
+                                log.warning("couldn't bind local socket address: " + ex.toString());
+                                return;
+                            }
+                            log.info("opened channel on local port " + localSocketAddress + " to receive UDP messages from learning server.");
+                            while (true) {
+                                try {
+                                    channel.receive(udpBuf);
+                                    udpBuf.flip();
+                                    String msg=new String(udpBuf.array());
+                                    log.info("got message:"+msg);
+                                } catch (IOException ex) {
+                                    log.warning("exception in recieving message: " + ex.toString());
+                                }
+                            }
+                        }
+                    }, "RoShamBoIncremental Listener");
+                }
+                portListenerThread.start();
+            } catch (IOException ex) {
+                log.warning("cannot open channel: " + ex.toString());
+                return;
+            }
+        }
+        try {
+            Socket socket = new Socket(host, port);
+            ByteBuffer b = ByteBuffer.wrap(string.getBytes());
+            channel.send(b, socket.getRemoteSocketAddress());
+        } catch (IOException ex) {
+            log.warning(String.format("socket exception for host=%s, port=%d: %s", host, port, ex.toString()));
+        }
+    }
 }
