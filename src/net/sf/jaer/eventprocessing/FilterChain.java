badf43c90a02b1f28f515821b7da9930a6830c68
Tobi Delbruck <Tobi Delbruck@tobidh87>
2018-05-03 10:05:43 +0200

    when new filters are added or the filter chain is changed by using classchooser, now each filter's initFilter method is called after the filter is constructed.

--- src/net/sf/jaer/eventprocessing/FilterChain.java
+++ src/net/sf/jaer/eventprocessing/FilterChain.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.LinkedList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
@@ -428,6 +429,14 @@ public class FilterChain extends LinkedList<EventFilter2D> {
             }
         } catch (Exception e) {
             log.warning(e.getMessage());
+        }
+        // call initFilter on all filters in the chain
+        for(EventFilter f:this){
+            try{
+                f.initFilter();
+            }catch(Exception e){
+                log.log(Level.WARNING,"Exception initializing filter "+f+": "+e.toString(),e);
+            }
         }
     }
 
