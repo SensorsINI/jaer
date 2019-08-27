7a09a86b768f647860ed31bf7ff7ce31646a3fb3
Tobi Delbruck <Tobi Delbruck@tobidh87>
2019-08-27 15:07:45 +0200

    changed the way that PathPoints are created, now uses factory method that can be overridden for custom path points holding additional statistics

--- src/net/sf/jaer/eventprocessing/tracking/ClusterPathPoint.java
+++ src/net/sf/jaer/eventprocessing/tracking/ClusterPathPoint.java
@@ -9,42 +9,46 @@ import java.awt.geom.Point2D;
 public class ClusterPathPoint extends Point2D.Float {
 
     /** timestamp of this point. */
-    public int t;
+    protected int t;
     /** Number of events that contributed to this point. */
-    private int nEvents;
+    protected int nEvents;
     /** Velocity of cluster (filtered) at this point in pixels per timestamp tick (e.g. us).
      * This field is initially null and is initialized by the velocityPPT estimation, if used. */
     public Point2D.Float velocityPPT=null;
     /** disparity of stereo vision. Valid for stereo vision only */
     public float stereoDisparity;
     /** Measured size (average radius) of cluster */
-    public float averageRadius=-1;
+    protected float radiusPixels=-1;
     
-    public ClusterPathPoint(float x, float y, int t, int numEvents) {
+    /** Protected constructor to force use of factory method that can be overridden 
+     * 
+     * @param x
+     * @param y
+     * @param t 
+     */
+    protected ClusterPathPoint(float x, float y, int t) {
         super();
         this.x = x;
         this.y = y;
         this.t = t;
-        this.nEvents = numEvents;
-    }
-    
-    /** Constructor that sets average radius */
-    public ClusterPathPoint(float x, float y, int t, int numEvents, float averageRadius) {
-        super();
-        this.x = x;
-        this.y = y;
-        this.t = t;
-        this.nEvents = numEvents;
-        this.averageRadius = averageRadius;
     }
 
+    /** Factory method that subclasses can override to create custom path points, e.g. for storing different statistics
+     * 
+     * @return a new ClusterPathPoint with x,y,t set. Other fields must be set using methods.
+     */
+    public static ClusterPathPoint createPoint(float x, float y, int t){
+        return new ClusterPathPoint(x, y, t);
+    }
+    
+    /** Get time of point 
+     * 
+     * @return timestamp (typically us)
+     */
     public int getT() {
         return t;
     }
-   /** Returns number of events that contributed to this point. 
-    * 
-    * @return number of events
-    */
+   
     public int getNEvents() {
         return nEvents;
     }
@@ -59,6 +63,42 @@ public class ClusterPathPoint extends Point2D.Float {
 
     @Override
     public String toString() {
-        return String.format("%d, %f, %f, %d, %f", t, x, y, nEvents,averageRadius);
+        return String.format("%d, %f, %f, %d, %f", t, x, y, nEvents,radiusPixels);
+    }
+
+    /**
+     * @param t the t to set
+     */
+    public void setT(int t) {
+        this.t = t;
+    }
+
+  /** Returns number of events that contributed to this point. 
+    * 
+    * @return number of events
+    */
+    public int getnEvents() {
+        return nEvents;
+    }
+
+    /**
+     * @param nEvents the nEvents to set
+     */
+    public void setnEvents(int nEvents) {
+        this.nEvents = nEvents;
+    }
+
+    /**
+     * @return the radiusPixels
+     */
+    public float getRadiusPixels() {
+        return radiusPixels;
+    }
+
+    /**
+     * @param radiusPixels the radiusPixels to set
+     */
+    public void setRadiusPixels(float radiusPixels) {
+        this.radiusPixels = radiusPixels;
     }
 }
