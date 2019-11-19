61f412060032af8d636209888ada703d194558b2
Tobi Delbruck <Tobi Delbruck@tobidh87>
2019-11-19 10:53:32 +0100

    fixed for lib setup and using auto dir for jaer path

--- dvs-slice-avi-writer.sh
+++ dvs-slice-avi-writer.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # If you get "UnsatisfiedLinkError: /tmp/usb4java... failed to map segment from shared object: Operation not permitted",
 # add the following line to the command below:
 # -Djava.io.tmpdir=~/tmpdir/
 
-JHOME="/media/iulialexandra/Storage/packages/jaer/code/jAER/trunk"
-java -classpath "${JHOME}/dist/jAER.jar:${JHOME}/jars/*:${JHOME}/jars/javacv/*:${JHOME}/jars/jogl/*:${JHOME}/jars/usb4java/*" -Dsun.java2d.noddraw=true -Dsun.java2d.opengl=false net.sf.jaer.util.avioutput.DvsSliceAviWriter "$@"
+# from https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# change JAERHOME to your jAER top folder, i.e. the one that holds this script, if DIR doesn't work
+JAERHOME="$DIR"
+java -classpath "${JAERHOME}/dist/jAER.jar:${JAERHOME}/jars/*:${JAERHOME}/lib/*" -Dsun.java2d.noddraw=true -Dsun.java2d.opengl=false net.sf.jaer.util.avioutput.DvsSliceAviWriter "$@"
