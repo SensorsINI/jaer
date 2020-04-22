f2f60188ee0a9d873b4a855fa3f3d595e6fd8047
Tobi Delbruck <Tobi Delbruck@tobidh87>
2020-04-22 21:03:55 +0200

    fixed script to run from anywhere

--- jAERViewer_linux.sh
+++ jAERViewer_linux.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # If you get "UnsatisfiedLinkError: /tmp/usb4java... failed to map segment from shared object: Operation not permitted",
 # add the following line to the command below:
@@ -7,9 +7,12 @@
 # If you're using AMD graphics cards on Linux with the latest open-source Mesa drivers, you might get an
 # error about not finding a supported OpenGL profile, to fix please change the classpath line below to:
 # -classpath "dist/jAER.jar:jars/*:jars/jogl_mesa_amd_fix/*:lib/*" \
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#DIR=`dirname "$0`
 
 java \
--classpath "dist/jAER.jar:jars/*:lib/*" \
+-classpath "$DIR/dist/jAER.jar:$DIR/jars/*:$DIR/lib/*" \
 -splash:images/SplashScreen.gif \
--Djava.util.logging.config.file=conf/Logging.properties -Dsun.java2d.noddraw=true -Dsun.java2d.opengl=false \
+-Dsun.java2d.uiScale=2.5 \
+-Djava.util.logging.config.file="$DIR/conf/Logging.properties" -Dsun.java2d.noddraw=true -Dsun.java2d.opengl=false \
 net.sf.jaer.JAERViewer
