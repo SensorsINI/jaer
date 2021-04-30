74d70133ec1e9c21bdbb5f75c35c7bb116a16163
Tobi Delbruck <Tobi Delbruck@tobidh87>
2021-04-30 10:52:29 +0200

    refactored computation of ROI values, added CSV output

--- src/au/edu/wsu/PolarizationComplementaryFilter.java
+++ src/au/edu/wsu/PolarizationComplementaryFilter.java
@@ -15,6 +15,8 @@ import ch.unizh.ini.jaer.projects.davis.frames.DavisComplementaryFilter;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import net.sf.jaer.util.DrawGL;
+import net.sf.jaer.util.TobiLogger;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 /**
  * Extracts Polarization information using Cedric's complementary filter to
@@ -34,6 +36,9 @@ public class PolarizationComplementaryFilter extends DavisComplementaryFilter {
     private float[] aop;
     private float[] dop;
     FloatFunction exp = (s) -> (float) Math.exp(s);
+    private TobiLogger tobiLogger = new TobiLogger("PolarizationComplementaryFilter", "PolarizationComplementaryFilter");
+    private DescriptiveStatistics aopStats = new DescriptiveStatistics(), dopStats = new DescriptiveStatistics(); // mean values, computed after the ROI is processed
+    private float meanAoP = Float.NaN, meanDoP = Float.NaN;
 
     public PolarizationComplementaryFilter(final AEChip chip) {
         super(chip);
@@ -49,13 +54,15 @@ public class PolarizationComplementaryFilter extends DavisComplementaryFilter {
             }
         });
         initFilter();
+        tobiLogger.setColumnHeaderLine("lastTimestamp(us),ROINumPixels, AoP(deg),AoPStd(deg),DoLP,DoLPStd"); // CSV columns, not including the first column which is system time in ms since epoch
+        setPropertyTooltip("writePolarizationCSV", "Write a CSV file with the the mean and std of polarization AoP and DoLP for the ROI");
     }
 
     @Override
     public EventPacket<? extends BasicEvent> filterPacket(EventPacket<? extends BasicEvent> in) {
         checkMaps();
         in = super.filterPacket(in);
-        displayPreBuffer();
+        computeAndShowPolarization(in);
         if (showAPSFrameDisplay) {
             apsDisplayPola.repaint();
         }
@@ -72,7 +79,7 @@ public class PolarizationComplementaryFilter extends DavisComplementaryFilter {
     @Override
     public void initFilter() {
         super.initFilter();
-        if(maxIDX > 0){
+        if (maxIDX > 0) {
             indexf0 = new int[maxIDX];
             indexf45 = new int[maxIDX];
             indexf90 = new int[maxIDX];
@@ -85,8 +92,8 @@ public class PolarizationComplementaryFilter extends DavisComplementaryFilter {
             PolarizationUtils.drawLegend(apsDisplayPixmapBufferAop, height, width);
         }
     }
-    
-    public void displayPreBuffer() {
+
+    private void computeAndShowPolarization(EventPacket<? extends BasicEvent> in) {
         if (maxIDX != indexf0.length && maxIDX > 0) {
             indexf0 = new int[maxIDX];
             indexf45 = new int[maxIDX];
@@ -99,7 +106,34 @@ public class PolarizationComplementaryFilter extends DavisComplementaryFilter {
             PolarizationUtils.fillIndex(indexf0, indexf45, indexf90, indexf135, height, width);
             PolarizationUtils.drawLegend(apsDisplayPixmapBufferAop, height, width);
         }
+
+        // compute the AoP and DoLP in the ROI
         PolarizationUtils.computeAoPDoP(logFinalFrame, aop, dop, exp, indexf0, indexf45, indexf90, indexf135, height, width);
+
+        if (roiRect != null) {
+            // compute mean values
+            int nb = 0, idx;
+            aopStats.clear();
+            dopStats.clear();
+            for (double x = roiRect.getCenterX() - roiRect.getWidth() / 2; x < roiRect.getCenterX() + roiRect.getWidth() / 2; x += 2) {
+                for (double y = roiRect.getCenterY() - roiRect.getHeight() / 2; y < roiRect.getCenterY() + roiRect.getHeight() / 2; y += 2) {
+                    idx = (int) (x / 2 + y / 2 * width / 2);
+                    aopStats.addValue(aop[idx]);
+                    dopStats.addValue(dop[idx]);
+                    nb += 1;
+                }
+            }
+            meanAoP = (float) aopStats.getMean();
+            meanDoP = (float) dopStats.getMean();
+
+            // log the mean values to the CSV if open, should match the header line 
+            if (tobiLogger.isEnabled()) {
+                tobiLogger.log(String.format("%d,%d, %f,%f,%f,%f", in.getLastTimestamp(), nb, meanAoP, aopStats.getStandardDeviation(), meanDoP, dopStats.getStandardDeviation()));
+            }
+        }
+
+//        System.out.printf("Angle: %f", m_aop * 180);
+        // Show the polarization display; the ROI values are shown in annotate
         PolarizationUtils.setDisplay(apsDisplayPixmapBufferAop, aop, dop, height, width);
         apsDisplayPola.setPixmapArray(apsDisplayPixmapBufferAop);
 
@@ -108,15 +142,15 @@ public class PolarizationComplementaryFilter extends DavisComplementaryFilter {
 //        if(apsDisplayPixmapBufferAop.length > 0)
 //            apsDisplayPola.setPixmapArray(apsDisplayPixmapBufferAop);
     }
-    
-    public float getDoP(int x, int y){
+
+    public float getDoP(int x, int y) {
         return dop[getIndex(x, y)];
     }
 
-    public float getAoP(int x, int y){
+    public float getAoP(int x, int y) {
         return aop[getIndex(x, y)];
     }
-    
+
     @Override
     public void annotate(GLAutoDrawable drawable) {
         super.annotate(drawable); // draws the ROI selection rectangle
@@ -129,24 +163,19 @@ public class PolarizationComplementaryFilter extends DavisComplementaryFilter {
         gl.glColor3f(1, 1, 1);  // set the RGB color
         gl.glLineWidth(3); // set the line width in screen pixels
         // draw the polarization ellipse
-        float m_aop = 0, m_dop = 0;
-        int nb = 0, idx;
-        for(double x = roiRect.getCenterX() - roiRect.getWidth() / 2; x < roiRect.getCenterX() + roiRect.getWidth() / 2; x +=2){
-            for (double y = roiRect.getCenterY() - roiRect.getHeight() / 2; y < roiRect.getCenterY() + roiRect.getHeight() / 2; y +=2){
-                idx = (int)(x / 2 + y / 2 * width / 2);
-                m_aop += aop[idx];
-                m_dop += dop[idx];
-                nb += 1;
-            }
-        }
-        if(nb > 0){
-            m_aop /= nb;
-            m_dop /= nb;
-        }
-        System.out.printf("Angle: %f", m_aop * 180);   
+
         gl.glPushMatrix();
-        DrawGL.drawEllipse(gl, (float) roiRect.getCenterX(), (float) roiRect.getCenterY(), (float)(20), (float)( 20 * (1 - m_dop)), m_aop * 2.0f, 32);
+        DrawGL.drawEllipse(gl, (float) roiRect.getCenterX(), (float) roiRect.getCenterY(), (float) (20), (float) (20 * (1 - meanDoP)), meanAoP * 2.0f, 32);
         gl.glPopMatrix();
+    }
+
+    public void doToggleOnWritePolarizationCSV() {
+        tobiLogger.setEnabled(true);
+    }
+
+    public void doToggleOffWritePolarizationCSV() {
+        tobiLogger.setEnabled(false);
+        tobiLogger.showFolderInDesktop();
     }
 
 }
