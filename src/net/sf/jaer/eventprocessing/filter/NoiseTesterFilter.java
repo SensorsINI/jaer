fc74f7369fa6864c9de200f9f2beb2a5ad082173
Tobi Delbruck <Tobi Delbruck@tobidh87>
2021-06-08 13:34:40 +0200

    improve status text to use eng format

--- src/net/sf/jaer/eventprocessing/filter/NoiseTesterFilter.java
+++ src/net/sf/jaer/eventprocessing/filter/NoiseTesterFilter.java
@@ -230,7 +230,7 @@ public class NoiseTesterFilter extends AbstractNoiseFilter implements FrameAnnot
         }
         glut.glutBitmapString(GLUT.BITMAP_HELVETICA_18, s);
         gl.glRasterPos3f(0, getAnnotationRasterYPosition("NTF"), 0);
-        s = String.format("TPR=%s%% TNR=%s%% dT=%.2fus", eng.format(100 * TPR), eng.format(100 * TNR), poissonDtUs);
+        s = String.format("TPR=%s%% FPR=%s%% TNR=%s%% dT=%.2fus", eng.format(100 * TPR),eng.format(100 * (1-TNR)), eng.format(100 * TNR), poissonDtUs);
         glut.glutBitmapString(GLUT.BITMAP_HELVETICA_18, s);
         gl.glRasterPos3f(0, getAnnotationRasterYPosition("NTF") + 10, 0);
         s = String.format("In sigRate=%s noiseRate=%s, Out sigRate=%s noiseRate=%s Hz", eng.format(inSignalRateHz), eng.format(inNoiseRateHz), eng.format(outSignalRateHz), eng.format(outNoiseRateHz));
