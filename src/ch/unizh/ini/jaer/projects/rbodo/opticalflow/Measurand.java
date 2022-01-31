241615f5cd6143f2be5790aa8ac62884845fafcb
Tobi Delbruck <Tobi Delbruck@tobidh87>
2022-01-31 22:30:27 +0100

    added median statistic for all errors to disregard outliers more

--- src/ch/unizh/ini/jaer/projects/rbodo/opticalflow/Measurand.java
+++ src/ch/unizh/ini/jaer/projects/rbodo/opticalflow/Measurand.java
@@ -41,7 +41,7 @@ public class Measurand extends DescriptiveStatistics {
     }
     public String graphicsString(String header, String units) {
         Point2D.Float q = getQuartileErrors();
-        return String.format("%s mean: %4.2f quartiles:[%.2f,%.2f] %s [N=%,d]", header, getMean(), q.x, q.y, units,getN());
+        return String.format("%s mean: %4.2f median: %4.2f quartiles:[%.2f,%.2f] %s [N=%,d]", header, getMean(), getMedian(), q.x, q.y, units,getN());
     }
 
     public static void main(String[] args) {
