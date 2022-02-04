2b7388bb31f5276c5c59937b54ec3f35cd7e7199
Tobi Delbruck <Tobi Delbruck@tobidh87>
2022-02-04 09:19:23 +0100

    fixed accuracy annotation to include outliers and increased scale

--- src/ch/unizh/ini/jaer/projects/rbodo/opticalflow/AbstractMotionFlowIMU.java
+++ src/ch/unizh/ini/jaer/projects/rbodo/opticalflow/AbstractMotionFlowIMU.java
@@ -1378,9 +1378,9 @@ abstract public class AbstractMotionFlowIMU extends EventFilter2DMouseAdaptor im
         if (measureAccuracy) {
             MultilineAnnotationTextRenderer.resetToYPositionPixels(-20);
 //            MultilineAnnotationTextRenderer.setDefaultScale();
-            MultilineAnnotationTextRenderer.setScale(.4f);
+            MultilineAnnotationTextRenderer.setScale(.6f);
 //            MultilineAnnotationTextRenderer.setFontSize(24);
-            String s = String.format("Accuracy statistics:%n%s%n%s%n%s",
+            String s = String.format("Accuracy statistics:%n%s%n%s%n%s%n%s",
                     motionFlowStatistics.endpointErrorAbs.graphicsString("AEE:", "px/s"),
                     motionFlowStatistics.endpointErrorRel.graphicsString("AREE:", "%"),
                     motionFlowStatistics.angularError.graphicsString("AAE:", "deg"),
