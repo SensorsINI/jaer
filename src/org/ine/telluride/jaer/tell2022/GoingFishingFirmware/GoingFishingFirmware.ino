a9619d6181c8fcc0e30d71a75f71eb45581825af
Tobi Delbruck <Tobi Delbruck@tobidh87>
2022-07-12 21:19:51 -0600

    fixed some bugs with disabling, turn on LED when servos active

--- src/org/ine/telluride/jaer/tell2022/GoingFishingFirmware/GoingFishingFirmware.ino
+++ src/org/ine/telluride/jaer/tell2022/GoingFishingFirmware/GoingFishingFirmware.ino
@@ -7,35 +7,40 @@ bool disabled = true;
 byte bytes[3];
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
   theta.attach(12);
   z.attach(9);
-
-  Serial.begin(115200);
-  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  theta.write(90);
+  z.write(90);
+  delay(300);
   theta.detach();
   z.detach();
-
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
   while (Serial.available() < 3)
     return;
-  if (Serial.readBytes(bytes, 3) == 3) {
-    bool disable = bytes[0];
-    int thetaDeg = bytes[1];
-    int zDeg = bytes[2];
-    if (disable) {
-      disabled = true;
-      theta.detach();
-      z.detach();
-    } else {
-      if (disabled) {
-        theta.attach(12);
-        z.attach(9);
-        disabled = false;
-      }
-      theta.write(thetaDeg);
-      z.write(zDeg);
+  if (Serial.readBytes(bytes, 3) < 3)
+    return;
+  bool disable = bytes[0];
+  int thetaDeg = bytes[1];
+  int zDeg = bytes[2];
+  if (disable) {
+    disabled = true;
+    theta.detach();
+    z.detach();
+    digitalWrite(LED_BUILTIN, LOW);
+  } else {
+    if (disabled) {
+      theta.attach(12);
+      z.attach(9);
+      disabled = false;
+      digitalWrite(LED_BUILTIN, HIGH);
     }
+    theta.write(thetaDeg);
+    z.write(zDeg);
   }
 }
