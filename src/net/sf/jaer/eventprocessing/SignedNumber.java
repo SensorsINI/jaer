2cb5d17cd158d3e0561bc48fa3b297ae133c9995
Tobi Delbruck <Tobi Delbruck@tobidh87>
2022-09-26 13:39:55 +0200

    added annotation class for signed numbers so that we can treat mouse wheel and arrow key actions differently

---
+++ src/net/sf/jaer/eventprocessing/SignedNumber.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2022 tobid.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package net.sf.jaer.eventprocessing;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Signals that the setter method refers to a signed quantity (int or float). Used in EventFilter's to indicate that the GUI control should act like a singed control.
+ * @author tobid
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface SignedNumber {
+    
+}
