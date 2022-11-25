4811554c5bb228e52a99ac5f768d2305c15aae3f
Tobi Delbruck <Tobi Delbruck@tobidh87>
2022-11-25 15:17:24 +0100

    changed to 44kHz and 5ms spike sounds to make sounds work on linux ubuntu 22

--- src/net/sf/jaer/util/SpikeSound.java
+++ src/net/sf/jaer/util/SpikeSound.java
@@ -29,9 +29,9 @@ public class SpikeSound{
     };
     SoundShape shape = SoundShape.ROUNDED;
     /** duration of spike sound in ms */
-    public static final int SPIKE_DURATION_MS = 2;
+    public static final int SPIKE_DURATION_MS = 5;
     /** sample rate in Hz */
-    public static final float SAMPLE_RATE = 8000f; // 44100; // tobi changed to try on linux since 8000f doesn't seem to work 8000f;
+    public static final float SAMPLE_RATE = 44100f; // 44100; // tobi changed to try on linux since 8000f doesn't seem to work 8000f;
     /** length of spike sound sample in samples.  Duration of spike sound is BUFFER_LENGTH/{@link #SAMPLE_RATE}.
      * e.g., with a sample rate of 4kHz, 8 sample spikes make the spikes 2ms long.  This seems about right.
      */
