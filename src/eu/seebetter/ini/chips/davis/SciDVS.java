d905ca9691c9025ad0b6b7c72154e8327e071c0a
Tobi Delbruck <Tobi Delbruck@tobidh87>
2024-03-03 11:20:00 +0100

    add SciDvs and SciDVSConfig from scidvs branch. Note that it will not currently run SciDVS because the correct HardwareInterface is not correctly assigned to SciDVS since the VID/PID is exactly the same as Davis. Therefore the translateEvents method will be the wrong one. But it can be used to play back recordings from SciDVS.

---
+++ src/eu/seebetter/ini/chips/davis/SciDVS.java
@@ -0,0 +1,40 @@
+package eu.seebetter.ini.chips.davis;
+
+import java.awt.Point;
+
+import eu.seebetter.ini.chips.DavisChip;
+import net.sf.jaer.Description;
+import net.sf.jaer.DevelopmentStatus;
+import net.sf.jaer.graphics.DavisRenderer;
+import net.sf.jaer.hardwareinterface.HardwareInterface;
+
+@Description("SciDVS 126x112 pixel with APS-DVS DAVIS sensor")
+@DevelopmentStatus(DevelopmentStatus.Status.Experimental)
+public class SciDVS extends DavisBaseCamera {
+
+	public static final short WIDTH_PIXELS = 112;
+	public static final short HEIGHT_PIXELS = 126;
+
+	public SciDVS() {
+		setName("SciDVS");
+		setDefaultPreferencesFile("biasgenSettings/SciDVS/SciDVS_Test.xml");
+
+		setSizeX(SciDVS.WIDTH_PIXELS);
+		setSizeY(SciDVS.HEIGHT_PIXELS);
+
+		davisRenderer = new DavisRenderer(this);
+		davisRenderer.setMaxADC(DavisChip.MAX_ADC);
+		setRenderer(davisRenderer);
+
+		setApsFirstPixelReadOut(new Point(0, getSizeY() - 1));
+		setApsLastPixelReadOut(new Point(getSizeX() - 1, 0));
+
+		setBiasgen(davisConfig = new SciDVSConfig(this));
+	}
+	
+
+	public SciDVS(final HardwareInterface hardwareInterface) {
+		this();
+		setHardwareInterface(hardwareInterface);
+	}
+}
