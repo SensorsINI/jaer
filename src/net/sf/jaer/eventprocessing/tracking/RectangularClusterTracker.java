896bf05f9a81828752ba7e45a75a47d74cbf33e9
Tobi Delbruck <Tobi Delbruck@tobidh87>
2024-12-13 08:57:09 +0100

    add null check for log level

--- src/net/sf/jaer/eventprocessing/tracking/RectangularClusterTracker.java
+++ src/net/sf/jaer/eventprocessing/tracking/RectangularClusterTracker.java
@@ -807,6 +807,7 @@ public class RectangularClusterTracker extends EventFilter2D
     }
 
     private static boolean isFineLoggingLevel() {
+        if (log.getLevel()==null) return false;
         return log.getLevel().intValue() < Level.INFO.intValue();
     }
 
