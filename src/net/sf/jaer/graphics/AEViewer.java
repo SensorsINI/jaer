545f5578b8c3cd28e767e6b2d647f5d56746d4c2
Tobi Delbruck <Tobi Delbruck@tobidh87>
2024-12-14 12:37:52 +0100

    improved output while saving recording to a different filesystem

--- src/net/sf/jaer/graphics/AEViewer.java
+++ src/net/sf/jaer/graphics/AEViewer.java
@@ -4873,7 +4873,7 @@ public class AEViewer extends javax.swing.JFrame implements PropertyChangeListen
                                         }
                                     };
                                     t.start();
-                                    StringBuilder sb = new StringBuilder("Still saving.");
+                                    StringBuilder sb = new StringBuilder("Saving..");
                                     while (t.isAlive()) {
                                         try {
                                             Thread.sleep(500);
@@ -4883,8 +4883,11 @@ public class AEViewer extends javax.swing.JFrame implements PropertyChangeListen
                                         log.info(sb.toString());
                                     }
                                     if (result.exception == null) {
+                                        log.info("done saving "+newFinalFile.getAbsolutePath());
                                         savedIt = true;
                                         loggingFile = newFile;
+                                    }else{
+                                        log.severe(String.format("Could not save %s: %s",newFinalFile,result.exception));
                                     }
                                 }
                             }
