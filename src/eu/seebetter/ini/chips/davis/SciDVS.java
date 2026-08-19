478740fd491a78c940832ab4e0b795645de2932e
Rui Graça <rpgraca@ini.ethz.ch>
2026-08-19 03:48:58 +0200

    phase4-scidvs: restore canonical sensitive bias default
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/eu/seebetter/ini/chips/davis/SciDVS.java
+++ src/eu/seebetter/ini/chips/davis/SciDVS.java
@@ -23,7 +23,7 @@ public class SciDVS extends DavisBaseCamera {
 
 	public SciDVS() {
 		setName("SciDVS");
-		setDefaultPreferencesFile(null);
+		setDefaultPreferencesFile("biasgenSettings/SciDVS/SciDVS_sensitive_highVgMfb.xml");
 
 		setSizeX(SciDVS.WIDTH_PIXELS);
 		setSizeY(SciDVS.HEIGHT_PIXELS);
