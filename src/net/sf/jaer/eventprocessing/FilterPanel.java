b69d825554aed84fc86907057483a39b60bc8689
Tobi Delbruck <Tobi Delbruck@tobidh87>
2026-08-25 10:53:58 +0200

    demote hidden property message to FINE to reduce chatter

--- src/net/sf/jaer/eventprocessing/FilterPanel.java
+++ src/net/sf/jaer/eventprocessing/FilterPanel.java
@@ -926,7 +926,7 @@ public class FilterPanel extends javax.swing.JPanel implements PropertyChangeLis
                     if (hidden) {
                         // hideNonEnabledEnclosedFilters is hidden on every EventFilter; skip log to reduce chatter
                         if (!"hideNonEnabledEnclosedFilters".equals(p.getName())) {
-                            log.log(Level.INFO, "not constructing control for {0} for hidden property {1}", new Object[]{filter.getClass().getSimpleName(), p.getName()});
+                            log.log(Level.FINE, "not constructing control for {0} for hidden property {1}", new Object[]{filter.getClass().getSimpleName(), p.getName()});
                         }
                         continue;
                     }
